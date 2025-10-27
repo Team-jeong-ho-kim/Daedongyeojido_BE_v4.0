35a05c73beced3853ca7bfb1f837c9d2233f402b
박태수 <taesupark0504@gmail.com>
2025-10-28 00:22:02 +0900

    feat(#4) :: ErrorResponse

---
+++ src/main/java/team/jeonghokim/daedongyeojido/global/error/ErrorResponse.java
@@ -0,0 +1,36 @@
+package team.jeonghokim.daedongyeojido.global.error;
+
+import lombok.*;
+import team.jeonghokim.daedongyeojido.global.error.exception.ErrorCode;
+
+import java.time.LocalDateTime;
+
+@Builder
+@Getter
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+@AllArgsConstructor
+public class ErrorResponse {
+    private String message;
+    private Integer status;
+    private LocalDateTime timestamp;
+    private String description;
+
+    public static ErrorResponse of(ErrorCode errorCode, String description) {
+        return ErrorResponse.builder()
+                .message(errorCode.getErrorMessage())
+                .status(errorCode.getStatusCode())
+                .timestamp(LocalDateTime.now())
+                .description(description)
+                .build();
+    }
+
+    public static ErrorResponse of(int statusCode, String description) {
+        return ErrorResponse.builder()
+                .message(description)
+                .status(statusCode)
+                .timestamp(LocalDateTime.now())
+                .description(description)
+                .build();
+    }
+
+}
