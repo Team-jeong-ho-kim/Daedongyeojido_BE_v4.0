6dbb2d5e64c7bcc4ac64fba810cd7d3f78bc4e87
박태수 <taesupark0504@gmail.com>
2025-10-28 11:05:28 +0900

    refactor(#4) :: MethodArgumentNotValidException 추가

--- src/main/java/team/jeonghokim/daedongyeojido/global/error/GlobalExceptionHandler.java
+++ src/main/java/team/jeonghokim/daedongyeojido/global/error/GlobalExceptionHandler.java
@@ -2,6 +2,7 @@ package team.jeonghokim.daedongyeojido.global.error;
 
 import lombok.extern.slf4j.Slf4j;
 import org.hibernate.exception.ConstraintViolationException;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -13,11 +14,9 @@ import team.jeonghokim.daedongyeojido.global.error.exception.ErrorCode;
 @RestControllerAdvice
 public class GlobalExceptionHandler {
 
-
     //비즈니스 로직에서의 에러
     @ExceptionHandler(DaedongException.class)
     public ResponseEntity<ErrorResponse> handClimException(DaedongException e) {
-
         ErrorCode errorCode = e.getErrorCode();
         ErrorResponse response = ErrorResponse.of(errorCode, e.getMessage());
         e.printStackTrace();
@@ -35,6 +34,15 @@ public class GlobalExceptionHandler {
         return new ResponseEntity<>(response, HttpStatus.valueOf(errorCode.getStatusCode()));
     }
 
+    // DTO 필드 검증 에러
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<ErrorResponse> handleMethodArgumentNotValidException(MethodArgumentNotValidException e) {
+        ErrorCode errorCode = ErrorCode.BAD_REQUEST;
+        ErrorResponse response = ErrorResponse.of(errorCode, errorCode.getErrorMessage());
+        e.printStackTrace();
+
+        return new ResponseEntity<>(response, HttpStatus.valueOf(errorCode.getStatusCode()));
+    }
 
     //그 외 에러들
     @ExceptionHandler(Exception.class)
