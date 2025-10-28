f2c17733fee90d99e8586703fbcc4016133230fd
박태수 <taesupark0504@gmail.com>
2025-10-28 14:18:59 +0900

    feat(#5) :: user entity

---
+++ src/main/java/team/jeonghokim/daedongyeojido/domain/user/domain/User.java
@@ -0,0 +1,51 @@
+package team.jeonghokim.daedongyeojido.domain.user.domain;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import team.jeonghokim.daedongyeojido.domain.user.domain.enums.Major;
+import team.jeonghokim.daedongyeojido.domain.user.domain.enums.Role;
+import team.jeonghokim.daedongyeojido.global.entity.BaseIdEntity;
+
+import java.util.List;
+
+@Entity
+@Getter
+@NoArgsConstructor
+public class User extends BaseIdEntity {
+
+    @Column(nullable = false, unique = true)
+    private String accountId;
+
+    @Column(nullable = false, length = 4)
+    private String userName;
+
+    @Column(nullable = false, length = 4)
+    private String classNumber;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Column(nullable = false,  length = 11)
+    private int phoneNumber;
+
+    @Column(length = 30)
+    private String introduction;
+
+    @Column(length = 100)
+    private List<String> links;
+
+    @Column(length = 300)
+    private String profileImage;
+
+    @Enumerated(EnumType.STRING)
+    @Column(length = 10)
+    private List<Major> majors;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false, length = 11)
+    private Role role;
+}
