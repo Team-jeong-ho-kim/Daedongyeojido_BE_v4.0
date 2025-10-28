09f1a5b5ce12b7ca6b1313e98dac37a353f4ed58
박태수 <taesupark0504@gmail.com>
2025-10-28 10:49:53 +0900

    refactor(#4) :: 상수화

--- src/main/java/team/jeonghokim/daedongyeojido/global/error/GlobalExceptionFilter.java
+++ src/main/java/team/jeonghokim/daedongyeojido/global/error/GlobalExceptionFilter.java
@@ -15,13 +15,15 @@ import java.io.IOException;
 public class GlobalExceptionFilter extends OncePerRequestFilter {
 
     private final ObjectMapper objectMapper;
+    private static final String CONTENT_TYPE = "application/json";
+    private static final String CHARACTER_ENCODING = "UTF-8";
 
     @Override
     protected void doFilterInternal(
             HttpServletRequest request,
             HttpServletResponse response,
             FilterChain filterChain
-    )throws IOException {
+    ) throws IOException {
 
         try {
             filterChain.doFilter(request,response);
@@ -37,8 +39,8 @@ public class GlobalExceptionFilter extends OncePerRequestFilter {
 
     private void writerErrorResponse(HttpServletResponse response, int statusCode, ErrorResponse errorResponse) throws IOException{
         response.setStatus(statusCode);
-        response.setContentType("application/json");
-        response.setCharacterEncoding("UTF-8");
+        response.setContentType(CONTENT_TYPE);
+        response.setCharacterEncoding(CHARACTER_ENCODING);
         objectMapper.writeValue(response.getWriter(), errorResponse);
     }
 }
