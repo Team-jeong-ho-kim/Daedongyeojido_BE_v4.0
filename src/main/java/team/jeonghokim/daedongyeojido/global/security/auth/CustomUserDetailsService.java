7243dfff2c952b7178454711ff5bf8dde3e98e45
박태수 <taesupark0504@gmail.com>
2025-10-28 14:38:18 +0900

    feat(#5) :: CustomUserDetailsService

---
+++ src/main/java/team/jeonghokim/daedongyeojido/global/security/auth/CustomUserDetailsService.java
@@ -0,0 +1,23 @@
+package team.jeonghokim.daedongyeojido.global.security.auth;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Component;
+import team.jeonghokim.daedongyeojido.domain.user.domain.User;
+import team.jeonghokim.daedongyeojido.domain.user.domain.repository.UserRepository;
+
+@Component
+@RequiredArgsConstructor
+public class CustomUserDetailsService implements UserDetailsService {
+    private final UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String accountId) {
+        User user = userRepository.findByAccountId(accountId)
+                .orElseThrow(() -> new UsernameNotFoundException("User Not Found"));
+
+        return new CustomUserDetails(user);
+    }
+}
