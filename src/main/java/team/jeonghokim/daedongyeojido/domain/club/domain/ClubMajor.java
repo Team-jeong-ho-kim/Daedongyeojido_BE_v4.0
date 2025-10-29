57592488affd74ddc46a43cf95d7f115717d2654
coehgns <modooboiroo@gmail.com>
2025-10-29 19:22:13 +0900

    feat (#9) :: ClubMajor

---
+++ src/main/java/team/jeonghokim/daedongyeojido/domain/club/domain/ClubMajor.java
@@ -0,0 +1,35 @@
+package team.jeonghokim.daedongyeojido.domain.club.domain;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import team.jeonghokim.daedongyeojido.domain.user.domain.enums.Major;
+import team.jeonghokim.daedongyeojido.global.entity.BaseIdEntity;
+
+@Entity(name = "tbl_club_major")
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class ClubMajor extends BaseIdEntity {
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "club_id")
+    private Club club;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "major", nullable = false, length = 10)
+    private Major major;
+
+    @Builder
+    public ClubMajor(Club club, Major major) {
+        this.club = club;
+        this.major = major;
+    }
+}
