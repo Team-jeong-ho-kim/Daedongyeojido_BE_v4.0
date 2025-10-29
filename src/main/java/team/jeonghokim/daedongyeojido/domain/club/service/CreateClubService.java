2d058e1837d02a219daf24f8804b334a443b5e86
박태수 <taesupark0504@gmail.com>
2025-10-30 08:30:02 +0900

    chore(#13) :: facade 패키지 이동

--- src/main/java/team/jeonghokim/daedongyeojido/domain/club/service/CreateClubService.java
+++ src/main/java/team/jeonghokim/daedongyeojido/domain/club/service/CreateClubService.java
@@ -10,7 +10,7 @@ import team.jeonghokim.daedongyeojido.domain.club.domain.repository.ClubReposito
 import team.jeonghokim.daedongyeojido.domain.club.presentation.dto.request.CreateClubRequest;
 import team.jeonghokim.daedongyeojido.domain.club.service.validator.CreateClubValidator;
 import team.jeonghokim.daedongyeojido.domain.user.domain.User;
-import team.jeonghokim.daedongyeojido.domain.user.domain.facade.UserFacade;
+import team.jeonghokim.daedongyeojido.domain.user.facade.UserFacade;
 
 import java.util.List;
 import java.util.Optional;
