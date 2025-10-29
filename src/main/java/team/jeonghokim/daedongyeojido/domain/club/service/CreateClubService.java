b1a7edc1629acf14b6b8af19ee6036a0f9041835
coehgns <modooboiroo@gmail.com>
2025-10-29 19:41:34 +0900

    refactor (#9) :: CreateClubService

--- src/main/java/team/jeonghokim/daedongyeojido/domain/club/service/CreateClubService.java
+++ src/main/java/team/jeonghokim/daedongyeojido/domain/club/service/CreateClubService.java
@@ -4,6 +4,8 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import team.jeonghokim.daedongyeojido.domain.club.domain.Club;
+import team.jeonghokim.daedongyeojido.domain.club.domain.ClubLink;
+import team.jeonghokim.daedongyeojido.domain.club.domain.ClubMajor;
 import team.jeonghokim.daedongyeojido.domain.club.domain.repository.ClubRepository;
 import team.jeonghokim.daedongyeojido.domain.club.exception.AlreadyApplyClubException;
 import team.jeonghokim.daedongyeojido.domain.club.exception.AlreadyExistsClubException;
@@ -11,6 +13,10 @@ import team.jeonghokim.daedongyeojido.domain.club.exception.AlreadyJoinClubExcep
 import team.jeonghokim.daedongyeojido.domain.club.presentation.dto.request.CreateClubRequest;
 import team.jeonghokim.daedongyeojido.domain.user.domain.User;
 import team.jeonghokim.daedongyeojido.domain.user.domain.facade.UserFacade;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
@@ -35,19 +41,43 @@ public class CreateClubService {
             throw AlreadyExistsClubException.EXCEPTION;
         }
 
-        clubRepository.save(createClub(request, clubApplicant));
-    }
-
-    private Club createClub(CreateClubRequest request, User clubApplicant) {
-        return Club.builder()
+        Club club = Club.builder()
                 .clubName(request.getClubName())
                 .clubImage(request.getClubImage())
                 .oneLiner(request.getOneLiner())
                 .introduction(request.getIntroduction())
-                .major(request.getMajor())
-                .link(request.getLink())
                 .isOpened(false)
                 .clubApplicant(clubApplicant)
                 .build();
+
+        List<ClubLink> clubLinks = new ArrayList<>();
+        List<ClubMajor> clubMajors = createClubMajor(request, club);
+
+        if (request.getLink() != null && !request.getLink().isEmpty()) {
+            clubLinks = createClubLink(request, club);
+        }
+
+        club.getMajors().addAll(clubMajors);
+        club.getLinks().addAll(clubLinks);
+
+        clubRepository.save(club);
+    }
+
+    private List<ClubMajor> createClubMajor(CreateClubRequest request, Club club) {
+        return request.getMajor().stream().map(major ->
+                ClubMajor.builder()
+                        .club(club)
+                        .major(major)
+                        .build())
+                .collect(Collectors.toList());
+    }
+
+    private List<ClubLink> createClubLink(CreateClubRequest request, Club club) {
+        return request.getLink().stream().map(link ->
+                ClubLink.builder()
+                        .club(club)
+                        .link(link)
+                        .build())
+                .collect(Collectors.toList());
     }
 }
