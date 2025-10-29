f97a2743d8d35be8a4149fc89a935ee0251b11b8
coehgns <modooboiroo@gmail.com>
2025-10-29 18:49:48 +0900

    feat (#9) :: ClubController에 @RequestBody 추가

--- src/main/java/team/jeonghokim/daedongyeojido/domain/club/presentation/ClubController.java
+++ src/main/java/team/jeonghokim/daedongyeojido/domain/club/presentation/ClubController.java
@@ -2,6 +2,7 @@ package team.jeonghokim.daedongyeojido.domain.club.presentation;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import team.jeonghokim.daedongyeojido.domain.club.presentation.dto.request.CreateClubRequest;
@@ -15,7 +16,7 @@ public class ClubController {
     private final CreateClubService createClubService;
 
     @PostMapping("/create/apply")
-    public void createClub(CreateClubRequest request) {
+    public void createClub(@RequestBody CreateClubRequest request) {
         createClubService.execute(request);
     }
 }
