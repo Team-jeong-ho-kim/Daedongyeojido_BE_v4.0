3b5eb20b11c92ab4c40a737848896a7aab03f9f8
박태수 <taesupark0504@gmail.com>
2025-10-30 08:50:15 +0900

    feat(#13) :: User에 적용

--- src/main/java/team/jeonghokim/daedongyeojido/domain/user/domain/User.java
+++ src/main/java/team/jeonghokim/daedongyeojido/domain/user/domain/User.java
@@ -1,18 +1,13 @@
 package team.jeonghokim.daedongyeojido.domain.user.domain;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.EnumType;
-import jakarta.persistence.Enumerated;
-import jakarta.persistence.FetchType;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.ManyToOne;
+import jakarta.persistence.*;
 import lombok.*;
 import team.jeonghokim.daedongyeojido.domain.club.domain.Club;
-import team.jeonghokim.daedongyeojido.domain.user.domain.enums.Major;
+import team.jeonghokim.daedongyeojido.domain.club.domain.ClubMajor;
 import team.jeonghokim.daedongyeojido.domain.user.domain.enums.Role;
 import team.jeonghokim.daedongyeojido.global.entity.BaseIdEntity;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Entity(name = "tbl_user")
@@ -40,13 +35,14 @@ public class User extends BaseIdEntity {
     @Column(length = 30)
     private String introduction;
 
-    private List<String> links;
+    @OneToMany(mappedBy = "club", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<UserLink> links = new ArrayList<>();
 
     @Column(length = 300)
     private String profileImage;
 
-    @Enumerated(EnumType.STRING)
-    private List<Major> majors;
+    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<ClubMajor> majors =  new ArrayList<>();
 
     @Enumerated(EnumType.STRING)
     @Column(nullable = false, length = 11)
