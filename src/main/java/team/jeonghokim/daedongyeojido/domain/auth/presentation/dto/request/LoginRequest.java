df83d85a8d9667f3a00c6d46540bf59272600745
박태수 <taesupark0504@gmail.com>
2025-10-30 11:12:23 +0900

    chore(#13) :: 유효성 검사

--- src/main/java/team/jeonghokim/daedongyeojido/domain/auth/presentation/dto/request/LoginRequest.java
+++ src/main/java/team/jeonghokim/daedongyeojido/domain/auth/presentation/dto/request/LoginRequest.java
@@ -1,7 +1,13 @@
 package team.jeonghokim.daedongyeojido.domain.auth.presentation.dto.request;
 
+import jakarta.validation.constraints.NotBlank;
+
 public record LoginRequest(
+
+        @NotBlank(message = "계정 ID를 입력해주세요.")
         String accountId,
+
+        @NotBlank(message = "비밀번호를 입력해주세요.")
         String password
 ) {
 }
