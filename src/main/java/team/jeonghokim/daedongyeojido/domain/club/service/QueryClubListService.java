7a552876bf4c2a06e2473af7f19a5ad5005dcda0
coehgns <modooboiroo@gmail.com>
2025-10-30 12:21:33 +0900

    feat (#15) :: QueryClubListService

---
+++ src/main/java/team/jeonghokim/daedongyeojido/domain/club/service/QueryClubListService.java
@@ -0,0 +1,25 @@
+package team.jeonghokim.daedongyeojido.domain.club.service;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import team.jeonghokim.daedongyeojido.domain.club.domain.repository.ClubRepository;
+import team.jeonghokim.daedongyeojido.domain.club.presentation.dto.response.QueryClubListResponse;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class QueryClubListService {
+
+    private final ClubRepository clubRepository;
+
+    @Transactional(readOnly = true)
+    public QueryClubListResponse execute() {
+        List<QueryClubListResponse.ClubDto> clubs = clubRepository.findAllByIsOpenIsTrue().stream()
+                .map(QueryClubListResponse.ClubDto::from)
+                .toList();
+
+        return new QueryClubListResponse(clubs);
+    }
+}
