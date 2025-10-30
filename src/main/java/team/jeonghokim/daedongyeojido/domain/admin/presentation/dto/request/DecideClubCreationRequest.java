1c64f5e949dc7472df275cf124ab5bfb14e0210d
coehgns <modooboiroo@gmail.com>
2025-10-30 10:57:14 +0900

    refactor (#15) :: DecideClubCreationRequest @JsonProperty 삭제

--- src/main/java/team/jeonghokim/daedongyeojido/domain/admin/presentation/dto/request/DecideClubCreationRequest.java
+++ src/main/java/team/jeonghokim/daedongyeojido/domain/admin/presentation/dto/request/DecideClubCreationRequest.java
@@ -1,6 +1,5 @@
 package team.jeonghokim.daedongyeojido.domain.admin.presentation.dto.request;
 
-import com.fasterxml.jackson.annotation.JsonProperty;
 import jakarta.validation.constraints.NotNull;
 import lombok.Getter;
 
@@ -8,6 +7,5 @@ import lombok.Getter;
 public class DecideClubCreationRequest {
 
     @NotNull(message = "개설 신청 수락 및 거절 여부는 null일 수 없습니다.")
-    @JsonProperty("isOpen")
     private Boolean isOpen;
 }
