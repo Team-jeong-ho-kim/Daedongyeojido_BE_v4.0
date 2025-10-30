f4731ed6aba62e192fefd7ee72a3b0a690383442
coehgns <modooboiroo@gmail.com>
2025-10-30 12:21:46 +0900

    feat (#15) :: ClubController에 queryClubList 메서드 추가

--- src/main/java/team/jeonghokim/daedongyeojido/domain/club/presentation/ClubController.java
+++ src/main/java/team/jeonghokim/daedongyeojido/domain/club/presentation/ClubController.java
@@ -3,13 +3,16 @@ package team.jeonghokim.daedongyeojido.domain.club.presentation;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 import team.jeonghokim.daedongyeojido.domain.club.presentation.dto.request.CreateClubRequest;
+import team.jeonghokim.daedongyeojido.domain.club.presentation.dto.response.QueryClubListResponse;
 import team.jeonghokim.daedongyeojido.domain.club.service.CreateClubService;
+import team.jeonghokim.daedongyeojido.domain.club.service.QueryClubListService;
 
 @RestController
 @RequestMapping("/club")
@@ -17,10 +20,17 @@ import team.jeonghokim.daedongyeojido.domain.club.service.CreateClubService;
 public class ClubController {
 
     private final CreateClubService createClubService;
+    private final QueryClubListService queryClubListService;
 
     @PostMapping("/create/apply")
     @ResponseStatus(HttpStatus.CREATED)
     public void createClub(@RequestBody @Valid CreateClubRequest request) {
         createClubService.execute(request);
+    }
+
+    @GetMapping
+    @ResponseStatus(HttpStatus.OK)
+    public QueryClubListResponse queryClubList() {
+        return queryClubListService.execute();
     }
 }
