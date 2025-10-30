55a84999657e060d7c05a35ab4686db9bf0508b4
coehgns <modooboiroo@gmail.com>
2025-10-30 12:21:11 +0900

    feat (#15) :: ClubRepository에 findAllByIsOpenIsTrue 추가

--- src/main/java/team/jeonghokim/daedongyeojido/domain/club/domain/repository/ClubRepository.java
+++ src/main/java/team/jeonghokim/daedongyeojido/domain/club/domain/repository/ClubRepository.java
@@ -4,9 +4,13 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import team.jeonghokim.daedongyeojido.domain.club.domain.Club;
 import team.jeonghokim.daedongyeojido.domain.user.domain.User;
 
+import java.util.List;
+
 public interface ClubRepository extends JpaRepository<Club, Long> {
 
     boolean existsByClubName(String clubName);
 
     boolean existsByClubApplicant(User clubApplicant);
+
+    List<Club> findAllByIsOpenIsTrue();
 }
