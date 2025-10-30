0360f6f2aa614cb81515f733a45f9424f386a589
박태수 <taesupark0504@gmail.com>
2025-10-30 11:33:18 +0900

    feat(#13) :: q클래스

--- src/main/generated/team/jeonghokim/daedongyeojido/domain/user/domain/QUser.java
+++ src/main/generated/team/jeonghokim/daedongyeojido/domain/user/domain/QUser.java
@@ -18,6 +18,8 @@ public class QUser extends EntityPathBase<User> {
 
     private static final long serialVersionUID = -1994891311L;
 
+    private static final PathInits INITS = PathInits.DIRECT2;
+
     public static final QUser user = new QUser("user");
 
     public final team.jeonghokim.daedongyeojido.global.entity.QBaseIdEntity _super = new team.jeonghokim.daedongyeojido.global.entity.QBaseIdEntity(this);
@@ -26,14 +28,16 @@ public class QUser extends EntityPathBase<User> {
 
     public final StringPath classNumber = createString("classNumber");
 
+    public final team.jeonghokim.daedongyeojido.domain.club.domain.QClub club;
+
     //inherited
     public final NumberPath<Long> id = _super.id;
 
     public final StringPath introduction = createString("introduction");
 
-    public final ListPath<String, StringPath> links = this.<String, StringPath>createList("links", String.class, StringPath.class, PathInits.DIRECT2);
+    public final ListPath<UserLink, QUserLink> links = this.<UserLink, QUserLink>createList("links", UserLink.class, QUserLink.class, PathInits.DIRECT2);
 
-    public final ListPath<team.jeonghokim.daedongyeojido.domain.user.domain.enums.Major, EnumPath<team.jeonghokim.daedongyeojido.domain.user.domain.enums.Major>> majors = this.<team.jeonghokim.daedongyeojido.domain.user.domain.enums.Major, EnumPath<team.jeonghokim.daedongyeojido.domain.user.domain.enums.Major>>createList("majors", team.jeonghokim.daedongyeojido.domain.user.domain.enums.Major.class, EnumPath.class, PathInits.DIRECT2);
+    public final ListPath<UserMajor, QUserMajor> majors = this.<UserMajor, QUserMajor>createList("majors", UserMajor.class, QUserMajor.class, PathInits.DIRECT2);
 
     public final StringPath password = createString("password");
 
@@ -46,15 +50,24 @@ public class QUser extends EntityPathBase<User> {
     public final StringPath userName = createString("userName");
 
     public QUser(String variable) {
-        super(User.class, forVariable(variable));
+        this(User.class, forVariable(variable), INITS);
     }
 
     public QUser(Path<? extends User> path) {
-        super(path.getType(), path.getMetadata());
+        this(path.getType(), path.getMetadata(), PathInits.getFor(path.getMetadata(), INITS));
     }
 
     public QUser(PathMetadata metadata) {
-        super(User.class, metadata);
+        this(metadata, PathInits.getFor(metadata, INITS));
+    }
+
+    public QUser(PathMetadata metadata, PathInits inits) {
+        this(User.class, metadata, inits);
+    }
+
+    public QUser(Class<? extends User> type, PathMetadata metadata, PathInits inits) {
+        super(type, metadata, inits);
+        this.club = inits.isInitialized("club") ? new team.jeonghokim.daedongyeojido.domain.club.domain.QClub(forProperty("club"), inits.get("club")) : null;
     }
 
 }
