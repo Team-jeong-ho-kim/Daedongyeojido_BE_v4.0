a3a19c0a58ab597f6acdb6affeb0acac514bda9a
박태수 <taesupark0504@gmail.com>
2025-10-30 11:35:34 +0900

    feat (#13) :: loginService

---
+++ src/main/java/team/jeonghokim/daedongyeojido/domain/auth/service/LoginService.java
@@ -0,0 +1,28 @@
+package team.jeonghokim.daedongyeojido.domain.auth.service;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import team.jeonghokim.daedongyeojido.domain.auth.presentation.dto.request.LoginRequest;
+import team.jeonghokim.daedongyeojido.domain.user.exception.UserNotFoundException;
+import team.jeonghokim.daedongyeojido.global.security.jwt.JwtTokenProvider;
+import team.jeonghokim.daedongyeojido.global.xquare.XquareClient;
+import team.jeonghokim.daedongyeojido.global.xquare.dto.XquareResponse;
+
+@Service
+@RequiredArgsConstructor
+public class LoginService {
+    private final JwtTokenProvider jwtTokenProvider;
+    private final XquareClient xquareClient;
+
+    @Transactional
+    public void execute(LoginRequest request) {
+        XquareResponse xquareUser = xquareClient.getUser(request);
+
+        if (xquareUser == null || xquareUser.accountId() == null) {
+            throw UserNotFoundException.EXCEPTION;
+        }
+
+        jwtTokenProvider.receiveToken(xquareUser.accountId());
+    }
+}
