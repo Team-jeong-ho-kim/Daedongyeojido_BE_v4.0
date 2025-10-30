6285a63d6b69b9f9784977ea9c22add7540a11b9
coehgns <modooboiroo@gmail.com>
2025-10-30 12:24:22 +0900

    refactor (#15) :: QueryClubListResponse record로 리팩토링

--- src/main/java/team/jeonghokim/daedongyeojido/domain/club/presentation/dto/response/QueryClubListResponse.java
+++ src/main/java/team/jeonghokim/daedongyeojido/domain/club/presentation/dto/response/QueryClubListResponse.java
@@ -1,41 +1,34 @@
 package team.jeonghokim.daedongyeojido.domain.club.presentation.dto.response;
 
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Getter;
 import team.jeonghokim.daedongyeojido.domain.club.domain.Club;
 import team.jeonghokim.daedongyeojido.domain.club.domain.ClubMajor;
 import team.jeonghokim.daedongyeojido.domain.user.domain.enums.Major;
 
 import java.util.List;
 
-@Getter
-@AllArgsConstructor
-public class QueryClubListResponse {
+public record QueryClubListResponse(List<ClubDto> clubs) {
 
-    private final List<ClubDto> clubs;
+    public static QueryClubListResponse from(List<Club> clubs) {
+        return new QueryClubListResponse(
+                clubs.stream().map(ClubDto::from).toList()
+        );
+    }
 
-    @Getter
-    @Builder
-    public static final class ClubDto {
-        private final Long clubId;
-        private final String clubName;
-        private final String clubImage;
-        private final String introduction;
-        private final List<Major> majors;
-
+    public record ClubDto(
+            Long clubId,
+            String clubName,
+            String clubImage,
+            String introduction,
+            List<Major> majors
+    ) {
         public static ClubDto from(Club club) {
-            return ClubDto.builder()
-                    .clubId(club.getId())
-                    .clubName(club.getClubName())
-                    .clubImage(club.getClubImage())
-                    .introduction(club.getIntroduction())
-                    .majors(
-                            club.getMajors().stream()
-                                    .map(ClubMajor::getMajor)
-                                    .toList()
-                    )
-                    .build();
+            return new ClubDto(
+                    club.getId(),
+                    club.getClubName(),
+                    club.getClubImage(),
+                    club.getIntroduction(),
+                    club.getMajors().stream().map(ClubMajor::getMajor).toList()
+            );
         }
     }
 }
