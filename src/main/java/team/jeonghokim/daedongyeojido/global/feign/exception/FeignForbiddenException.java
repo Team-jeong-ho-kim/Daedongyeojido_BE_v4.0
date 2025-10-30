952712b233cf4f48d8068fe1cd73cf3520a5e29d
박태수 <taesupark0504@gmail.com>
2025-10-30 10:49:03 +0900

    feat(#13) :: feign 예외 클래스

---
+++ src/main/java/team/jeonghokim/daedongyeojido/global/feign/exception/FeignForbiddenException.java
@@ -0,0 +1,14 @@
+package team.jeonghokim.daedongyeojido.global.feign.exception;
+
+
+import team.jeonghokim.daedongyeojido.global.error.exception.DaedongException;
+import team.jeonghokim.daedongyeojido.global.error.exception.ErrorCode;
+
+public class FeignForbiddenException extends DaedongException {
+
+    public static final DaedongException EXCEPTION = new FeignForbiddenException();
+
+    private FeignForbiddenException(){
+        super(ErrorCode.FEIGN_FORBIDDDEN_EXCEPTION);
+    }
+}
