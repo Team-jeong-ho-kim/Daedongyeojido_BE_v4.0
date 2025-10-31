4e886ee155c473374497bc4958dabc67a1c9c2f8
coehgns <modooboiroo@gmail.com>
2025-10-31 18:25:25 +0900

    feat (#19) :: ClubController에 queryClubDetail 추가

--- src/main/java/team/jeonghokim/daedongyeojido/domain/club/presentation/ClubController.java
+++ src/main/java/team/jeonghokim/daedongyeojido/domain/club/presentation/ClubController.java
@@ -4,14 +4,17 @@ import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 import team.jeonghokim.daedongyeojido.domain.club.presentation.dto.request.CreateClubRequest;
+import team.jeonghokim.daedongyeojido.domain.club.presentation.dto.response.QueryClubDetailResponse;
 import team.jeonghokim.daedongyeojido.domain.club.presentation.dto.response.QueryClubListResponse;
 import team.jeonghokim.daedongyeojido.domain.club.service.CreateClubService;
+import team.jeonghokim.daedongyeojido.domain.club.service.QueryClubDetailService;
 import team.jeonghokim.daedongyeojido.domain.club.service.QueryClubListService;
 
 @RestController
@@ -21,6 +24,7 @@ public class ClubController {
 
     private final CreateClubService createClubService;
     private final QueryClubListService queryClubListService;
+    private final QueryClubDetailService queryClubDetailService;
 
     @PostMapping("/create/apply")
     @ResponseStatus(HttpStatus.CREATED)
@@ -32,5 +36,10 @@ public class ClubController {
     @ResponseStatus(HttpStatus.OK)
     public QueryClubListResponse queryClubList() {
         return queryClubListService.execute();
+    }
+
+    @GetMapping("/{club-id}")
+    public QueryClubDetailResponse queryClubDetail(@PathVariable("club-id") Long clubId) {
+        return queryClubDetailService.execute(clubId);
     }
 }
