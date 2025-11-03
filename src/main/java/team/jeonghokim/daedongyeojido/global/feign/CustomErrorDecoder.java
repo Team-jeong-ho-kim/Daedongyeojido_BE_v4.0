bc14f4f8d33b18f9e836ea991368d931d1e9fbdf
박태수 <taesupark0504@gmail.com>
2025-11-03 09:08:12 +0900

    refactor (#13) :: feign decoder에 404추가

--- src/main/java/team/jeonghokim/daedongyeojido/global/feign/CustomErrorDecoder.java
+++ src/main/java/team/jeonghokim/daedongyeojido/global/feign/CustomErrorDecoder.java
@@ -2,6 +2,7 @@ package team.jeonghokim.daedongyeojido.global.feign;
 
 import feign.codec.ErrorDecoder;
 import feign.Response;
+import team.jeonghokim.daedongyeojido.domain.user.exception.UserNotFoundException;
 import team.jeonghokim.daedongyeojido.global.feign.exception.FeignBadRequestException;
 import team.jeonghokim.daedongyeojido.global.feign.exception.FeignForbiddenException;
 import team.jeonghokim.daedongyeojido.global.feign.exception.FeignUnauthorizedException;
@@ -22,6 +23,8 @@ public class CustomErrorDecoder implements ErrorDecoder {
                 return FeignUnauthorizedException.EXCEPTION;
             case 403:
                 return FeignForbiddenException.EXCEPTION;
+            case 404:
+                return UserNotFoundException.EXCEPTION;
             default:
                 return defaultDecoder.decode(methodKey, response);
         }
