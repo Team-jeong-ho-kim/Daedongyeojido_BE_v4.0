b3c0457f1315a1c6c55e32ab41544815c0fc6dc6
박태수 <taesupark0504@gmail.com>
2025-11-03 23:41:55 +0900

    feat (#27) :: s3적용

--- src/main/java/team/jeonghokim/daedongyeojido/domain/user/service/InputMyInfoService.java
+++ src/main/java/team/jeonghokim/daedongyeojido/domain/user/service/InputMyInfoService.java
@@ -7,22 +7,27 @@ import team.jeonghokim.daedongyeojido.domain.user.domain.User;
 import team.jeonghokim.daedongyeojido.domain.user.facade.UserFacade;
 import team.jeonghokim.daedongyeojido.domain.user.mapper.UserMapper;
 import team.jeonghokim.daedongyeojido.domain.user.presentation.dto.request.MyInfoRequest;
+import team.jeonghokim.daedongyeojido.infrastructure.s3.service.S3Service;
 
 @Service
 @RequiredArgsConstructor
 public class InputMyInfoService {
     private final UserFacade userFacade;
     private final UserMapper userMapper;
+    private final S3Service s3Service;
 
     @Transactional
     public void execute(MyInfoRequest request) {
         User user = userFacade.getCurrentUser();
 
+        String profileImage = s3Service.upload(request.profileImage());
+
         user.inputMyInfo(
                 request.phoneNumber(),
                 request.introduction(),
                 userMapper.toUserMajors(request, user),
-                userMapper.toUserLinks(request, user)
+                userMapper.toUserLinks(request, user),
+                profileImage
         );
     }
 }
