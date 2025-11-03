1465c178bbc7d02445b67bfe8a4b19f433304922
박태수 <taesupark0504@gmail.com>
2025-11-03 23:19:48 +0900

    chore (#27) :: dto 유효성 검사 @Size추가

--- src/main/java/team/jeonghokim/daedongyeojido/domain/user/presentation/dto/request/MyInfoRequest.java
+++ src/main/java/team/jeonghokim/daedongyeojido/domain/user/presentation/dto/request/MyInfoRequest.java
@@ -1,16 +1,19 @@
 package team.jeonghokim.daedongyeojido.domain.user.presentation.dto.request;
 
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import team.jeonghokim.daedongyeojido.domain.user.domain.enums.Major;
 
 import java.util.List;
 
 public record MyInfoRequest(
 
-        @NotBlank(message = "전화번호를 입력해주세요.")
+        @NotBlank
+        @Size(max = 11, message = "전화번호는 11자 이내로 작성해주세요.")
         String phoneNumber,
 
-        @NotBlank(message = "자기소개를 입력해주세요.")
+        @NotBlank
+        @Size(max = 30, message = "자기소개는 30자 이내로 작성해주세요.")
         String introduction,
 
         List<Major> majors,
