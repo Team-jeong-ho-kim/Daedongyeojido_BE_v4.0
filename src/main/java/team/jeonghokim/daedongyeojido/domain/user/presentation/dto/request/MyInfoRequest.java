b3822960ffffee943dd5b93af0ed1b38730f217b
박태수 <taesupark0504@gmail.com>
2025-11-03 23:07:59 +0900

    chore (#27) :: major로 변경

--- src/main/java/team/jeonghokim/daedongyeojido/domain/user/presentation/dto/request/MyInfoRequest.java
+++ src/main/java/team/jeonghokim/daedongyeojido/domain/user/presentation/dto/request/MyInfoRequest.java
@@ -1,11 +1,19 @@
 package team.jeonghokim.daedongyeojido.domain.user.presentation.dto.request;
 
+import jakarta.validation.constraints.NotBlank;
+import team.jeonghokim.daedongyeojido.domain.user.domain.enums.Major;
+
 import java.util.List;
 
 public record MyInfoRequest(
+
+        @NotBlank
         String phoneNumber,
+
+        @NotBlank
         String introduction,
-        List<String> majors,
+
+        List<Major> majors,
         List<String> links
 ) {
 }
