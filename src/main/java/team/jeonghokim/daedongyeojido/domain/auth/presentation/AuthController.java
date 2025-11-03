184e9507a03b33162471f23149b54403243eb8d5
박태수 <taesupark0504@gmail.com>
2025-11-03 16:15:29 +0900

    feat (#23) :: logout API

--- src/main/java/team/jeonghokim/daedongyeojido/domain/auth/presentation/AuthController.java
+++ src/main/java/team/jeonghokim/daedongyeojido/domain/auth/presentation/AuthController.java
@@ -7,16 +7,24 @@ import org.springframework.web.bind.annotation.*;
 import team.jeonghokim.daedongyeojido.domain.auth.presentation.dto.request.LoginRequest;
 import team.jeonghokim.daedongyeojido.domain.auth.presentation.dto.response.TokenResponse;
 import team.jeonghokim.daedongyeojido.domain.auth.service.LoginService;
+import team.jeonghokim.daedongyeojido.domain.auth.service.LogoutService;
 
 @RestController
 @RequestMapping("/auth")
 @RequiredArgsConstructor
 public class AuthController {
     private final LoginService loginService;
+    private final LogoutService logoutService;
 
     @PostMapping("/login")
     @ResponseStatus(HttpStatus.OK)
     public TokenResponse login(@RequestBody @Valid LoginRequest request) {
         return loginService.execute(request);
+    }
+
+    @DeleteMapping("/logout")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void logout() {
+        logoutService.execute();
     }
 }
