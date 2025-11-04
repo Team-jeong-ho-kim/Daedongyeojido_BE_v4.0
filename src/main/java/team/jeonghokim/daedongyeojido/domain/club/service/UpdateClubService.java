f5993747ed850d0dcfcadd1c34896ec01d39f1e3
coehgns <modooboiroo@gmail.com>
2025-11-04 17:07:11 +0900

    refactor (#21) :: UpdateClubService 재수정

--- src/main/java/team/jeonghokim/daedongyeojido/domain/club/service/UpdateClubService.java
+++ src/main/java/team/jeonghokim/daedongyeojido/domain/club/service/UpdateClubService.java
@@ -4,23 +4,15 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import team.jeonghokim.daedongyeojido.domain.club.domain.Club;
-import team.jeonghokim.daedongyeojido.domain.club.domain.ClubLink;
-import team.jeonghokim.daedongyeojido.domain.club.domain.ClubMajor;
-import team.jeonghokim.daedongyeojido.domain.club.domain.repository.ClubLinkRepository;
-import team.jeonghokim.daedongyeojido.domain.club.domain.repository.ClubMajorRepository;
 import team.jeonghokim.daedongyeojido.domain.club.domain.repository.ClubRepository;
 import team.jeonghokim.daedongyeojido.domain.club.exception.ClubNotFoundException;
 import team.jeonghokim.daedongyeojido.domain.club.presentation.dto.request.ClubRequest;
-
-import java.util.List;
 
 @Service
 @RequiredArgsConstructor
 public class UpdateClubService {
 
     private final ClubRepository clubRepository;
-    private final ClubMajorRepository clubMajorRepository;
-    private final ClubLinkRepository clubLinkRepository;
 
     @Transactional
     public void execute(Long clubId, ClubRequest request) {
@@ -28,27 +20,5 @@ public class UpdateClubService {
                 .orElseThrow(() -> ClubNotFoundException.EXCEPTION);
 
         club.updateClub(request);
-        updateClubMajor(club, request);
-        updateClubLink(club, request);
-    }
-
-    private void updateClubMajor(Club club, ClubRequest request) {
-        clubMajorRepository.deleteAllByClub(club);
-        List<ClubMajor> clubMajors = request.getMajor().stream()
-                .map(major -> new ClubMajor(club, major))
-                .toList();
-
-        clubMajorRepository.saveAll(clubMajors);
-    }
-
-    private void updateClubLink(Club club, ClubRequest request) {
-        if (request.getLink() != null) {
-            clubLinkRepository.deleteAllByClub(club);
-            List<ClubLink> clubLinks = request.getLink().stream()
-                    .map(link -> new ClubLink(club, link))
-                    .toList();
-
-            clubLinkRepository.saveAll(clubLinks);
-        }
     }
 }
