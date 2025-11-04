1951af160b41f1565524c7743f5558685ccddc67
coehgns <modooboiroo@gmail.com>
2025-11-04 14:50:39 +0900

    feat (#21) :: ClubMajorRepository에 findAllByClub 메서드 추가

--- src/main/java/team/jeonghokim/daedongyeojido/domain/club/domain/repository/ClubMajorRepository.java
+++ src/main/java/team/jeonghokim/daedongyeojido/domain/club/domain/repository/ClubMajorRepository.java
@@ -1,7 +1,12 @@
 package team.jeonghokim.daedongyeojido.domain.club.domain.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import team.jeonghokim.daedongyeojido.domain.club.domain.Club;
 import team.jeonghokim.daedongyeojido.domain.club.domain.ClubMajor;
 
+import java.util.List;
+
 public interface ClubMajorRepository extends JpaRepository<ClubMajor, Long> {
+
+    List<ClubMajor> findAllByClub(Club club);
 }
