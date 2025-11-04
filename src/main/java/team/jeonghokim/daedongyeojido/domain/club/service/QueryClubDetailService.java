220ebc58fca96ce80dee4a67b7c55d068682e7eb
coehgns <modooboiroo@gmail.com>
2025-11-04 17:06:54 +0900

    refactor (#21) :: QueryClubDetailService 재수정

--- src/main/java/team/jeonghokim/daedongyeojido/domain/club/service/QueryClubDetailService.java
+++ src/main/java/team/jeonghokim/daedongyeojido/domain/club/service/QueryClubDetailService.java
@@ -19,17 +19,12 @@ import java.util.List;
 public class QueryClubDetailService {
 
     private final ClubRepository clubRepository;
-    private final ClubMajorRepository clubMajorRepository;
-    private final ClubLinkRepository clubLinkRepository;
 
     @Transactional(readOnly = true)
     public QueryClubDetailResponse execute(Long clubId) {
         Club club = clubRepository.findById(clubId)
                 .orElseThrow(() -> ClubNotFoundException.EXCEPTION);
 
-        List<ClubMajor> clubMajors = clubMajorRepository.findAllByClub(club);
-        List<ClubLink> clubLinks = clubLinkRepository.findAllByClub(club);
-
-        return QueryClubDetailResponse.of(club, clubMajors, clubLinks);
+        return QueryClubDetailResponse.of(club);
     }
 }
