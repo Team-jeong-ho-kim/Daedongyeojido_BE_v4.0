91cac7199e0e1700134c2c671116cbf698984539
박태수 <taesupark0504@gmail.com>
2025-11-04 09:49:11 +0900

    refactor (#27) :: service단에서 메서드 처리

--- src/main/java/team/jeonghokim/daedongyeojido/domain/user/service/InputMyInfoService.java
+++ src/main/java/team/jeonghokim/daedongyeojido/domain/user/service/InputMyInfoService.java
@@ -4,16 +4,20 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import team.jeonghokim.daedongyeojido.domain.user.domain.User;
+import team.jeonghokim.daedongyeojido.domain.user.domain.UserLink;
+import team.jeonghokim.daedongyeojido.domain.user.domain.UserMajor;
 import team.jeonghokim.daedongyeojido.domain.user.facade.UserFacade;
-import team.jeonghokim.daedongyeojido.domain.user.mapper.UserMapper;
 import team.jeonghokim.daedongyeojido.domain.user.presentation.dto.request.MyInfoRequest;
 import team.jeonghokim.daedongyeojido.infrastructure.s3.service.S3Service;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
 public class InputMyInfoService {
     private final UserFacade userFacade;
-    private final UserMapper userMapper;
     private final S3Service s3Service;
 
     @Transactional
@@ -25,9 +29,31 @@ public class InputMyInfoService {
         user.inputMyInfo(
                 request.phoneNumber(),
                 request.introduction(),
-                userMapper.toUserMajors(request, user),
-                userMapper.toUserLinks(request, user),
+                createUserMajor(request, user),
+                createUserLink(request, user),
                 profileImage
         );
+    }
+
+    private List<UserMajor> createUserMajor(MyInfoRequest request, User user) {
+        return Optional.ofNullable(request.majors())
+                .orElseGet(List::of)
+                .stream()
+                .map(major -> UserMajor.builder()
+                        .user(user)
+                        .major(major)
+                        .build())
+                .collect(Collectors.toList());
+    }
+
+    private List<UserLink> createUserLink(MyInfoRequest request, User user) {
+        return Optional.ofNullable(request.links())
+                .orElseGet(List::of)
+                .stream()
+                .map(link -> UserLink.builder()
+                        .user(user)
+                        .link(link)
+                        .build())
+                .collect(Collectors.toList());
     }
 }
