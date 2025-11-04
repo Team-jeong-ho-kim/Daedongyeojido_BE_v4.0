5d321b88ebf8367a5a00fa5728d5a45dd43fbf45
coehgns <modooboiroo@gmail.com>
2025-11-04 17:01:58 +0900

    refactor (#21) :: CreateClubService 연관관계 설정에 맞게 수정

--- src/main/java/team/jeonghokim/daedongyeojido/domain/club/service/CreateClubService.java
+++ src/main/java/team/jeonghokim/daedongyeojido/domain/club/service/CreateClubService.java
@@ -6,8 +6,6 @@ import org.springframework.transaction.annotation.Transactional;
 import team.jeonghokim.daedongyeojido.domain.club.domain.Club;
 import team.jeonghokim.daedongyeojido.domain.club.domain.ClubLink;
 import team.jeonghokim.daedongyeojido.domain.club.domain.ClubMajor;
-import team.jeonghokim.daedongyeojido.domain.club.domain.repository.ClubLinkRepository;
-import team.jeonghokim.daedongyeojido.domain.club.domain.repository.ClubMajorRepository;
 import team.jeonghokim.daedongyeojido.domain.club.domain.repository.ClubRepository;
 import team.jeonghokim.daedongyeojido.domain.club.presentation.dto.request.ClubRequest;
 import team.jeonghokim.daedongyeojido.domain.club.service.validator.CreateClubValidator;
@@ -23,8 +21,6 @@ import java.util.stream.Collectors;
 public class CreateClubService {
 
     private final ClubRepository clubRepository;
-    private final ClubMajorRepository clubMajorRepository;
-    private final ClubLinkRepository clubLinkRepository;
     private final UserFacade userFacade;
     private final CreateClubValidator createClubValidator;
 
@@ -34,16 +30,15 @@ public class CreateClubService {
 
         createClubValidator.validate(request, clubApplicant);
 
-        Club club = createClub(request, clubApplicant);
-        List<ClubMajor> clubMajors = createClubMajor(request, club);
-        List<ClubLink> clubLinks = createClubLink(request, club);
+        List<ClubMajor> clubMajors = createClubMajor(request);
+        List<ClubLink> clubLinks = createClubLink(request);
+
+        Club club = createClub(request, clubApplicant, clubMajors, clubLinks);
 
         clubRepository.save(club);
-        clubMajorRepository.saveAll(clubMajors);
-        clubLinkRepository.saveAll(clubLinks);
     }
 
-    private Club createClub(ClubRequest request, User clubApplicant) {
+    private Club createClub(ClubRequest request, User clubApplicant, List<ClubMajor> clubMajors, List<ClubLink> clubLinks) {
         return Club.builder()
                 .clubName(request.getClubName())
                 .clubImage(request.getClubImage())
@@ -51,24 +46,24 @@ public class CreateClubService {
                 .introduction(request.getIntroduction())
                 .isOpen(false)
                 .clubApplicant(clubApplicant)
+                .clubMajors(clubMajors)
+                .clubLinks(clubLinks)
                 .build();
     }
 
-    private List<ClubMajor> createClubMajor(ClubRequest request, Club cLub) {
+    private List<ClubMajor> createClubMajor(ClubRequest request) {
         return request.getMajor().stream().map(major ->
                 ClubMajor.builder()
-                        .club(cLub)
                         .major(major)
                         .build())
                 .collect(Collectors.toList());
     }
 
-    private List<ClubLink> createClubLink(ClubRequest request, Club club) {
+    private List<ClubLink> createClubLink(ClubRequest request) {
         return Optional.ofNullable(request.getLink())
                 .orElseGet(List::of)
                 .stream()
                 .map(link -> ClubLink.builder()
-                        .club(club)
                         .link(link)
                         .build())
                 .collect(Collectors.toList());
