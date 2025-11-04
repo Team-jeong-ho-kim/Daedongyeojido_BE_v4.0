f7b6d178c5e91d8e427ac503e9e409bdc22c3b82
coehgns <modooboiroo@gmail.com>
2025-11-04 14:51:29 +0900

    refactor (#21) :: QueryClubListService

--- src/main/java/team/jeonghokim/daedongyeojido/domain/club/service/QueryClubListService.java
+++ src/main/java/team/jeonghokim/daedongyeojido/domain/club/service/QueryClubListService.java
@@ -3,6 +3,8 @@ package team.jeonghokim.daedongyeojido.domain.club.service;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import team.jeonghokim.daedongyeojido.domain.club.domain.ClubMajor;
+import team.jeonghokim.daedongyeojido.domain.club.domain.repository.ClubMajorRepository;
 import team.jeonghokim.daedongyeojido.domain.club.domain.repository.ClubRepository;
 import team.jeonghokim.daedongyeojido.domain.club.presentation.dto.response.QueryClubListResponse;
 
@@ -13,12 +15,15 @@ import java.util.List;
 public class QueryClubListService {
 
     private final ClubRepository clubRepository;
+    private final ClubMajorRepository clubMajorRepository;
 
     @Transactional(readOnly = true)
     public QueryClubListResponse execute() {
         List<QueryClubListResponse.ClubDto> clubs = clubRepository.findAllByIsOpenIsTrue().stream()
-                .map(QueryClubListResponse.ClubDto::from)
-                .toList();
+                .map(club -> {
+                    List<ClubMajor> clubMajors = clubMajorRepository.findAllByClub(club);
+                    return QueryClubListResponse.ClubDto.from(club, clubMajors);
+                }).toList();
 
         return new QueryClubListResponse(clubs);
     }
