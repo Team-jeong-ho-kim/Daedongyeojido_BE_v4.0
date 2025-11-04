4d40866f99214043a776993c2a07f4ca749505a5
coehgns <modooboiroo@gmail.com>
2025-11-04 20:11:22 +0900

    chore (#21) :: ClubLinkRepository 미사용 메서드 제거

--- src/main/java/team/jeonghokim/daedongyeojido/domain/club/domain/repository/ClubLinkRepository.java
+++ src/main/java/team/jeonghokim/daedongyeojido/domain/club/domain/repository/ClubLinkRepository.java
@@ -1,14 +1,8 @@
 package team.jeonghokim.daedongyeojido.domain.club.domain.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import team.jeonghokim.daedongyeojido.domain.club.domain.Club;
 import team.jeonghokim.daedongyeojido.domain.club.domain.ClubLink;
-
-import java.util.List;
 
 public interface ClubLinkRepository extends JpaRepository<ClubLink, Long> {
 
-    List<ClubLink> findAllByClub(Club club);
-
-    void deleteAllByClub(Club club);
 }
