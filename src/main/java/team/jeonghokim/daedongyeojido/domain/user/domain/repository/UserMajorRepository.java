b7f884bc5a9f8a8e3bc3598e8a8f967c07cd47b6
박태수 <taesupark0504@gmail.com>
2025-11-05 09:48:36 +0900

    feat (#29) :: userMajorRepository

--- src/main/java/team/jeonghokim/daedongyeojido/domain/user/domain/repository/UserMajorRepository.java
+++ src/main/java/team/jeonghokim/daedongyeojido/domain/user/domain/repository/UserMajorRepository.java
@@ -3,5 +3,9 @@ package team.jeonghokim.daedongyeojido.domain.user.domain.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import team.jeonghokim.daedongyeojido.domain.user.domain.UserMajor;
 
+import java.util.List;
+
 public interface UserMajorRepository extends JpaRepository<UserMajor, Long> {
+
+    List<UserMajor> findAllByUserId(Long userId);
 }
