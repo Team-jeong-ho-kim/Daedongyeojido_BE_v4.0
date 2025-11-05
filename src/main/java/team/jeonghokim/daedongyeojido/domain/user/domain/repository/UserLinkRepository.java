1aca9c8568e9c83768c693fd70d366b2f1d8e4bd
박태수 <taesupark0504@gmail.com>
2025-11-05 09:48:26 +0900

    feat (#29) :: userLinkRepository

--- src/main/java/team/jeonghokim/daedongyeojido/domain/user/domain/repository/UserLinkRepository.java
+++ src/main/java/team/jeonghokim/daedongyeojido/domain/user/domain/repository/UserLinkRepository.java
@@ -3,5 +3,9 @@ package team.jeonghokim.daedongyeojido.domain.user.domain.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import team.jeonghokim.daedongyeojido.domain.user.domain.UserLink;
 
+import java.util.List;
+
 public interface UserLinkRepository extends JpaRepository<UserLink, Long> {
+
+    List<UserLink> findAllByUserId(Long userId);
 }
