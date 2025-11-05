d865cbd2a63a71b931957b9526169ae2c4dd2ae3
박태수 <taesupark0504@gmail.com>
2025-11-05 09:54:35 +0900

    feat (#29) :: customRepository

---
+++ src/main/java/team/jeonghokim/daedongyeojido/domain/user/domain/repository/UserRepositoryCustomImpl.java
@@ -0,0 +1,9 @@
+package team.jeonghokim.daedongyeojido.domain.user.domain.repository;
+
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+public class UserRepositoryCustomImpl implements UserRepositoryCustom {
+    private final JPAQueryFactory queryFactory;
+}
