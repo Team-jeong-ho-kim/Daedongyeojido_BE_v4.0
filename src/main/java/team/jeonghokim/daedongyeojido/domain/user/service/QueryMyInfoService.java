dacefd7573b567430a477d01f56d60eaba2489fc
박태수 <taesupark0504@gmail.com>
2025-11-06 20:34:35 +0900

    feat (#29) :: queryMyInfo

--- src/main/java/team/jeonghokim/daedongyeojido/domain/user/service/QueryMyInfoService.java
+++ src/main/java/team/jeonghokim/daedongyeojido/domain/user/service/QueryMyInfoService.java
@@ -4,10 +4,13 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import team.jeonghokim.daedongyeojido.domain.user.domain.User;
+import team.jeonghokim.daedongyeojido.domain.user.domain.UserLink;
+import team.jeonghokim.daedongyeojido.domain.user.domain.UserMajor;
 import team.jeonghokim.daedongyeojido.domain.user.domain.repository.UserRepository;
-import team.jeonghokim.daedongyeojido.domain.user.exception.UserNotFoundException;
 import team.jeonghokim.daedongyeojido.domain.user.facade.UserFacade;
 import team.jeonghokim.daedongyeojido.domain.user.presentation.dto.response.MyInfoResponse;
+
+import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
@@ -16,10 +19,17 @@ public class QueryMyInfoService {
     private final UserRepository userRepository;
 
     @Transactional(readOnly = true)
-    public MyInfoResponse execute(Long userId) {
+    public MyInfoResponse execute() {
         User user = userFacade.getCurrentUser();
 
-        return userRepository.findByUserId(userId)
-                .orElseThrow(() -> UserNotFoundException.EXCEPTION);
+        return MyInfoResponse.builder()
+                .userName(user.getUserName())
+                .classNumber(user.getClassNumber())
+                .introduction(user.getIntroduction())
+                .club(Optional.ofNullable(user.getClub()).map(club -> club.getClubName()).orElse(null))
+                .major(user.getMajors().stream().map(UserMajor::getMajor).toList())
+                .link(user.getLinks().stream().map(UserLink::getLink).toList())
+                .profileImage(user.getProfileImage())
+                .build();
     }
 }
