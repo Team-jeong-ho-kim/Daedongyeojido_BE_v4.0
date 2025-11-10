9a3a97f867852753443213a2da398d2b4e448dbb
coehgns <modooboiroo@gmail.com>
2025-11-10 18:49:37 +0900

    refactor (#21) :: DecideClubCreationService에 ClubFacade 적용

--- src/main/java/team/jeonghokim/daedongyeojido/domain/club/service/DecideClubCreationService.java
+++ src/main/java/team/jeonghokim/daedongyeojido/domain/club/service/DecideClubCreationService.java
@@ -5,19 +5,17 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import team.jeonghokim.daedongyeojido.domain.admin.presentation.dto.request.DecideClubCreationRequest;
 import team.jeonghokim.daedongyeojido.domain.club.domain.Club;
-import team.jeonghokim.daedongyeojido.domain.club.domain.repository.ClubRepository;
-import team.jeonghokim.daedongyeojido.domain.club.exception.ClubNotFoundException;
+import team.jeonghokim.daedongyeojido.domain.club.facade.ClubFacade;
 
 @Service
 @RequiredArgsConstructor
 public class DecideClubCreationService {
 
-    private final ClubRepository clubRepository;
+    private final ClubFacade clubFacade;
 
     @Transactional
     public void execute(Long clubId, DecideClubCreationRequest request) {
-        Club club = clubRepository.findById(clubId)
-                .orElseThrow(() -> ClubNotFoundException.EXCEPTION);
+        Club club = clubFacade.getClubById(clubId);
 
         if (request.isOpen()) {
             club.clubOpen();
