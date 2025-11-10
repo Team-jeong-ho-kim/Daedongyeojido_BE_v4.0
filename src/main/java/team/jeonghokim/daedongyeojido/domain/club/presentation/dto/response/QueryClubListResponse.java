8b2a98c3e041de7751c56a74fdd70650517ba96e
coehgns <modooboiroo@gmail.com>
2025-11-10 13:34:52 +0900

    refactor (#21) :: QueryClubListResponse

--- src/main/java/team/jeonghokim/daedongyeojido/domain/club/presentation/dto/response/QueryClubListResponse.java
+++ src/main/java/team/jeonghokim/daedongyeojido/domain/club/presentation/dto/response/QueryClubListResponse.java
@@ -1,28 +1,9 @@
 package team.jeonghokim.daedongyeojido.domain.club.presentation.dto.response;
 
-import team.jeonghokim.daedongyeojido.domain.club.domain.Club;
-import team.jeonghokim.daedongyeojido.domain.club.domain.ClubMajor;
-import team.jeonghokim.daedongyeojido.domain.user.domain.enums.Major;
+import team.jeonghokim.daedongyeojido.domain.club.domain.repository.vo.ClubVO;
 
 import java.util.List;
 
-public record QueryClubListResponse(List<ClubDto> clubs) {
+public record QueryClubListResponse(List<ClubVO> clubs) {
 
-    public record ClubDto(
-            Long clubId,
-            String clubName,
-            String clubImage,
-            String introduction,
-            List<Major> majors
-    ) {
-        public static ClubDto from(Club club) {
-            return new ClubDto(
-                    club.getId(),
-                    club.getClubName(),
-                    club.getClubImage(),
-                    club.getIntroduction(),
-                    club.getClubMajors().stream().map(ClubMajor::getMajor).toList()
-            );
-        }
-    }
 }
