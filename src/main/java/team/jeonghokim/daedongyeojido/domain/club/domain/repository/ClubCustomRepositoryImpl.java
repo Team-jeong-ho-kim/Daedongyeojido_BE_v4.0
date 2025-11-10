f4373b06ee5451bbb16bba5c4ef2569bdb40d8e7
coehgns <modooboiroo@gmail.com>
2025-11-10 13:35:12 +0900

    refactor (#21) :: ClubCustomRepositoryImpl

--- src/main/java/team/jeonghokim/daedongyeojido/domain/club/domain/repository/ClubCustomRepositoryImpl.java
+++ src/main/java/team/jeonghokim/daedongyeojido/domain/club/domain/repository/ClubCustomRepositoryImpl.java
@@ -2,8 +2,8 @@ package team.jeonghokim.daedongyeojido.domain.club.domain.repository;
 
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import lombok.RequiredArgsConstructor;
-import team.jeonghokim.daedongyeojido.domain.club.domain.Club;
-import team.jeonghokim.daedongyeojido.domain.club.presentation.dto.response.QueryClubListResponse;
+import team.jeonghokim.daedongyeojido.domain.club.domain.repository.vo.ClubVO;
+import team.jeonghokim.daedongyeojido.domain.club.domain.repository.vo.QQueryClubVO;
 
 import java.util.List;
 
@@ -15,16 +15,21 @@ public class ClubCustomRepositoryImpl implements ClubCustomRepository {
     private final JPAQueryFactory jpaQueryFactory;
 
     @Override
-    public List<QueryClubListResponse.ClubDto> findAllByIsOpenIsTrue() {
-        List<Club> clubs = jpaQueryFactory
-                .selectDistinct(club)
-                .from(club)
-                .leftJoin(club.clubMajors).fetchJoin()
-                .where(club.isOpen.isTrue())
-                .fetch();
-
-        return clubs.stream()
-                .map(QueryClubListResponse.ClubDto::from)
-                .toList();
+    public List<ClubVO> findAllByIsOpenIsTrue() {
+        return jpaQueryFactory.select(
+            new QQueryClubVO(
+                club.id,
+                club.clubName,
+                club.clubImage,
+                club.introduction
+            )
+        )
+        .from(club)
+        .leftJoin(club.clubMajors)
+        .where(club.isOpen.isTrue())
+        .fetch()
+        .stream()
+        .map(ClubVO.class::cast)
+        .toList();
     }
 }
