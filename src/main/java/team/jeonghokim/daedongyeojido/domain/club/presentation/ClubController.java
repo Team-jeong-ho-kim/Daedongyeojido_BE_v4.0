7ff846aa7cb8ec5c31366a79b7d4caae65f17c33
박태수 <taesupark0504@gmail.com>
2025-11-10 20:02:36 +0900

    feat (#34) :: applyTeamMember api적용

--- src/main/java/team/jeonghokim/daedongyeojido/domain/club/presentation/ClubController.java
+++ src/main/java/team/jeonghokim/daedongyeojido/domain/club/presentation/ClubController.java
@@ -3,21 +3,12 @@ package team.jeonghokim.daedongyeojido.domain.club.presentation;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PatchMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseStatus;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import team.jeonghokim.daedongyeojido.domain.club.presentation.dto.request.ClubRequest;
+import team.jeonghokim.daedongyeojido.domain.club.presentation.dto.request.TeamMemberRequest;
 import team.jeonghokim.daedongyeojido.domain.club.presentation.dto.response.QueryClubDetailResponse;
 import team.jeonghokim.daedongyeojido.domain.club.presentation.dto.response.QueryClubListResponse;
-import team.jeonghokim.daedongyeojido.domain.club.service.CreateClubService;
-import team.jeonghokim.daedongyeojido.domain.club.service.QueryClubDetailService;
-import team.jeonghokim.daedongyeojido.domain.club.service.QueryClubListService;
-import team.jeonghokim.daedongyeojido.domain.club.service.UpdateClubService;
+import team.jeonghokim.daedongyeojido.domain.club.service.*;
 
 @RestController
 @RequestMapping("/club")
@@ -28,6 +19,7 @@ public class ClubController {
     private final QueryClubListService queryClubListService;
     private final QueryClubDetailService queryClubDetailService;
     private final UpdateClubService updateClubService;
+    private final ApplyTeamMemberService applyTeamMemberService;
 
     @PostMapping("/create/apply")
     @ResponseStatus(HttpStatus.CREATED)
@@ -51,5 +43,11 @@ public class ClubController {
     @ResponseStatus(HttpStatus.NO_CONTENT)
     public void updateClub(@PathVariable("club-id") Long clubId, @ModelAttribute @Valid ClubRequest request) {
         updateClubService.execute(clubId, request);
+    }
+
+    @PostMapping("/member/apply")
+    @ResponseStatus(HttpStatus.CREATED)
+    public void applyMember(@RequestBody @Valid TeamMemberRequest request) {
+        applyTeamMemberService.execute(request);
     }
 }
