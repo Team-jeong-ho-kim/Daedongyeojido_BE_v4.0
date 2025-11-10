f9bd6efe9b193122f160cfb5000c809bc59280fd
박태수 <taesupark0504@gmail.com>
2025-11-11 08:31:50 +0900

    feat (#34) :: clubApplication entity

---
+++ src/main/java/team/jeonghokim/daedongyeojido/domain/club/domain/ClubApplication.java
@@ -0,0 +1,25 @@
+package team.jeonghokim.daedongyeojido.domain.club.domain;
+
+import jakarta.persistence.*;
+import lombok.*;
+import team.jeonghokim.daedongyeojido.domain.user.domain.User;
+import team.jeonghokim.daedongyeojido.global.entity.BaseIdEntity;
+
+@Entity
+@Getter
+@Builder
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+public class ClubApplication extends BaseIdEntity {
+
+    @OneToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "club_id", nullable = false)
+    private Club club;
+
+    @OneToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id", nullable = false)
+    private User clubLeader;
+
+    @Column(nullable = false)
+    private boolean isApproved;
+}
