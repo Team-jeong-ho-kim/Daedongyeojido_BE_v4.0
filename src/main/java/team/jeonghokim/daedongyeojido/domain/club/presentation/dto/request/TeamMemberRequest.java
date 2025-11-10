e716204f16a438dfb9cbbb1b459e855aaf149922
박태수 <taesupark0504@gmail.com>
2025-11-10 18:46:41 +0900

    refactor (#34) :: classNumber field String -> Integer

--- src/main/java/team/jeonghokim/daedongyeojido/domain/club/presentation/dto/request/TeamMemberRequest.java
+++ src/main/java/team/jeonghokim/daedongyeojido/domain/club/presentation/dto/request/TeamMemberRequest.java
@@ -13,5 +13,5 @@ public class TeamMemberRequest {
 
     @NotBlank(message = "추가하실 팀원의 학번을 작성해주세요.")
     @Size(max = 4, message = "팀원 학번은 최대 4글자까지 작성할 수 있습니다.")
-    private String classNumber;
+    private Integer classNumber;
 }
