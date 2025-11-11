63a77e4bc8d60ff5a0160389edf32791f3dcffce
coehgns <modooboiroo@gmail.com>
2025-11-11 19:32:30 +0900

    feat (#39) :: DecideClubDissolveService

---
+++ src/main/java/team/jeonghokim/daedongyeojido/domain/club/service/DecideClubDissolveService.java
@@ -0,0 +1,26 @@
+package team.jeonghokim.daedongyeojido.domain.club.service;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import team.jeonghokim.daedongyeojido.domain.club.domain.Club;
+import team.jeonghokim.daedongyeojido.domain.club.domain.repository.ClubRepository;
+import team.jeonghokim.daedongyeojido.domain.club.facade.ClubFacade;
+import team.jeonghokim.daedongyeojido.domain.club.presentation.dto.request.DecideClubDissolveRequest;
+
+@Service
+@RequiredArgsConstructor
+public class DecideClubDissolveService {
+
+    private final ClubFacade clubFacade;
+    private final ClubRepository clubRepository;
+
+    @Transactional
+    public void execute(Long clubId, DecideClubDissolveRequest request) {
+        Club club = clubFacade.getClubById(clubId);
+
+        if (request.isDecision()) {
+            clubRepository.delete(club);
+        }
+    }
+}
