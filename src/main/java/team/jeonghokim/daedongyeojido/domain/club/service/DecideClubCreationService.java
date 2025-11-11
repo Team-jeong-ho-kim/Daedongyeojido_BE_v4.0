f9eff806ff1f249ea64434ccc7155d7f6a544eab
박태수 <taesupark0504@gmail.com>
2025-11-11 19:14:56 +0900

    refactor (#38) :: decideClubCreationService

--- src/main/java/team/jeonghokim/daedongyeojido/domain/club/service/DecideClubCreationService.java
+++ src/main/java/team/jeonghokim/daedongyeojido/domain/club/service/DecideClubCreationService.java
@@ -8,7 +8,7 @@ import team.jeonghokim.daedongyeojido.domain.club.domain.Club;
 import team.jeonghokim.daedongyeojido.domain.club.domain.ClubApplication;
 import team.jeonghokim.daedongyeojido.domain.club.domain.repository.ClubApplicationRepository;
 import team.jeonghokim.daedongyeojido.domain.club.domain.repository.ClubRepository;
-import team.jeonghokim.daedongyeojido.domain.club.exception.ClubNotFoundException;
+import team.jeonghokim.daedongyeojido.domain.club.exception.ClubApplicationNotFoundException;
 import team.jeonghokim.daedongyeojido.domain.user.domain.User;
 import team.jeonghokim.daedongyeojido.domain.user.domain.repository.UserRepository;
 import team.jeonghokim.daedongyeojido.domain.user.exception.UserNotFoundException;
@@ -28,12 +28,12 @@ public class DecideClubCreationService {
         Club club = clubFacade.getClubById(clubId);
 
         ClubApplication clubApplication = clubApplicationRepository.findByClubId(clubId)
-                .orElseThrow(() -> UserNotFoundException.EXCEPTION);
+                .orElseThrow(() -> ClubApplicationNotFoundException.EXCEPTION);
 
         User user = userRepository.findById(clubApplication.getClubLeader().getId())
                 .orElseThrow(() -> UserNotFoundException.EXCEPTION);
 
-        if (request.isOpen()) {
+        if (request.isApproved()) {
             club.clubOpen();
             clubApplication.approve();
             user.approvedClub(club);
