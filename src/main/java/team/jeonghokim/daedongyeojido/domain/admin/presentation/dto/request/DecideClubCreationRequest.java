094466050ce56cf9438a3ea6c8f109a34869813b
박태수 <taesupark0504@gmail.com>
2025-11-11 19:14:23 +0900

    refactor (#38) :: isOpen -> isApproved

--- src/main/java/team/jeonghokim/daedongyeojido/domain/admin/presentation/dto/request/DecideClubCreationRequest.java
+++ src/main/java/team/jeonghokim/daedongyeojido/domain/admin/presentation/dto/request/DecideClubCreationRequest.java
@@ -4,5 +4,5 @@ import jakarta.validation.constraints.NotNull;
 
 public record DecideClubCreationRequest(
         @NotNull(message = "개설 신청 수락 및 거절 여부는 null일 수 없습니다.")
-        Boolean isOpen
+        Boolean isApproved
 ) {}
