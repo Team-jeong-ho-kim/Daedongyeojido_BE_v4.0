d5aa4b71c3338eacb2edef81984917a13a2f297b
박태수 <taesupark0504@gmail.com>
2025-11-13 11:30:28 +0900

    chore (#41) :: jsonProperty 설정

--- src/main/java/team/jeonghokim/daedongyeojido/domain/user/presentation/dto/request/DecideTeamMemberApplicationRequest.java
+++ src/main/java/team/jeonghokim/daedongyeojido/domain/user/presentation/dto/request/DecideTeamMemberApplicationRequest.java
@@ -1,10 +1,12 @@
 package team.jeonghokim.daedongyeojido.domain.user.presentation.dto.request;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import jakarta.validation.constraints.NotNull;
 import lombok.Getter;
 
 @Getter
 public class DecideTeamMemberApplicationRequest {
     @NotNull(message = "팀원 신청 수락 및 거절 여부는 null일 수 없습니다.")
+    @JsonProperty("isApproved")
     Boolean isApproved;
 }
