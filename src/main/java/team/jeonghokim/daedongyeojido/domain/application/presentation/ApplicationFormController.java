875a93d9359486b42e46f6cefc84e76a545b2f13
박태수 <taesupark0504@gmail.com>
2025-11-13 10:32:18 +0900

    feat (#41) :: applicationFormController

---
+++ src/main/java/team/jeonghokim/daedongyeojido/domain/application/presentation/ApplicationFormController.java
@@ -0,0 +1,11 @@
+package team.jeonghokim.daedongyeojido.domain.application.presentation;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/application")
+@RequiredArgsConstructor
+public class ApplicationFormController {
+}
