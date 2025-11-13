286c6fcaf70c9c5f5b22c7cc524f0d36bfbe8390
박태수 <taesupark0504@gmail.com>
2025-11-13 11:29:10 +0900

    refactor (#41) :: applicationFormRequest

--- src/main/java/team/jeonghokim/daedongyeojido/domain/application/presentation/dto/request/ApplicationFormRequest.java
+++ src/main/java/team/jeonghokim/daedongyeojido/domain/application/presentation/dto/request/ApplicationFormRequest.java
@@ -1,20 +1,20 @@
 package team.jeonghokim.daedongyeojido.domain.application.presentation.dto.request;
 
 import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.Size;
 import lombok.Getter;
 
-import java.time.LocalDate;
 import java.util.List;
 
 @Getter
 public class ApplicationFormRequest {
 
-    @NotBlank(message = "질문을 작성해주세요.")
-    @Size(max = 150, message = "질문은 150자까지 작성할 수 있습니다.")
-    private List<String> content;
+    @NotEmpty(message = "질문을 최소 하나 이상 작성해주세요.")
+    private List<@NotBlank(message = "질문을 작성해주세요.")
+                 @Size(max = 150, message = "질문은 150자까지 작성할 수 있습니다.")
+            String> content;
 
-    @NotNull(message = "제출 기한을 설정해주세요.")
-    private LocalDate submissionDuration;
+    @NotBlank(message = "제출 기한을 설정해주세요.")
+    private String submissionDuration;
 }
