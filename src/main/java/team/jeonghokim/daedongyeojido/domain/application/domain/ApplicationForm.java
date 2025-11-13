b49a63968e853eadbaf48691301d16153732f9e5
박태수 <taesupark0504@gmail.com>
2025-11-13 10:13:55 +0900

    feat (#41) :: 양방향 매핑

--- src/main/java/team/jeonghokim/daedongyeojido/domain/application/domain/ApplicationForm.java
+++ src/main/java/team/jeonghokim/daedongyeojido/domain/application/domain/ApplicationForm.java
@@ -10,10 +10,10 @@ import team.jeonghokim.daedongyeojido.domain.user.domain.User;
 import team.jeonghokim.daedongyeojido.global.entity.BaseIdEntity;
 
 import java.time.LocalDate;
+import java.util.List;
 
-@Entity
+@Entity(name = "tbl_application_form")
 @Getter
-@Table(name = "tbl_application_form")
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @AllArgsConstructor(access = AccessLevel.PRIVATE)
 public class ApplicationForm extends BaseIdEntity {
@@ -25,6 +25,9 @@ public class ApplicationForm extends BaseIdEntity {
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "club_id", nullable = false)
     private Club club;
+
+    @OneToMany(mappedBy = "application_form", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<ApplicationQuestion> questions;
 
     @Column(nullable = false)
     private LocalDate submissionDuration;
