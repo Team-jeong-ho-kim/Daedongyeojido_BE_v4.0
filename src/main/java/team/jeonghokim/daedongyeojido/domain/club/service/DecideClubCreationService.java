32713163b6107a962860d36a6f5223acc39b8de2
coehgns <modooboiroo@gmail.com>
2025-11-13 11:39:57 +0900

    feat (#39) :: DecideClubCreationService에 수락 및 거절 후 Alarm에서 데이터를 지우도록 구현

--- src/main/java/team/jeonghokim/daedongyeojido/domain/club/service/DecideClubCreationService.java
+++ src/main/java/team/jeonghokim/daedongyeojido/domain/club/service/DecideClubCreationService.java
@@ -4,6 +4,10 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import team.jeonghokim.daedongyeojido.domain.admin.presentation.dto.request.DecideClubCreationRequest;
+import team.jeonghokim.daedongyeojido.domain.alarm.domain.Alarm;
+import team.jeonghokim.daedongyeojido.domain.alarm.domain.enums.AlarmType;
+import team.jeonghokim.daedongyeojido.domain.alarm.domain.repository.AlarmRepository;
+import team.jeonghokim.daedongyeojido.domain.alarm.exception.AlarmNotFoundException;
 import team.jeonghokim.daedongyeojido.domain.club.domain.Club;
 import team.jeonghokim.daedongyeojido.domain.user.domain.User;
 import team.jeonghokim.daedongyeojido.domain.user.domain.repository.UserRepository;
@@ -16,15 +20,23 @@ public class DecideClubCreationService {
 
     private final UserRepository userRepository;
     private final ClubFacade clubFacade;
+    private final AlarmRepository alarmRepository;
 
     @Transactional
     public void execute(Long clubId, DecideClubCreationRequest request) {
-        if (!request.isOpen()) return;
         Club club = clubFacade.getClubById(clubId);
         User user = userRepository.findById(club.getClubApplicant().getId())
                 .orElseThrow(() -> UserNotFoundException.EXCEPTION);
 
-        club.clubOpen();
-        user.approvedClub(club);
+        Alarm alarm = alarmRepository.findByClubAndAlarmType(club, AlarmType.CREATE_CLUB)
+                .orElseThrow(() -> AlarmNotFoundException.EXCEPTION);
+
+        if (request.isOpen()) {
+            club.clubOpen();
+            user.approvedClub(club);
+            alarmRepository.delete(alarm);
+        } else {
+            alarmRepository.delete(alarm);
+        }
     }
 }
