f9df97db9281f25eaff081f6f07297fa1c997f81
coehgns <modooboiroo@gmail.com>
2025-11-13 11:19:12 +0900

    feat (#39) :: DissolveClubService에 동아리 소속 여부 검증 예외처리 추가

--- src/main/java/team/jeonghokim/daedongyeojido/domain/club/service/DissolveClubService.java
+++ src/main/java/team/jeonghokim/daedongyeojido/domain/club/service/DissolveClubService.java
@@ -7,8 +7,11 @@ import team.jeonghokim.daedongyeojido.domain.alarm.domain.Alarm;
 import team.jeonghokim.daedongyeojido.domain.alarm.domain.enums.AlarmType;
 import team.jeonghokim.daedongyeojido.domain.alarm.domain.repository.AlarmRepository;
 import team.jeonghokim.daedongyeojido.domain.club.domain.Club;
+import team.jeonghokim.daedongyeojido.domain.club.exception.UserNotInClubException;
 import team.jeonghokim.daedongyeojido.domain.user.domain.User;
 import team.jeonghokim.daedongyeojido.domain.user.facade.UserFacade;
+
+import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
@@ -20,7 +23,9 @@ public class DissolveClubService {
     @Transactional
     public void execute() {
         User receiver = userFacade.getCurrentUser();
-        Club club = receiver.getClub();
+
+        Club club = Optional.ofNullable(receiver.getClub())
+                .orElseThrow(() -> UserNotInClubException.EXCEPTION);
 
         // sms 알림 기능 로직 추가 해야 함.
 
