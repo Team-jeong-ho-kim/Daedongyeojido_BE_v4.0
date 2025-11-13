c5fcb22f817385fb4c8c78bd2f29ef8929fbe777
coehgns <modooboiroo@gmail.com>
2025-11-13 11:28:57 +0900

    chore (#39) :: AlarmRepository의 deleteAllByClub 메서드 삭제

--- src/main/java/team/jeonghokim/daedongyeojido/domain/alarm/domain/repository/AlarmRepository.java
+++ src/main/java/team/jeonghokim/daedongyeojido/domain/alarm/domain/repository/AlarmRepository.java
@@ -2,9 +2,7 @@ package team.jeonghokim.daedongyeojido.domain.alarm.domain.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import team.jeonghokim.daedongyeojido.domain.alarm.domain.Alarm;
-import team.jeonghokim.daedongyeojido.domain.club.domain.Club;
 
 public interface AlarmRepository extends JpaRepository<Alarm, Long> {
 
-    void deleteAllByClub(Club club);
 }
