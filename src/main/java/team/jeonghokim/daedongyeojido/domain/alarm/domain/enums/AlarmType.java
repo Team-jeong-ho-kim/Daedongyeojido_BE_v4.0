d8777161d0ad3967425a6f6405ec068b373d5641
coehgns <modooboiroo@gmail.com>
2025-11-13 11:18:24 +0900

    feat (#39) :: AlarmType에 CREATE_CLUB 추가

--- src/main/java/team/jeonghokim/daedongyeojido/domain/alarm/domain/enums/AlarmType.java
+++ src/main/java/team/jeonghokim/daedongyeojido/domain/alarm/domain/enums/AlarmType.java
@@ -9,6 +9,7 @@ import lombok.Getter;
 @JsonFormat(shape = JsonFormat.Shape.OBJECT)
 public enum AlarmType {
 
+    CREATE_CLUB("동아리 개설 신청", "%s의 동아리 개설 신청이 접수되었습니다"),
     DISSOLVE_CLUB("동아리 해체 신청", "%s의 동아리 해체 신청이 접수되었습니다.");
 
     private final String title;
