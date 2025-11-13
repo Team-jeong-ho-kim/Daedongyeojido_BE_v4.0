8af9c84ef17d88964912c4504a8c5a718067a2af
coehgns <modooboiroo@gmail.com>
2025-11-13 10:13:52 +0900

    feat (#39) :: UserRepository에 findAllByClub 추가

--- src/main/java/team/jeonghokim/daedongyeojido/domain/user/domain/repository/UserRepository.java
+++ src/main/java/team/jeonghokim/daedongyeojido/domain/user/domain/repository/UserRepository.java
@@ -1,8 +1,10 @@
 package team.jeonghokim.daedongyeojido.domain.user.domain.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import team.jeonghokim.daedongyeojido.domain.club.domain.Club;
 import team.jeonghokim.daedongyeojido.domain.user.domain.User;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User, Long> {
@@ -10,4 +12,6 @@ public interface UserRepository extends JpaRepository<User, Long> {
     Optional<User> findByAccountId(String accountId);
 
     Optional<User> findByUserNameAndClassNumber(String userName, String classNumber);
+
+    List<User> findAllByClub(Club club);
 }
