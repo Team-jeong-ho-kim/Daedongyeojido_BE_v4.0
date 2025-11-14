f21cd9ed8db4529b4a0f00279e7d3ac56f402501
coehgns <modooboiroo@gmail.com>
2025-11-14 11:36:58 +0900

    fix (#47) :: conflict

--- src/main/java/team/jeonghokim/daedongyeojido/global/config/SecurityConfig.java
+++ src/main/java/team/jeonghokim/daedongyeojido/global/config/SecurityConfig.java
@@ -53,7 +53,6 @@ public class SecurityConfig {
                         .requestMatchers(HttpMethod.POST, "/club/dissolution").hasRole(CLUB_LEADER)
                         .requestMatchers(HttpMethod.PATCH,"/club/**").hasAnyRole(CLUB_MEMBER)
                         .requestMatchers(HttpMethod.POST,"/application/**").hasAnyRole(CLUB_LEADER, CLUB_MEMBER)
-                        .requestMatchers(HttpMethod.DELETE, "/club/member/**").hasRole(CLUB_LEADER)
                         .anyRequest().authenticated()
                 )
                 .with(new SecurityFilterConfig(jwtTokenProvider, objectMapper), Customizer.withDefaults())
