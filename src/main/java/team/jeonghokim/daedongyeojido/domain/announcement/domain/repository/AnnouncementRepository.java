fba5b8d08f5cbb7cbbaf017bbb916f81558d1a33
coehgns <modooboiroo@gmail.com>
2025-11-15 20:57:45 +0900

    feat (#51) :: AnnouncementRepository

---
+++ src/main/java/team/jeonghokim/daedongyeojido/domain/announcement/domain/repository/AnnouncementRepository.java
@@ -0,0 +1,7 @@
+package team.jeonghokim.daedongyeojido.domain.announcement.domain.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import team.jeonghokim.daedongyeojido.domain.announcement.domain.Announcement;
+
+public interface AnnouncementRepository extends JpaRepository<Announcement, Long> {
+}
