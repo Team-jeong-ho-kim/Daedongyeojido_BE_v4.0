21a93ca09dadb23ada94d03235f6b81be60fb343
박태수 <taesupark0504@gmail.com>
2025-11-15 21:07:26 +0900

    chore (#46) :: import도 제거

--- src/main/java/team/jeonghokim/daedongyeojido/domain/application/service/QueryApplicationFormDetailService.java
+++ src/main/java/team/jeonghokim/daedongyeojido/domain/application/service/QueryApplicationFormDetailService.java
@@ -7,12 +7,10 @@ import team.jeonghokim.daedongyeojido.domain.application.domain.ApplicationForm;
 import team.jeonghokim.daedongyeojido.domain.application.domain.repository.ApplicationFormRepository;
 import team.jeonghokim.daedongyeojido.domain.application.exception.ApplicationFormNotFoundException;
 import team.jeonghokim.daedongyeojido.domain.application.presentation.dto.response.ApplicationFormResponse;
-import team.jeonghokim.daedongyeojido.domain.user.facade.UserFacade;
 
 @Service
 @RequiredArgsConstructor
 public class QueryApplicationFormDetailService {
-    private final UserFacade userFacade;
     private final ApplicationFormRepository applicationFormRepository;
 
     @Transactional(readOnly = true)
