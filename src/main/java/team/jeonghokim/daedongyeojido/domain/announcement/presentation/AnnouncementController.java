d6c07613bd6fc65e201698bad124ea3b06a37d7e
coehgns <modooboiroo@gmail.com>
2025-11-15 22:14:01 +0900

    feat (#51) :: AnnouncementController createAnnouncement 메서드에 @ResponseStatus 추가

--- src/main/java/team/jeonghokim/daedongyeojido/domain/announcement/presentation/AnnouncementController.java
+++ src/main/java/team/jeonghokim/daedongyeojido/domain/announcement/presentation/AnnouncementController.java
@@ -2,9 +2,11 @@ package team.jeonghokim.daedongyeojido.domain.announcement.presentation;
 
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 import team.jeonghokim.daedongyeojido.domain.announcement.presentation.dto.request.CreateAnnouncementRequest;
 import team.jeonghokim.daedongyeojido.domain.announcement.service.CreateAnnouncementService;
@@ -17,6 +19,7 @@ public class AnnouncementController {
     private final CreateAnnouncementService createAnnouncementService;
 
     @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
     public void createAnnouncement(@RequestBody @Valid CreateAnnouncementRequest request) {
         createAnnouncementService.execute(request);
     }
