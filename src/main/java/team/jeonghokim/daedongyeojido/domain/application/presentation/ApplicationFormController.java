c1d1577074e02368a641d3af661f11f081506f1a
박태수 <taesupark0504@gmail.com>
2025-11-15 20:55:18 +0900

    feat (#46) :: queryApplicationFormDetail api

--- src/main/java/team/jeonghokim/daedongyeojido/domain/application/presentation/ApplicationFormController.java
+++ src/main/java/team/jeonghokim/daedongyeojido/domain/application/presentation/ApplicationFormController.java
@@ -5,17 +5,26 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 import team.jeonghokim.daedongyeojido.domain.application.presentation.dto.request.ApplicationFormRequest;
+import team.jeonghokim.daedongyeojido.domain.application.presentation.dto.response.ApplicationFormResponse;
 import team.jeonghokim.daedongyeojido.domain.application.service.CreateApplicationFormService;
+import team.jeonghokim.daedongyeojido.domain.application.service.QueryApplicationFormDetailService;
 
 @RestController
 @RequestMapping("/application")
 @RequiredArgsConstructor
 public class ApplicationFormController {
     private final CreateApplicationFormService createApplicationFormService;
+    private final QueryApplicationFormDetailService queryApplicationFormDetailService;
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
     public void createApplicationForm(@RequestBody @Valid ApplicationFormRequest request) {
         createApplicationFormService.execute(request);
+    }
+
+    @GetMapping("/{club-id}")
+    @ResponseStatus(HttpStatus.OK)
+    public ApplicationFormResponse queryApplicationForm(@PathVariable("club-id") Long clubId) {
+        return queryApplicationFormDetailService.execute(clubId);
     }
 }
