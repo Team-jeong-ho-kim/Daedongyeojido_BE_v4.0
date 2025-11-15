b352f69226d60a5af03ff1a3345b4a59fe9bc073
박태수 <taesupark0504@gmail.com>
2025-11-15 20:50:25 +0900

    feat (#46) :: ApplicationFormResponse

--- src/main/java/team/jeonghokim/daedongyeojido/domain/application/presentation/dto/response/ApplicationFormResponse.java
+++ src/main/java/team/jeonghokim/daedongyeojido/domain/application/presentation/dto/response/ApplicationFormResponse.java
@@ -1,9 +1,21 @@
 package team.jeonghokim.daedongyeojido.domain.application.presentation.dto.response;
 
+import lombok.Builder;
+import team.jeonghokim.daedongyeojido.domain.application.domain.ApplicationForm;
+import team.jeonghokim.daedongyeojido.domain.application.domain.ApplicationQuestion;
+
+import java.time.LocalDate;
 import java.util.List;
 
+@Builder
 public record ApplicationFormResponse(
         List<String> content,
-        String submissionDuration
+        LocalDate submissionDuration
 ) {
+    public static ApplicationFormResponse of(ApplicationForm applicationForm) {
+        return new ApplicationFormResponse(
+                applicationForm.getApplicationQuestions().stream().map(ApplicationQuestion::getContent).toList(),
+                applicationForm.getSubmissionDuration()
+        );
+    }
 }
