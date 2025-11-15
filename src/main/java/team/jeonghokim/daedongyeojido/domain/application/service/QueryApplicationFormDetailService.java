339f0a415f64e5f39a745d70cef22f472c1fb61d
박태수 <taesupark0504@gmail.com>
2025-11-15 20:50:50 +0900

    feat (#46) :: QueryApplicationFormDetailService

---
+++ src/main/java/team/jeonghokim/daedongyeojido/domain/application/service/QueryApplicationFormDetailService.java
@@ -0,0 +1,28 @@
+package team.jeonghokim.daedongyeojido.domain.application.service;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import team.jeonghokim.daedongyeojido.domain.application.domain.ApplicationForm;
+import team.jeonghokim.daedongyeojido.domain.application.domain.repository.ApplicationFormRepository;
+import team.jeonghokim.daedongyeojido.domain.application.exception.ApplicationFormNotFoundException;
+import team.jeonghokim.daedongyeojido.domain.application.presentation.dto.response.ApplicationFormResponse;
+import team.jeonghokim.daedongyeojido.domain.user.domain.User;
+import team.jeonghokim.daedongyeojido.domain.user.facade.UserFacade;
+
+@Service
+@RequiredArgsConstructor
+public class QueryApplicationFormDetailService {
+    private final UserFacade userFacade;
+    private final ApplicationFormRepository applicationFormRepository;
+
+    @Transactional(readOnly = true)
+    public ApplicationFormResponse execute() {
+        User user = userFacade.getCurrentUser();
+
+        ApplicationForm applicationForm = applicationFormRepository.findByClub(user.getClub())
+                .orElseThrow(() -> ApplicationFormNotFoundException.EXCEPTION);
+
+        return ApplicationFormResponse.of(applicationForm);
+    }
+}
