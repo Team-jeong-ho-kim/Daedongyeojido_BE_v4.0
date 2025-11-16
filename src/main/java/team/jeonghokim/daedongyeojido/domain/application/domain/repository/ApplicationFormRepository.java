91a7de89c022547135870af9d5a3218a3bf2919f
박태수 <taesupark0504@gmail.com>
2025-11-16 21:18:43 +0900

    chore (#56) :: findAllApplicationFormListResponse삭제

--- src/main/java/team/jeonghokim/daedongyeojido/domain/application/domain/repository/ApplicationFormRepository.java
+++ src/main/java/team/jeonghokim/daedongyeojido/domain/application/domain/repository/ApplicationFormRepository.java
@@ -2,13 +2,9 @@ package team.jeonghokim.daedongyeojido.domain.application.domain.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import team.jeonghokim.daedongyeojido.domain.application.domain.ApplicationForm;
-import team.jeonghokim.daedongyeojido.domain.application.presentation.dto.response.ApplicationFormListResponse;
 
-import java.util.List;
 import java.util.Optional;
 
 public interface ApplicationFormRepository extends JpaRepository<ApplicationForm, Long> {
     Optional<ApplicationForm> findByClubId(Long clubId);
-
-    List<ApplicationFormListResponse> findAllApplicationFormListResponse();
 }
