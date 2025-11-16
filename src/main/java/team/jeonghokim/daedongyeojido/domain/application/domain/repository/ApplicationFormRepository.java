7b4eacfbc959e116cac8f0587b05caf8f83aa1e7
박태수 <taesupark0504@gmail.com>
2025-11-16 21:56:36 +0900

    refactor (#56) :: findByApplicationFormId 삭제

--- src/main/java/team/jeonghokim/daedongyeojido/domain/application/domain/repository/ApplicationFormRepository.java
+++ src/main/java/team/jeonghokim/daedongyeojido/domain/application/domain/repository/ApplicationFormRepository.java
@@ -4,10 +4,8 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import team.jeonghokim.daedongyeojido.domain.application.domain.ApplicationForm;
 
 import java.util.List;
-import java.util.Optional;
 
 public interface ApplicationFormRepository extends JpaRepository<ApplicationForm, Long> {
-    Optional<ApplicationForm> findByApplicationFormId(Long applicationFormId);
 
     List<ApplicationForm> findAllByClubId(Long clubId);
 }
