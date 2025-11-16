94772e86fc54700be483bf920225a425e08721be
박태수 <taesupark0504@gmail.com>
2025-11-17 08:46:12 +0900

    chore (#56) :: delete findAllByClubId

--- src/main/java/team/jeonghokim/daedongyeojido/domain/application/domain/repository/ApplicationFormRepository.java
+++ src/main/java/team/jeonghokim/daedongyeojido/domain/application/domain/repository/ApplicationFormRepository.java
@@ -3,9 +3,5 @@ package team.jeonghokim.daedongyeojido.domain.application.domain.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import team.jeonghokim.daedongyeojido.domain.application.domain.ApplicationForm;
 
-import java.util.List;
-
 public interface ApplicationFormRepository extends JpaRepository<ApplicationForm, Long>, ApplicationFormRepositoryCustom {
-
-    List<ApplicationForm> findAllByClubId(Long clubId);
 }
