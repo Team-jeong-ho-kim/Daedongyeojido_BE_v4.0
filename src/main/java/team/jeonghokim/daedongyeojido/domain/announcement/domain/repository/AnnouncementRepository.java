9e988921252fce963a60025bef73ff183b2aae25
coehgns <modooboiroo@gmail.com>
2025-11-16 20:13:43 +0900

    feat (#54) :: AnnouncementRepository에 AnnouncementCustomRepository를 상속하도록 설정

--- src/main/java/team/jeonghokim/daedongyeojido/domain/announcement/domain/repository/AnnouncementRepository.java
+++ src/main/java/team/jeonghokim/daedongyeojido/domain/announcement/domain/repository/AnnouncementRepository.java
@@ -3,5 +3,5 @@ package team.jeonghokim.daedongyeojido.domain.announcement.domain.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import team.jeonghokim.daedongyeojido.domain.announcement.domain.Announcement;
 
-public interface AnnouncementRepository extends JpaRepository<Announcement, Long> {
+public interface AnnouncementRepository extends JpaRepository<Announcement, Long>, AnnouncementCustomRepository {
 }
