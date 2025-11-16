595496484082c89acf2ad04e826322d83e768d31
coehgns <modooboiroo@gmail.com>
2025-11-16 21:41:13 +0900

    feat (#54) :: QueryAnnouncementListResponse에 @builder 추가

--- src/main/java/team/jeonghokim/daedongyeojido/domain/announcement/presentation/dto/response/QueryAnnouncementListResponse.java
+++ src/main/java/team/jeonghokim/daedongyeojido/domain/announcement/presentation/dto/response/QueryAnnouncementListResponse.java
@@ -1,6 +1,9 @@
 package team.jeonghokim.daedongyeojido.domain.announcement.presentation.dto.response;
 
+import lombok.Builder;
+
 import java.util.List;
 
+@Builder
 public record QueryAnnouncementListResponse(List<AnnouncementResponse> announcements) {
 }
