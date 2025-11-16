dc4020d8aa53966473126a2d8bdcac04580d79a6
박태수 <taesupark0504@gmail.com>
2025-11-16 21:29:06 +0900

    feat (#56) :: applicationFormTitle listResponse에 추가

--- src/main/java/team/jeonghokim/daedongyeojido/domain/application/presentation/dto/response/ApplicationFormListResponse.java
+++ src/main/java/team/jeonghokim/daedongyeojido/domain/application/presentation/dto/response/ApplicationFormListResponse.java
@@ -5,12 +5,14 @@ import team.jeonghokim.daedongyeojido.domain.application.domain.ApplicationForm;
 import java.time.LocalDate;
 
 public record ApplicationFormListResponse(
+        String applicationFormTitle,
         String clubName,
         String clubImage,
         LocalDate submissionDuration
 ) {
     public static ApplicationFormListResponse of(ApplicationForm applicationForm) {
         return new ApplicationFormListResponse(
+                applicationForm.getApplicationFormTitle(),
                 applicationForm.getClub().getClubName(),
                 applicationForm.getClub().getClubImage(),
                 applicationForm.getSubmissionDuration()
