0a546f506008b2dda2d88c4e87a6c25f3ebfa6ae
coehgns <modooboiroo@gmail.com>
2025-11-16 21:11:41 +0900

    chore (#54) :: QueryAnnouncementListResponse의 매개변수 타입 수정

--- src/main/java/team/jeonghokim/daedongyeojido/domain/announcement/presentation/dto/response/QueryAnnouncementListResponse.java
+++ src/main/java/team/jeonghokim/daedongyeojido/domain/announcement/presentation/dto/response/QueryAnnouncementListResponse.java
@@ -1,8 +1,6 @@
 package team.jeonghokim.daedongyeojido.domain.announcement.presentation.dto.response;
 
-import team.jeonghokim.daedongyeojido.domain.announcement.domain.repository.vo.AnnouncementVO;
-
 import java.util.List;
 
-public record QueryAnnouncementListResponse(List<AnnouncementVO> announcements) {
+public record QueryAnnouncementListResponse(List<AnnouncementResponse> announcements) {
 }
