7f5e76c549d12ed93fb2192861d7a39a77b22d53
박태수 <taesupark0504@gmail.com>
2025-11-16 21:20:51 +0900

    feat (#56) :: findAllByClubId

--- src/main/java/team/jeonghokim/daedongyeojido/domain/application/domain/repository/ApplicationFormRepository.java
+++ src/main/java/team/jeonghokim/daedongyeojido/domain/application/domain/repository/ApplicationFormRepository.java
@@ -3,8 +3,11 @@ package team.jeonghokim.daedongyeojido.domain.application.domain.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import team.jeonghokim.daedongyeojido.domain.application.domain.ApplicationForm;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface ApplicationFormRepository extends JpaRepository<ApplicationForm, Long> {
     Optional<ApplicationForm> findByClubId(Long clubId);
+
+    List<ApplicationForm> findAllByClubId(Long clubId);
 }
