30061425dd51383b676ec0627aa2b0aca37a23e4
coehgns <modooboiroo@gmail.com>
2025-11-16 20:13:04 +0900

    feat (#54) :: QueryAnnouncementVO

---
+++ src/main/java/team/jeonghokim/daedongyeojido/domain/announcement/domain/repository/vo/QueryAnnouncementVO.java
@@ -0,0 +1,15 @@
+package team.jeonghokim.daedongyeojido.domain.announcement.domain.repository.vo;
+
+import com.querydsl.core.annotations.QueryProjection;
+import lombok.Getter;
+
+import java.time.LocalDate;
+
+@Getter
+public class QueryAnnouncementVO extends AnnouncementVO {
+
+    @QueryProjection
+    public QueryAnnouncementVO(Long announcementId, String title, String clubName, LocalDate deadLine, String clubImage) {
+        super(announcementId, title, clubName, deadLine, clubImage);
+    }
+}
