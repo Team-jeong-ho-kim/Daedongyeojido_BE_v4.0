f9c99df18f888183c2e271fa6fabd19f3ad95eca
coehgns <modooboiroo@gmail.com>
2025-11-16 20:54:32 +0900

    chore (#54) :: ClubCustomRepositoryImpl Q클래스 import 하도록 수정

--- src/main/java/team/jeonghokim/daedongyeojido/domain/club/domain/repository/ClubCustomRepositoryImpl.java
+++ src/main/java/team/jeonghokim/daedongyeojido/domain/club/domain/repository/ClubCustomRepositoryImpl.java
@@ -4,18 +4,17 @@ import com.querydsl.core.group.GroupBy;
 import com.querydsl.core.types.Projections;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import lombok.RequiredArgsConstructor;
-import team.jeonghokim.daedongyeojido.domain.club.domain.QClub;
-import team.jeonghokim.daedongyeojido.domain.club.domain.QClubMajor;
 import team.jeonghokim.daedongyeojido.domain.club.domain.repository.vo.ClubVO;
 
 import java.util.List;
+
+import static team.jeonghokim.daedongyeojido.domain.club.domain.QClub.club;
+import static team.jeonghokim.daedongyeojido.domain.club.domain.QClubMajor.clubMajor;
 
 @RequiredArgsConstructor
 public class ClubCustomRepositoryImpl implements ClubCustomRepository {
 
     private final JPAQueryFactory jpaQueryFactory;
-    private final QClub club = QClub.club;
-    private final QClubMajor clubMajor = QClubMajor.clubMajor;
 
     @Override
     public List<ClubVO> findAllByIsOpenIsTrue() {
