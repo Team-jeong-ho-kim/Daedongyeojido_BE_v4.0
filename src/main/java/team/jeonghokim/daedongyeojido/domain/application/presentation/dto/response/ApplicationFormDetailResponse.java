a40449c4cd6ebe219f9964cd27457651e18eeed3
박태수 <taesupark0504@gmail.com>
2025-11-16 21:29:14 +0900

    feat (#56) :: applicationFormTitle detailResponse에 추가

--- src/main/java/team/jeonghokim/daedongyeojido/domain/application/presentation/dto/response/ApplicationFormDetailResponse.java
+++ src/main/java/team/jeonghokim/daedongyeojido/domain/application/presentation/dto/response/ApplicationFormDetailResponse.java
@@ -7,11 +7,13 @@ import java.time.LocalDate;
 import java.util.List;
 
 public record ApplicationFormDetailResponse(
+        String applicationFormTitle,
         List<String> content,
         LocalDate submissionDuration
 ) {
     public static ApplicationFormDetailResponse of(ApplicationForm applicationForm) {
         return new ApplicationFormDetailResponse(
+                applicationForm.getApplicationFormTitle(),
                 applicationForm.getApplicationQuestions().stream().map(ApplicationQuestion::getContent).toList(),
                 applicationForm.getSubmissionDuration()
         );
