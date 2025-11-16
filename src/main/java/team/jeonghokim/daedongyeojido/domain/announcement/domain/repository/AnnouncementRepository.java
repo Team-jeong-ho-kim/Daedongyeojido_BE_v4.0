5983355294bf68016501036e52ec5f2feca58bbc
coehgns <modooboiroo@gmail.com>
2025-11-16 21:40:59 +0900

    chore (#54) :: AnnouncementRepository findAllAnnouncements 메서드 삭제

--- src/main/java/team/jeonghokim/daedongyeojido/domain/announcement/domain/repository/AnnouncementRepository.java
+++ src/main/java/team/jeonghokim/daedongyeojido/domain/announcement/domain/repository/AnnouncementRepository.java
@@ -1,23 +1,8 @@
 package team.jeonghokim.daedongyeojido.domain.announcement.domain.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 import team.jeonghokim.daedongyeojido.domain.announcement.domain.Announcement;
-import team.jeonghokim.daedongyeojido.domain.announcement.presentation.dto.response.AnnouncementResponse;
-
-import java.util.List;
 
 public interface AnnouncementRepository extends JpaRepository<Announcement, Long> {
 
-    @Query("""
-        SELECT new team.jeonghokim.daedongyeojido.domain.announcement.presentation.dto.response.AnnouncementResponse(
-            announcement.id,
-            announcement.title,
-            announcement.club.clubName,
-            announcement.deadline,
-            announcement.club.clubImage
-        )
-        FROM Announcement announcement
-        """)
-    List<AnnouncementResponse> findAllAnnouncements();
 }
