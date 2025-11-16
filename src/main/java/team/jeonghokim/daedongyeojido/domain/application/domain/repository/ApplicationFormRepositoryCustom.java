6aff60f77db30849e50a023c37bbbf8cdd634f53
박태수 <taesupark0504@gmail.com>
2025-11-17 08:46:50 +0900

    feat (#56) :: findAllByClubId queryDSL 구현

--- src/main/java/team/jeonghokim/daedongyeojido/domain/application/domain/repository/ApplicationFormRepositoryCustom.java
+++ src/main/java/team/jeonghokim/daedongyeojido/domain/application/domain/repository/ApplicationFormRepositoryCustom.java
@@ -1,4 +1,10 @@
 package team.jeonghokim.daedongyeojido.domain.application.domain.repository;
 
+import team.jeonghokim.daedongyeojido.domain.application.presentation.dto.response.ApplicationFormListResponse;
+
+import java.util.List;
+
 public interface ApplicationFormRepositoryCustom {
+
+    List<ApplicationFormListResponse> findAllByClubId(Long clubId);
 }
--- src/main/java/team/jeonghokim/daedongyeojido/domain/application/domain/repository/ApplicationFormRepositoryCustomImpl.java
+++ src/main/java/team/jeonghokim/daedongyeojido/domain/application/domain/repository/ApplicationFormRepositoryCustomImpl.java
@@ -1,7 +1,32 @@
 package team.jeonghokim.daedongyeojido.domain.application.domain.repository;
 
+import com.querydsl.jpa.impl.JPAQueryFactory;
 import lombok.RequiredArgsConstructor;
+import team.jeonghokim.daedongyeojido.domain.application.domain.QApplicationForm;
+import team.jeonghokim.daedongyeojido.domain.application.presentation.dto.response.ApplicationFormListResponse;
+import team.jeonghokim.daedongyeojido.domain.application.presentation.dto.response.QApplicationFormListResponse;
+import team.jeonghokim.daedongyeojido.domain.club.domain.QClub;
+
+import java.util.List;
 
 @RequiredArgsConstructor
 public class ApplicationFormRepositoryCustomImpl implements ApplicationFormRepositoryCustom {
+    private final JPAQueryFactory queryFactory;
+    private final QApplicationForm applicationForm =  QApplicationForm.applicationForm;
+    private final QClub club =  QClub.club;
+
+    @Override
+    public List<ApplicationFormListResponse> findAllByClubId(Long clubId) {
+        return queryFactory.select(new QApplicationFormListResponse(
+                        applicationForm.id,
+                        applicationForm.applicationFormTitle,
+                        club.clubName,
+                        club.clubImage,
+                        applicationForm.submissionDuration
+                ))
+                .from(applicationForm)
+                .join(applicationForm.club, club)
+                .where(club.id.eq(clubId))
+                .fetch();
+    }
 }
