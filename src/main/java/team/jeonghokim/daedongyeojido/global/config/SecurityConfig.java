68e356c2e079874a285657eb1eb52a8331c0a33d
박태수 <taesupark0504@gmail.com>
2025-11-17 16:53:15 +0900

    feat (#58) :: 지원서 폼 수정 권한 설정

--- src/main/java/team/jeonghokim/daedongyeojido/global/config/SecurityConfig.java
+++ src/main/java/team/jeonghokim/daedongyeojido/global/config/SecurityConfig.java
@@ -54,6 +54,7 @@ public class SecurityConfig {
                         .requestMatchers(HttpMethod.PATCH,"/club/**").hasAnyRole(CLUB_MEMBER)
                         .requestMatchers(HttpMethod.POST,"/application/**").hasAnyRole(CLUB_LEADER, CLUB_MEMBER)
                         .requestMatchers(HttpMethod.GET,"/application/**").permitAll()
+                        .requestMatchers(HttpMethod.PATCH,"/application/**").hasAnyRole(CLUB_MEMBER, CLUB_LEADER)
                         .requestMatchers(HttpMethod.DELETE, "/club/member/**").hasRole(CLUB_LEADER)
                         .requestMatchers(HttpMethod.POST, "/announcement").hasAnyRole(CLUB_LEADER, CLUB_MEMBER)
                         .requestMatchers(HttpMethod.GET, "/announcement/**").permitAll()
