f256a67b3a167d64bfd3a0d87bf7321ad619fc64
coehgns <modooboiroo@gmail.com>
2025-11-17 19:21:06 +0900

    feat (#65) :: DeleteAnnouncementService에 AnnouncementFacade 적용

--- src/main/java/team/jeonghokim/daedongyeojido/domain/announcement/service/DeleteAnnouncementService.java
+++ src/main/java/team/jeonghokim/daedongyeojido/domain/announcement/service/DeleteAnnouncementService.java
@@ -5,19 +5,18 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import team.jeonghokim.daedongyeojido.domain.announcement.domain.Announcement;
 import team.jeonghokim.daedongyeojido.domain.announcement.domain.repository.AnnouncementRepository;
-import team.jeonghokim.daedongyeojido.domain.announcement.exception.AnnouncementNotFoundException;
+import team.jeonghokim.daedongyeojido.domain.announcement.facade.AnnouncementFacade;
 
 @Service
 @RequiredArgsConstructor
 public class DeleteAnnouncementService {
 
     private final AnnouncementRepository announcementRepository;
+    private final AnnouncementFacade announcementFacade;
 
     @Transactional
     public void execute(Long announcementId) {
-        Announcement announcement = announcementRepository.findById(announcementId)
-                .orElseThrow(() -> AnnouncementNotFoundException.EXCEPTION);
-
+        Announcement announcement = announcementFacade.getAnnouncementById(announcementId);
         announcementRepository.delete(announcement);
     }
 }
