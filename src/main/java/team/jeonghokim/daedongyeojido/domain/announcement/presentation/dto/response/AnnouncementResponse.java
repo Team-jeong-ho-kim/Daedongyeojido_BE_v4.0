5040a68276515b0e02e3179d9ac021a0da478004
coehgns <modooboiroo@gmail.com>
2025-11-17 12:30:22 +0900

    feat (#54) :: AnnouncementResponse에 AnnouncementResponse @QueryProjection 추가

--- src/main/java/team/jeonghokim/daedongyeojido/domain/announcement/presentation/dto/response/AnnouncementResponse.java
+++ src/main/java/team/jeonghokim/daedongyeojido/domain/announcement/presentation/dto/response/AnnouncementResponse.java
@@ -1,7 +1,17 @@
 package team.jeonghokim.daedongyeojido.domain.announcement.presentation.dto.response;
 
+import com.querydsl.core.annotations.QueryProjection;
+
 import java.time.LocalDate;
 
 public record AnnouncementResponse(Long announcementId, String title, String clubName, LocalDate deadline, String clubImage) {
 
+    @QueryProjection
+    public AnnouncementResponse(Long announcementId, String title, String clubName, LocalDate deadline, String clubImage) {
+        this.announcementId = announcementId;
+        this.title = title;
+        this.clubName = clubName;
+        this.deadline = deadline;
+        this.clubImage = clubImage;
+    }
 }
