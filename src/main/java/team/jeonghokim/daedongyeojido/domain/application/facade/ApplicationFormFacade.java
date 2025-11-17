e08009bdeb70e959001e4c380201c184fa48fa5b
박태수 <taesupark0504@gmail.com>
2025-11-17 19:47:30 +0900

    refactor (#63) :: applicationFormFacade 검증 로직 추가

--- src/main/java/team/jeonghokim/daedongyeojido/domain/application/facade/ApplicationFormFacade.java
+++ src/main/java/team/jeonghokim/daedongyeojido/domain/application/facade/ApplicationFormFacade.java
@@ -4,15 +4,31 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 import team.jeonghokim.daedongyeojido.domain.application.domain.ApplicationForm;
 import team.jeonghokim.daedongyeojido.domain.application.domain.repository.ApplicationFormRepository;
+import team.jeonghokim.daedongyeojido.domain.application.exception.ApplicationFormAccessDeniedException;
 import team.jeonghokim.daedongyeojido.domain.application.exception.ApplicationFormNotFoundException;
+import team.jeonghokim.daedongyeojido.domain.club.domain.Club;
+import team.jeonghokim.daedongyeojido.domain.club.facade.ClubFacade;
+import team.jeonghokim.daedongyeojido.domain.user.domain.User;
+import team.jeonghokim.daedongyeojido.domain.user.facade.UserFacade;
 
 @Component
 @RequiredArgsConstructor
 public class ApplicationFormFacade {
     private final ApplicationFormRepository applicationFormRepository;
+    private final ClubFacade clubFacade;
+    private final UserFacade userFacade;
 
     public ApplicationForm getApplicationById(Long applicationId) {
-        return applicationFormRepository.findById(applicationId)
+
+        ApplicationForm applicationForm = applicationFormRepository.findById(applicationId)
                 .orElseThrow(() -> ApplicationFormNotFoundException.EXCEPTION);
+
+        User user = userFacade.getCurrentUser();
+
+        if (!applicationForm.getClub().getId().equals(user.getClub().getId())) {
+            throw ApplicationFormAccessDeniedException.EXCEPTION;
+        }
+
+        return applicationForm;
     }
 }
