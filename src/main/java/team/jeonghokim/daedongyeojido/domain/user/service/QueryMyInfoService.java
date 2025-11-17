48b0ab40ab33f7897894e73b196b7e292707f753
박태수 <taesupark0504@gmail.com>
2025-11-17 09:11:50 +0900

    refactor (#56) :: queryMyInfo

--- src/main/java/team/jeonghokim/daedongyeojido/domain/user/service/QueryMyInfoService.java
+++ src/main/java/team/jeonghokim/daedongyeojido/domain/user/service/QueryMyInfoService.java
@@ -4,13 +4,9 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import team.jeonghokim.daedongyeojido.domain.user.domain.User;
-import team.jeonghokim.daedongyeojido.domain.user.domain.UserLink;
-import team.jeonghokim.daedongyeojido.domain.user.domain.UserMajor;
 import team.jeonghokim.daedongyeojido.domain.user.domain.repository.UserRepository;
 import team.jeonghokim.daedongyeojido.domain.user.facade.UserFacade;
 import team.jeonghokim.daedongyeojido.domain.user.presentation.dto.response.MyInfoResponse;
-
-import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
@@ -22,14 +18,6 @@ public class QueryMyInfoService {
     public MyInfoResponse execute() {
         User user = userFacade.getCurrentUser();
 
-        return MyInfoResponse.builder()
-                .userName(user.getUserName())
-                .classNumber(user.getClassNumber())
-                .introduction(user.getIntroduction())
-                .club(Optional.ofNullable(user.getClub()).map(club -> club.getClubName()).orElse(null))
-                .major(user.getMajors().stream().map(UserMajor::getMajor).toList())
-                .link(user.getLinks().stream().map(UserLink::getLink).toList())
-                .profileImage(user.getProfileImage())
-                .build();
+        return MyInfoResponse.of(user);
     }
 }
