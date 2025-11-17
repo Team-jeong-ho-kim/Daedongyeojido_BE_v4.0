12fe765c3a0a0b3aea59f1ce719f3f55d5d083b7
박태수 <taesupark0504@gmail.com>
2025-11-17 17:20:31 +0900

    refactor (#58) :: updateApplicationFormService

--- src/main/java/team/jeonghokim/daedongyeojido/domain/application/service/UpdateApplicationFormService.java
+++ src/main/java/team/jeonghokim/daedongyeojido/domain/application/service/UpdateApplicationFormService.java
@@ -4,14 +4,9 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import team.jeonghokim.daedongyeojido.domain.application.domain.ApplicationForm;
-import team.jeonghokim.daedongyeojido.domain.application.domain.ApplicationQuestion;
 import team.jeonghokim.daedongyeojido.domain.application.domain.repository.ApplicationFormRepository;
 import team.jeonghokim.daedongyeojido.domain.application.exception.ApplicationFormNotFoundException;
 import team.jeonghokim.daedongyeojido.domain.application.presentation.dto.request.ApplicationFormRequest;
-
-import java.util.List;
-import java.util.Optional;
-import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
@@ -26,19 +21,8 @@ public class UpdateApplicationFormService {
 
         applicationForm.update(
                 request.getApplicationFormTitle(),
-                updateApplicationQuestion(request, applicationForm),
+                request.getContent(),
                 request.getSubmissionDuration()
         );
-    }
-
-    private List<ApplicationQuestion> updateApplicationQuestion(ApplicationFormRequest request, ApplicationForm applicationForm) {
-        return Optional.ofNullable(request.getContent())
-                .orElseGet(List::of)
-                .stream()
-                .map(applicationQuestion -> ApplicationQuestion.builder()
-                        .applicationForm(applicationForm)
-                        .content(applicationQuestion)
-                        .build())
-                .collect(Collectors.toList());
     }
 }
