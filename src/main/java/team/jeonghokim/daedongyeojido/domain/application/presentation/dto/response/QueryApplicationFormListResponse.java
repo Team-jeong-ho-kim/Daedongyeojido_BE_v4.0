febec0570d8578dd5c60bcab0d66b86e875e0f6a
박태수 <taesupark0504@gmail.com>
2025-11-17 14:44:38 +0900

    feat (#56) :: queryApplicationFormListResponse

---
+++ src/main/java/team/jeonghokim/daedongyeojido/domain/application/presentation/dto/response/QueryApplicationFormListResponse.java
@@ -0,0 +1,9 @@
+package team.jeonghokim.daedongyeojido.domain.application.presentation.dto.response;
+
+import lombok.Builder;
+
+import java.util.List;
+
+@Builder
+public record QueryApplicationFormListResponse(List<ApplicationFormListResponse> listResponses) {
+}
