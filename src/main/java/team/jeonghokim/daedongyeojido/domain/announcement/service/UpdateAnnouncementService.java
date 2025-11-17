c4351ddb5df6c6dbf12ab12231adedbc82c3f489
coehgns <modooboiroo@gmail.com>
2025-11-17 19:21:16 +0900

    feat (#65) :: UpdateAnnouncementService에 AnnouncementFacade 적용

--- src/main/java/team/jeonghokim/daedongyeojido/domain/announcement/service/UpdateAnnouncementService.java
+++ src/main/java/team/jeonghokim/daedongyeojido/domain/announcement/service/UpdateAnnouncementService.java
@@ -4,9 +4,8 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import team.jeonghokim.daedongyeojido.domain.announcement.domain.Announcement;
-import team.jeonghokim.daedongyeojido.domain.announcement.domain.repository.AnnouncementRepository;
 import team.jeonghokim.daedongyeojido.domain.announcement.exception.AnnouncementAccessDeniedException;
-import team.jeonghokim.daedongyeojido.domain.announcement.exception.AnnouncementNotFoundException;
+import team.jeonghokim.daedongyeojido.domain.announcement.facade.AnnouncementFacade;
 import team.jeonghokim.daedongyeojido.domain.announcement.presentation.dto.request.AnnouncementRequest;
 import team.jeonghokim.daedongyeojido.domain.user.domain.User;
 import team.jeonghokim.daedongyeojido.domain.user.facade.UserFacade;
@@ -15,14 +14,13 @@ import team.jeonghokim.daedongyeojido.domain.user.facade.UserFacade;
 @RequiredArgsConstructor
 public class UpdateAnnouncementService {
 
-    private final AnnouncementRepository announcementRepository;
     private final UserFacade userFacade;
+    private final AnnouncementFacade announcementFacade;
 
     @Transactional
     public void execute(Long announcementId, AnnouncementRequest request) {
         User currentUser = userFacade.getCurrentUser();
-        Announcement announcement = announcementRepository.findById(announcementId)
-                .orElseThrow(() -> AnnouncementNotFoundException.EXCEPTION);
+        Announcement announcement = announcementFacade.getAnnouncementById(announcementId);
 
         if (!announcement.getClub().equals(currentUser.getClub())) {
             throw AnnouncementAccessDeniedException.EXCEPTION;
