9ee9bfd99b99f45f89140808b4dab4d3387ecf33
박태수 <taesupark0504@gmail.com>
2025-11-17 19:31:15 +0900

    refactor (#63) :: queryDetail ApplicationFormFacade 적용

--- src/main/java/team/jeonghokim/daedongyeojido/domain/application/service/QueryApplicationFormDetailService.java
+++ src/main/java/team/jeonghokim/daedongyeojido/domain/application/service/QueryApplicationFormDetailService.java
@@ -5,19 +5,19 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import team.jeonghokim.daedongyeojido.domain.application.domain.ApplicationForm;
 import team.jeonghokim.daedongyeojido.domain.application.domain.repository.ApplicationFormRepository;
-import team.jeonghokim.daedongyeojido.domain.application.exception.ApplicationFormNotFoundException;
+import team.jeonghokim.daedongyeojido.domain.application.facade.ApplicationFormFacade;
 import team.jeonghokim.daedongyeojido.domain.application.presentation.dto.response.ApplicationFormDetailResponse;
 
 @Service
 @RequiredArgsConstructor
 public class QueryApplicationFormDetailService {
     private final ApplicationFormRepository applicationFormRepository;
+    private final ApplicationFormFacade applicationFormFacade;
 
     @Transactional(readOnly = true)
     public ApplicationFormDetailResponse execute(Long applicationFormId) {
 
-        ApplicationForm applicationForm = applicationFormRepository.findById(applicationFormId)
-                .orElseThrow(() -> ApplicationFormNotFoundException.EXCEPTION);
+        ApplicationForm applicationForm = applicationFormFacade.getApplicationById(applicationFormId);
 
         return ApplicationFormDetailResponse.of(applicationForm);
     }
