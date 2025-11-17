430e5e8e80ec71f018c1904c994fe278539cdea2
박태수 <taesupark0504@gmail.com>
2025-11-17 17:14:57 +0900

    refactor (#58) :: user entity

--- src/main/java/team/jeonghokim/daedongyeojido/domain/user/domain/User.java
+++ src/main/java/team/jeonghokim/daedongyeojido/domain/user/domain/User.java
@@ -3,6 +3,7 @@ package team.jeonghokim.daedongyeojido.domain.user.domain;
 import jakarta.persistence.*;
 import lombok.*;
 import team.jeonghokim.daedongyeojido.domain.club.domain.Club;
+import team.jeonghokim.daedongyeojido.domain.user.domain.enums.Major;
 import team.jeonghokim.daedongyeojido.domain.user.domain.enums.Role;
 import team.jeonghokim.daedongyeojido.global.entity.BaseIdEntity;
 
@@ -67,13 +68,31 @@ public class User extends BaseIdEntity {
         this.classNumber = classNumber;
     }
 
-    public void update(String introduction, List<UserMajor> majors, List<UserLink> links, String profileImage) {
+    public void update(String introduction, List<Major> majors, List<String> links, String profileImage) {
         this.introduction = introduction;
         this.majors.clear();
-        this.majors.addAll(majors);
+        majors.stream()
+                .map(major -> UserMajor.builder()
+                        .major(major)
+                        .build())
+                .forEach(this::addUserMajor);
         this.links.clear();
-        this.links.addAll(links);
+        links.stream()
+                .map(link -> UserLink.builder()
+                        .link(link)
+                        .build())
+                .forEach(this::addUserLink);
         this.profileImage = profileImage;
+    }
+
+    private void addUserMajor(UserMajor userMajor) {
+        userMajor.setUser(this);
+        this.majors.add(userMajor);
+    }
+
+    private void addUserLink(UserLink userLink) {
+        userLink.setUser(this);
+        this.links.add(userLink);
     }
 
     public void approvedClub(Club club) {
