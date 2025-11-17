f0f09245db2471b4514c3a0f938a29764905b0ec
coehgns <modooboiroo@gmail.com>
2025-11-17 17:01:38 +0900

    feat (#61) :: UpdateAnnouncementService에 동아리 공고 접근 권한 예외 처리 로직 추가

--- src/main/java/team/jeonghokim/daedongyeojido/domain/announcement/service/UpdateAnnouncementService.java
+++ src/main/java/team/jeonghokim/daedongyeojido/domain/announcement/service/UpdateAnnouncementService.java
@@ -5,19 +5,28 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import team.jeonghokim.daedongyeojido.domain.announcement.domain.Announcement;
 import team.jeonghokim.daedongyeojido.domain.announcement.domain.repository.AnnouncementRepository;
+import team.jeonghokim.daedongyeojido.domain.announcement.exception.AnnouncementAccessDeniedException;
 import team.jeonghokim.daedongyeojido.domain.announcement.exception.AnnouncementNotFoundException;
 import team.jeonghokim.daedongyeojido.domain.announcement.presentation.dto.request.UpdateAnnouncementRequest;
+import team.jeonghokim.daedongyeojido.domain.user.domain.User;
+import team.jeonghokim.daedongyeojido.domain.user.facade.UserFacade;
 
 @Service
 @RequiredArgsConstructor
 public class UpdateAnnouncementService {
 
     private final AnnouncementRepository announcementRepository;
+    private final UserFacade userFacade;
 
     @Transactional
     public void execute(Long announcementId, UpdateAnnouncementRequest request) {
+        User currenetUser = userFacade.getCurrentUser();
         Announcement announcement = announcementRepository.findById(announcementId)
                 .orElseThrow(() -> AnnouncementNotFoundException.EXCEPTION);
+
+        if (!announcement.getClub().equals(currenetUser.getClub())) {
+            throw AnnouncementAccessDeniedException.EXCEPTION;
+        }
 
         announcement.updateAnnouncement(request);
     }
