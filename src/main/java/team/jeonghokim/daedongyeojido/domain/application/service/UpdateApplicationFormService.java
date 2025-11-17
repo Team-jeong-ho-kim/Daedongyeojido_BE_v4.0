5ae59dbf74bc321dc633fc64054fdfb2e67847de
박태수 <taesupark0504@gmail.com>
2025-11-17 19:31:07 +0900

    refactor (#63) :: update ApplicationFormFacade 적용

--- src/main/java/team/jeonghokim/daedongyeojido/domain/application/service/UpdateApplicationFormService.java
+++ src/main/java/team/jeonghokim/daedongyeojido/domain/application/service/UpdateApplicationFormService.java
@@ -6,18 +6,19 @@ import org.springframework.transaction.annotation.Transactional;
 import team.jeonghokim.daedongyeojido.domain.application.domain.ApplicationForm;
 import team.jeonghokim.daedongyeojido.domain.application.domain.repository.ApplicationFormRepository;
 import team.jeonghokim.daedongyeojido.domain.application.exception.ApplicationFormNotFoundException;
+import team.jeonghokim.daedongyeojido.domain.application.facade.ApplicationFormFacade;
 import team.jeonghokim.daedongyeojido.domain.application.presentation.dto.request.ApplicationFormRequest;
 
 @Service
 @RequiredArgsConstructor
 public class UpdateApplicationFormService {
     private final ApplicationFormRepository applicationFormRepository;
+    private final ApplicationFormFacade applicationFormFacade;
 
     @Transactional
     public void execute(Long applicationFormId, ApplicationFormRequest request) {
 
-        ApplicationForm applicationForm = applicationFormRepository.findById(applicationFormId)
-                .orElseThrow(() -> ApplicationFormNotFoundException.EXCEPTION);
+        ApplicationForm applicationForm = applicationFormFacade.getApplicationById(applicationFormId);
 
         applicationForm.update(
                 request.getApplicationFormTitle(),
