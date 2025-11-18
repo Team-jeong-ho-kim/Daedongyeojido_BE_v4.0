2c064f55bdee0e52aee8a4119df59d77f588692a
박태수 <taesupark0504@gmail.com>
2025-11-19 08:31:38 +0900

    feat (#68) :: submissionRequest

---
+++ src/main/java/team/jeonghokim/daedongyeojido/domain/submission/presentation/dto/request/SubmissionRequest.java
@@ -0,0 +1,30 @@
+package team.jeonghokim.daedongyeojido.domain.submission.presentation.dto.request;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Size;
+import lombok.Getter;
+import team.jeonghokim.daedongyeojido.domain.user.domain.enums.Major;
+
+import java.util.List;
+
+@Getter
+public class SubmissionRequest {
+
+    @NotBlank(message = "이름을 입력해주세요")
+    @Size(max = 4, message = "이름은 최대 4자까지 작성할 수 있습니다.")
+    private String userName;
+
+    @NotBlank(message = "학번을 입력해주세요.")
+    @Size(max = 4, message = "학번은 최대 4자까지 작성할 수 있습니다.")
+    private String classNumber;
+
+    @Size(max = 300, message = "자기소개는 최대 300자까지 작성할 수 있습니다.")
+    private String introduction;
+
+    @NotEmpty(message = "최소 하나 이상의 전공을 선택해주세요.")
+    private List<Major> major;
+
+    private List<@Size(max = 200, message = "답변은 200자까지 작성할 수 있습니다.")
+            String> answer;
+}
