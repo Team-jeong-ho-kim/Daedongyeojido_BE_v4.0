b284d50b26602cc9cfc5b9f7007529d907b62d78
박태수 <taesupark0504@gmail.com>
2025-11-19 08:18:21 +0900

    feat (#68) :: applicationAnswerRepository

---
+++ src/main/java/team/jeonghokim/daedongyeojido/domain/application/domain/repository/ApplicationAnswerRepository.java
@@ -0,0 +1,7 @@
+package team.jeonghokim.daedongyeojido.domain.application.domain.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import team.jeonghokim.daedongyeojido.domain.application.domain.ApplicationAnswer;
+
+public interface ApplicationAnswerRepository extends JpaRepository<ApplicationAnswer, Long> {
+}
