09049f59e33c9ffa920c4e839b6a3551007d4038
박태수 <taesupark0504@gmail.com>
2025-11-19 20:02:37 +0900

    feat (#68) :: AnswerRequest @JsonProperty 적용

--- src/main/java/team/jeonghokim/daedongyeojido/domain/application/presentation/dto/request/AnswerRequest.java
+++ src/main/java/team/jeonghokim/daedongyeojido/domain/application/presentation/dto/request/AnswerRequest.java
@@ -1,5 +1,6 @@
 package team.jeonghokim.daedongyeojido.domain.application.presentation.dto.request;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 import lombok.Getter;
@@ -8,7 +9,8 @@ import lombok.Getter;
 public class AnswerRequest {
 
     @NotNull(message = "존재하지 않는 질문입니다.")
-    private Long applicationFormQuestionId;
+    @JsonProperty("applicationQuestionId")
+    private Long applicationQuestionId;
 
     @Size(max = 200, message = "답은 최대 200자까지 작성할 수 있습니다.")
     private String answer;
