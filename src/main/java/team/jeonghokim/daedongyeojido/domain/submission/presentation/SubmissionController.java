c63a3e1275dbd7cb3cad222f929de27d13b2a6b9
박태수 <taesupark0504@gmail.com>
2025-11-19 19:05:19 +0900

    feat (#68) :: SubmissionController

---
+++ src/main/java/team/jeonghokim/daedongyeojido/domain/submission/presentation/SubmissionController.java
@@ -0,0 +1,14 @@
+package team.jeonghokim.daedongyeojido.domain.submission.presentation;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+import team.jeonghokim.daedongyeojido.domain.submission.presentation.dto.request.SubmissionRequest;
+import team.jeonghokim.daedongyeojido.domain.submission.service.CreateSubmissionService;
+
+@RestController
+@RequestMapping("/submission")
+@RequiredArgsConstructor
+public class SubmissionController {
+
+}
