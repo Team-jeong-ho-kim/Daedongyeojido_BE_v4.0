4ae86e14a41ebdcb54aa219f45e1c3cd75ebd146
박태수 <taesupark0504@gmail.com>
2025-11-19 19:00:39 +0900

    refactor (#68) :: questionId로 변경

--- src/main/java/team/jeonghokim/daedongyeojido/domain/application/presentation/dto/request/AnswerRequest.java
+++ src/main/java/team/jeonghokim/daedongyeojido/domain/application/presentation/dto/request/AnswerRequest.java
@@ -7,8 +7,8 @@ import lombok.Getter;
 @Getter
 public class AnswerRequest {
 
-    @NotNull(message = "지원서 폼 ID를 입력해주세요.")
-    private Long applicationFormId;
+    @NotNull(message = "존재하지 않는 질문입니다.")
+    private Long applicationFormQuestionId;
 
     @Size(max = 200, message = "답은 최대 200자까지 작성할 수 있습니다.")
     private String answer;
