e7be632108e0761937bfe57616f3ccca218e2101
박태수 <taesupark0504@gmail.com>
2025-11-19 20:17:16 +0900

    feat (#68) :: applicationMajor

---
+++ src/main/java/team/jeonghokim/daedongyeojido/domain/application/domain/ApplicationMajor.java
@@ -0,0 +1,33 @@
+package team.jeonghokim.daedongyeojido.domain.application.domain;
+
+import jakarta.persistence.*;
+import lombok.Builder;
+import lombok.NoArgsConstructor;
+import team.jeonghokim.daedongyeojido.domain.user.domain.enums.Major;
+import team.jeonghokim.daedongyeojido.global.entity.BaseIdEntity;
+
+@Entity
+@Table(name = "tbl_application_major")
+@NoArgsConstructor
+public class ApplicationMajor extends BaseIdEntity {
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "application_form_id", nullable = false)
+    private ApplicationForm applicationForm;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false, length = 10)
+    private Major major;
+
+    @Builder
+    public ApplicationMajor(Major major) {
+        this.major = major;
+    }
+
+    protected void setApplicationForm(ApplicationForm applicationForm) {
+        if (this.applicationForm != null) {
+            return;
+        }
+        this.applicationForm = applicationForm;
+    }
+}
