6d4cdfb3c682e455c09562acb022aaa0ba6b0240
박태수 <taesupark0504@gmail.com>
2025-11-19 20:03:50 +0900

    feat (#68) :: SubmissionRequest @JsonProperty 적용

--- src/main/java/team/jeonghokim/daedongyeojido/domain/submission/presentation/dto/request/SubmissionRequest.java
+++ src/main/java/team/jeonghokim/daedongyeojido/domain/submission/presentation/dto/request/SubmissionRequest.java
@@ -1,7 +1,8 @@
 package team.jeonghokim.daedongyeojido.domain.submission.presentation.dto.request;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 import lombok.Getter;
 import team.jeonghokim.daedongyeojido.domain.application.presentation.dto.request.AnswerRequest;
@@ -14,17 +15,19 @@ public class SubmissionRequest {
 
     @NotBlank(message = "이름을 입력해주세요")
     @Size(max = 4, message = "이름은 최대 4자까지 작성할 수 있습니다.")
+    @JsonProperty("userName")
     private String userName;
 
     @NotBlank(message = "학번을 입력해주세요.")
     @Size(max = 4, message = "학번은 최대 4자까지 작성할 수 있습니다.")
+    @JsonProperty("classNumber")
     private String classNumber;
 
     @Size(max = 300, message = "자기소개는 최대 300자까지 작성할 수 있습니다.")
     private String introduction;
 
-    @NotEmpty(message = "최소 하나 이상의 전공을 선택해주세요.")
-    private List<Major> major;
+    @NotNull(message = "최소 하나 이상의 전공을 선택해주세요.")
+    private Major major;
 
     private List<AnswerRequest> answer;
 }
