3bac6c49d30587764b0c05fa283770863674cd11
coehgns <modooboiroo@gmail.com>
2025-11-20 11:13:47 +0900

    refactor (#50) :: SubmissionRepositoryCustom findAllByApplicationFormWithValidStatuses -> findAllByApplicationFormIdWithValidStatuses

--- src/main/java/team/jeonghokim/daedongyeojido/domain/submission/domain/repository/SubmissionRepositoryCustom.java
+++ src/main/java/team/jeonghokim/daedongyeojido/domain/submission/domain/repository/SubmissionRepositoryCustom.java
@@ -1,11 +1,10 @@
 package team.jeonghokim.daedongyeojido.domain.submission.domain.repository;
 
-import team.jeonghokim.daedongyeojido.domain.application.domain.ApplicationForm;
 import team.jeonghokim.daedongyeojido.domain.submission.presentation.dto.response.ApplicantResponse;
 
 import java.util.List;
 
 public interface SubmissionRepositoryCustom {
 
-    List<ApplicantResponse> findAllByApplicationFormWithValidStatuses(ApplicationForm applicationForm);
+    List<ApplicantResponse> findAllByApplicationFormIdWithValidStatuses(Long applicationFormId);
 }
