ccce242a4511e2844a6ac8720498ca93c948d399
coehgns <modooboiroo@gmail.com>
2025-11-20 09:24:53 +0900

    feat (#50) :: ApplicationFormRepository의 findByClub 메서드의 반환형을 Optional로 래핑

--- src/main/java/team/jeonghokim/daedongyeojido/domain/application/domain/repository/ApplicationFormRepository.java
+++ src/main/java/team/jeonghokim/daedongyeojido/domain/application/domain/repository/ApplicationFormRepository.java
@@ -4,7 +4,9 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import team.jeonghokim.daedongyeojido.domain.application.domain.ApplicationForm;
 import team.jeonghokim.daedongyeojido.domain.club.domain.Club;
 
+import java.util.Optional;
+
 public interface ApplicationFormRepository extends JpaRepository<ApplicationForm, Long>, ApplicationFormRepositoryCustom {
 
-    ApplicationForm findByClub(Club club);
+    Optional<ApplicationForm> findByClub(Club club);
 }
