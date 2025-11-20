2b009059b5082c3b201a2674093f8fcd01b918df
coehgns <modooboiroo@gmail.com>
2025-11-20 09:35:55 +0900

    feat (#50) :: QuerySubmissionListService에 applicationform이 존재하지 않을 경우 예외 처리

--- src/main/java/team/jeonghokim/daedongyeojido/domain/submission/service/QuerySubmissionListService.java
+++ src/main/java/team/jeonghokim/daedongyeojido/domain/submission/service/QuerySubmissionListService.java
@@ -5,6 +5,7 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import team.jeonghokim.daedongyeojido.domain.application.domain.ApplicationForm;
 import team.jeonghokim.daedongyeojido.domain.application.domain.repository.ApplicationFormRepository;
+import team.jeonghokim.daedongyeojido.domain.application.exception.ApplicationFormNotFoundException;
 import team.jeonghokim.daedongyeojido.domain.submission.domain.repository.SubmissionRepository;
 import team.jeonghokim.daedongyeojido.domain.submission.presentation.dto.response.ApplicantResponse;
 import team.jeonghokim.daedongyeojido.domain.submission.presentation.dto.response.QuerySubmissionListResponse;
@@ -23,14 +24,12 @@ public class QuerySubmissionListService {
 
     @Transactional(readOnly = true)
     public QuerySubmissionListResponse execute() {
-        // 1. 조회하려는 유저의 동아리 정보를 가져오기
         User currentUser = userFacade.getCurrentUser();
+        ApplicationForm applicationForm = applicationFormRepository.findByClub(currentUser.getClub())
+                .orElseThrow(() -> ApplicationFormNotFoundException.EXCEPTION);
 
-        // 2. 유저의 동아리 정보를 통해서 ApplicationForm 가져오기
-        ApplicationForm applicationForm = applicationFormRepository.findByClub(currentUser.getClub());
-
-        // 3. ApplicationForm에 해당하는 Submission 엔티티 중 ApplicationStatus가 SUBMITTED, ACCEPTED, REJECTED 인 엔티티를 모두 가져와서 list로 반환하기
-        List<ApplicantResponse> applicantResponses = submissionRepository.findAllByApplicationFormWithValidStatuses(applicationForm);
+        List<ApplicantResponse> applicantResponses =
+                submissionRepository.findAllByApplicationFormWithValidStatuses(applicationForm);
 
         return new QuerySubmissionListResponse(applicantResponses);
     }
