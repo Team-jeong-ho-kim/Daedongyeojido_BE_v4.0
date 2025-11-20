451a1731af28f61e8fa96bd5d65270d0a5a9be4f
coehgns <modooboiroo@gmail.com>
2025-11-20 09:05:09 +0900

    feat (#50) :: SubmissionCustomRepositoryImpl

---
+++ src/main/java/team/jeonghokim/daedongyeojido/domain/submission/domain/repository/SubmissionCustomRepositoryImpl.java
@@ -0,0 +1,39 @@
+package team.jeonghokim.daedongyeojido.domain.submission.domain.repository;
+
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import lombok.RequiredArgsConstructor;
+import team.jeonghokim.daedongyeojido.domain.application.domain.ApplicationForm;
+import team.jeonghokim.daedongyeojido.domain.application.domain.enums.ApplicationStatus;
+import team.jeonghokim.daedongyeojido.domain.submission.presentation.dto.response.ApplicantResponse;
+import team.jeonghokim.daedongyeojido.domain.submission.presentation.dto.response.QApplicantResponse;
+
+import java.util.List;
+
+import static team.jeonghokim.daedongyeojido.domain.submission.domain.QSubmission.submission;
+
+@RequiredArgsConstructor
+public class SubmissionCustomRepositoryImpl implements SubmissionCustomRepository{
+
+    private final JPAQueryFactory jpaQueryFactory;
+
+    @Override
+    public List<ApplicantResponse> findAllByApplicationFormWithValidStatuses(ApplicationForm applicationForm) {
+        return jpaQueryFactory
+                .select(new QApplicantResponse(
+                        submission.id,
+                        submission.userName,
+                        submission.classNumber,
+                        submission.major
+                ))
+                .from(submission)
+                .where(
+                        submission.applicationForm.eq(applicationForm),
+                        submission.applicationStatus.in(
+                                ApplicationStatus.SUBMITTED,
+                                ApplicationStatus.ACCEPTED,
+                                ApplicationStatus.REJECTED
+                        )
+                )
+                .fetch();
+    }
+}
