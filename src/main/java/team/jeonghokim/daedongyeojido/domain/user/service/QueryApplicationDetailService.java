cfd77a24a4c68687ee6e820968a3269c910fb4da
박태수 <taesupark0504@gmail.com>
2025-11-20 10:05:11 +0900

    refactor (#70) :: 지원서 작성 유저만 조회 가능

--- src/main/java/team/jeonghokim/daedongyeojido/domain/user/service/QueryApplicationDetailService.java
+++ src/main/java/team/jeonghokim/daedongyeojido/domain/user/service/QueryApplicationDetailService.java
@@ -3,21 +3,30 @@ package team.jeonghokim.daedongyeojido.domain.user.service;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import team.jeonghokim.daedongyeojido.domain.application.exception.ApplicationAccessDeniedException;
 import team.jeonghokim.daedongyeojido.domain.application.exception.ApplicationNotFoundException;
 import team.jeonghokim.daedongyeojido.domain.submission.domain.Submission;
 import team.jeonghokim.daedongyeojido.domain.submission.domain.repository.SubmissionRepository;
+import team.jeonghokim.daedongyeojido.domain.user.domain.User;
+import team.jeonghokim.daedongyeojido.domain.user.facade.UserFacade;
 import team.jeonghokim.daedongyeojido.domain.user.presentation.dto.response.QueryApplicationDetailResponse;
 
 @Service
 @RequiredArgsConstructor
 public class QueryApplicationDetailService {
     private final SubmissionRepository submissionRepository;
+    private final UserFacade userFacade;
 
     @Transactional(readOnly = true)
     public QueryApplicationDetailResponse execute(Long submissionId) {
+        User user = userFacade.getCurrentUser();
 
         Submission submission = submissionRepository.findById(submissionId)
                 .orElseThrow(() -> ApplicationNotFoundException.EXCEPTION);
+
+        if (!submission.getUser().getId().equals(user.getId())) {
+            throw ApplicationAccessDeniedException.EXCEPTION;
+        }
 
         return QueryApplicationDetailResponse.of(submission);
     }
