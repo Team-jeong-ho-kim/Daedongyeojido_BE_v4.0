156894d198374cc7694e38bfa01ff953e2cd679e
박태수 <taesupark0504@gmail.com>
2025-11-20 09:51:53 +0900

    refactor (#70) :: applicationNotFound 적용

--- src/main/java/team/jeonghokim/daedongyeojido/domain/user/service/QueryApplicationDetailService.java
+++ src/main/java/team/jeonghokim/daedongyeojido/domain/user/service/QueryApplicationDetailService.java
@@ -3,9 +3,9 @@ package team.jeonghokim.daedongyeojido.domain.user.service;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import team.jeonghokim.daedongyeojido.domain.application.exception.ApplicationNotFoundException;
 import team.jeonghokim.daedongyeojido.domain.submission.domain.Submission;
 import team.jeonghokim.daedongyeojido.domain.submission.domain.repository.SubmissionRepository;
-import team.jeonghokim.daedongyeojido.domain.submission.exception.SubmissionNotFoundException;
 import team.jeonghokim.daedongyeojido.domain.user.presentation.dto.response.QueryApplicationDetailResponse;
 
 @Service
@@ -17,7 +17,7 @@ public class QueryApplicationDetailService {
     public QueryApplicationDetailResponse execute(Long submissionId) {
 
         Submission submission = submissionRepository.findById(submissionId)
-                .orElseThrow(() -> SubmissionNotFoundException.EXCEPTION);
+                .orElseThrow(() -> ApplicationNotFoundException.EXCEPTION);
 
         return QueryApplicationDetailResponse.of(submission);
     }
