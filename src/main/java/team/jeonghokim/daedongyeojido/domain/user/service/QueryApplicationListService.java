301f4f32f47eb90645d28966a99934aca58fe46e
박태수 <taesupark0504@gmail.com>
2025-11-20 12:14:22 +0900

    feat (#73) :: queryApplicationListService

---
+++ src/main/java/team/jeonghokim/daedongyeojido/domain/user/service/QueryApplicationListService.java
@@ -0,0 +1,28 @@
+package team.jeonghokim.daedongyeojido.domain.user.service;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import team.jeonghokim.daedongyeojido.domain.application.presentation.dto.response.ApplicationListResponse;
+import team.jeonghokim.daedongyeojido.domain.submission.domain.repository.SubmissionRepository;
+import team.jeonghokim.daedongyeojido.domain.user.domain.User;
+import team.jeonghokim.daedongyeojido.domain.user.facade.UserFacade;
+import team.jeonghokim.daedongyeojido.domain.user.presentation.dto.response.QueryApplicationListResponse;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class QueryApplicationListService {
+    private final SubmissionRepository submissionRepository;
+    private final UserFacade userFacade;
+
+    @Transactional(readOnly = true)
+    public QueryApplicationListResponse execute() {
+        User user = userFacade.getCurrentUser();
+
+        List<ApplicationListResponse> applicationListResponses = submissionRepository.findAllByUserId(user.getId());
+
+        return new QueryApplicationListResponse(applicationListResponses);
+    }
+}
