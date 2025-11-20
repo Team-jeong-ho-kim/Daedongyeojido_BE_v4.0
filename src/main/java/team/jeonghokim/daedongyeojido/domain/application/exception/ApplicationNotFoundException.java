6b14f530610699faf2b39ccb8399aa73465c284e
박태수 <taesupark0504@gmail.com>
2025-11-20 09:51:24 +0900

    feat (#70) :: applicationNotFoundException

---
+++ src/main/java/team/jeonghokim/daedongyeojido/domain/application/exception/ApplicationNotFoundException.java
@@ -0,0 +1,12 @@
+package team.jeonghokim.daedongyeojido.domain.application.exception;
+
+import team.jeonghokim.daedongyeojido.global.error.exception.DaedongException;
+import team.jeonghokim.daedongyeojido.global.error.exception.ErrorCode;
+
+public class ApplicationNotFoundException extends DaedongException {
+    public static final DaedongException EXCEPTION = new ApplicationNotFoundException();
+
+    private ApplicationNotFoundException() {
+        super(ErrorCode.APPLICATION_FORM_NOT_FOUND);
+    }
+}
