6da54f5d5ea01d36c4c5ebc1bd0250bff1ef9137
박태수 <taesupark0504@gmail.com>
2025-11-20 09:38:30 +0900

    feat (#70) :: from static method

--- src/main/java/team/jeonghokim/daedongyeojido/domain/application/presentation/dto/response/ApplicationQuestionAndAnswerResponse.java
+++ src/main/java/team/jeonghokim/daedongyeojido/domain/application/presentation/dto/response/ApplicationQuestionAndAnswerResponse.java
@@ -1,9 +1,44 @@
 package team.jeonghokim.daedongyeojido.domain.application.presentation.dto.response;
 
+import team.jeonghokim.daedongyeojido.domain.application.domain.ApplicationAnswer;
+import team.jeonghokim.daedongyeojido.domain.application.domain.ApplicationQuestion;
+import team.jeonghokim.daedongyeojido.domain.submission.domain.Submission;
+
+import java.util.List;
+import java.util.Optional;
+
 public record ApplicationQuestionAndAnswerResponse(
         Long applicationQuestionId,
         String question,
         Long applicationAnswerId,
         String answer
 ) {
+    public static List<ApplicationQuestionAndAnswerResponse> from(Submission submission) {
+        List<ApplicationQuestion> questions = submission.getApplicationForm().getApplicationQuestions();
+        List<ApplicationAnswer> answers = submission.getApplicationAnswers();
+
+        return questions.stream()
+                .map(question -> {
+                    ApplicationAnswer applicationAnswer = answers.stream()
+                            .filter(a -> a.getApplicationQuestion().getId().equals(question.getId()))
+                            .findFirst()
+                            .orElse(null);
+
+                    Long applicationAnswerId = Optional.ofNullable(applicationAnswer)
+                            .map(ApplicationAnswer::getId)
+                            .orElse(null);
+
+                    String answer = Optional.ofNullable(applicationAnswer)
+                            .map(ApplicationAnswer::getContent)
+                            .orElse(null);
+
+                    return new ApplicationQuestionAndAnswerResponse(
+                            question.getId(),
+                            question.getContent(),
+                            applicationAnswerId,
+                            answer
+                    );
+                })
+                .toList();
+    }
 }
