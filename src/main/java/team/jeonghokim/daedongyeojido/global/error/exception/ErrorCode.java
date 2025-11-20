02204e15f791bf0c6ab17c630228e8ffea085806
박태수 <taesupark0504@gmail.com>
2025-11-20 09:50:49 +0900

    chore (#70) :: 지원서 폼으로 명시

--- src/main/java/team/jeonghokim/daedongyeojido/global/error/exception/ErrorCode.java
+++ src/main/java/team/jeonghokim/daedongyeojido/global/error/exception/ErrorCode.java
@@ -23,8 +23,8 @@ public enum ErrorCode {
     USER_APPLICATION_NOT_FOUND(404, "팀원 신청 기록을 찾을 수 없습니다."),
 
     //application
-    APPLICATION_FORM_NOT_FOUND(404, "지원서를 찾을 수 없습니다."),
-    APPLICATION_FORM_ACCESS_DENIED(403, "해당 지원서에 대한 수정 권한이 없습니다."),
+    APPLICATION_FORM_NOT_FOUND(404, "지원서 폼을 찾을 수 없습니다."),
+    APPLICATION_FORM_ACCESS_DENIED(403, "해당 지원서 폼에 대한 수정 권한이 없습니다."),
     INVALID_APPLICATION_QUESTION(400, "유효하지 않은 질문입니다."),
 
     //s3
