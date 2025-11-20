3c6907e68e8f40db89149bb4b39fe1702d6e889c
박태수 <taesupark0504@gmail.com>
2025-11-20 09:01:45 +0900

    chore (#70) :: response로 rename

--- src/main/java/team/jeonghokim/daedongyeojido/domain/application/presentation/dto/response/ApplicationQuestionAndAnswerResponse.java
+++ src/main/java/team/jeonghokim/daedongyeojido/domain/application/presentation/dto/response/ApplicationQuestionAndAnswerResponse.java
@@ -1,6 +1,6 @@
 package team.jeonghokim.daedongyeojido.domain.application.presentation.dto.response;
 
-public record ApplicationQuestionAndAnswerRequest(
+public record ApplicationQuestionAndAnswerResponse(
         Long applicationQuestionId,
         String question,
         Long applicationAnswerId,
