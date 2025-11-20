548b0d99c348edfaf517e537a1c2d5ea88357634
coehgns <modooboiroo@gmail.com>
2025-11-20 09:05:26 +0900

    feat (#50) :: ApplicationFormRepository에 findByClub 메서드 추가

--- src/main/java/team/jeonghokim/daedongyeojido/domain/application/domain/repository/ApplicationFormRepository.java
+++ src/main/java/team/jeonghokim/daedongyeojido/domain/application/domain/repository/ApplicationFormRepository.java
@@ -2,6 +2,9 @@ package team.jeonghokim.daedongyeojido.domain.application.domain.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import team.jeonghokim.daedongyeojido.domain.application.domain.ApplicationForm;
+import team.jeonghokim.daedongyeojido.domain.club.domain.Club;
 
 public interface ApplicationFormRepository extends JpaRepository<ApplicationForm, Long>, ApplicationFormRepositoryCustom {
+
+    ApplicationForm findByClub(Club club);
 }
