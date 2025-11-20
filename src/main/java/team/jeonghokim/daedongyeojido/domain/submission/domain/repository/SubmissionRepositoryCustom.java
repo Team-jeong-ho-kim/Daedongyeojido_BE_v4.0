620fc8d33c8d44908476bcdb7f73bdd86b44f60b
박태수 <taesupark0504@gmail.com>
2025-11-20 12:06:43 +0900

    feat (#73) :: findAllByUserId

--- src/main/java/team/jeonghokim/daedongyeojido/domain/submission/domain/repository/SubmissionRepositoryCustom.java
+++ src/main/java/team/jeonghokim/daedongyeojido/domain/submission/domain/repository/SubmissionRepositoryCustom.java
@@ -1,5 +1,6 @@
 package team.jeonghokim.daedongyeojido.domain.submission.domain.repository;
 
+import team.jeonghokim.daedongyeojido.domain.application.presentation.dto.response.ApplicationListResponse;
 import team.jeonghokim.daedongyeojido.domain.submission.presentation.dto.response.ApplicantResponse;
 
 import java.util.List;
@@ -7,4 +8,6 @@ import java.util.List;
 public interface SubmissionRepositoryCustom {
 
     List<ApplicantResponse> findAllByApplicationFormIdWithValidStatuses(Long applicationFormId);
+
+    List<ApplicationListResponse> findAllByUserId(Long userId);
 }
