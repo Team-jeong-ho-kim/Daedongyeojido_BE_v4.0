4d0cb36205eea89fa1b5edcb51c3db2278f6488e
박태수 <taesupark0504@gmail.com>
2025-11-21 13:44:59 +0900

    refactor (#78) :: deleteApplication SubmissionFacade 적용

--- src/main/java/team/jeonghokim/daedongyeojido/domain/user/service/DeleteApplicationService.java
+++ src/main/java/team/jeonghokim/daedongyeojido/domain/user/service/DeleteApplicationService.java
@@ -5,25 +5,25 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import team.jeonghokim.daedongyeojido.domain.application.domain.enums.ApplicationStatus;
 import team.jeonghokim.daedongyeojido.domain.application.exception.ApplicationAccessDeniedException;
-import team.jeonghokim.daedongyeojido.domain.application.exception.ApplicationNotFoundException;
 import team.jeonghokim.daedongyeojido.domain.application.exception.CannotDeleteApplicationException;
 import team.jeonghokim.daedongyeojido.domain.submission.domain.Submission;
 import team.jeonghokim.daedongyeojido.domain.submission.domain.repository.SubmissionRepository;
+import team.jeonghokim.daedongyeojido.domain.submission.facade.SubmissionFacade;
 import team.jeonghokim.daedongyeojido.domain.user.domain.User;
 import team.jeonghokim.daedongyeojido.domain.user.facade.UserFacade;
 
 @Service
 @RequiredArgsConstructor
 public class DeleteApplicationService {
-    private final SubmissionRepository submissionRepository;
     private final UserFacade userFacade;
+    private final SubmissionFacade  submissionFacade;
+    private final SubmissionRepository submissionRepository;
 
     @Transactional
     public void execute(Long submissionId) {
         User user = userFacade.getCurrentUser();
 
-        Submission submission = submissionRepository.findById(submissionId)
-                .orElseThrow(() -> ApplicationNotFoundException.EXCEPTION);
+        Submission submission = submissionFacade.getApplicationBySubmissionId(submissionId);
 
         if (!user.getId().equals(submission.getUser().getId())) {
             throw ApplicationAccessDeniedException.EXCEPTION;
