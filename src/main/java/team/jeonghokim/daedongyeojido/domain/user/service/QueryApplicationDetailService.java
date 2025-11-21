4712f145500da2cd2c317edd5ce9c4d6de178c28
박태수 <taesupark0504@gmail.com>
2025-11-21 13:44:52 +0900

    refactor (#78) :: queryApplication SubmissionFacade 적용

--- src/main/java/team/jeonghokim/daedongyeojido/domain/user/service/QueryApplicationDetailService.java
+++ src/main/java/team/jeonghokim/daedongyeojido/domain/user/service/QueryApplicationDetailService.java
@@ -4,9 +4,8 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import team.jeonghokim.daedongyeojido.domain.application.exception.ApplicationAccessDeniedException;
-import team.jeonghokim.daedongyeojido.domain.application.exception.ApplicationNotFoundException;
 import team.jeonghokim.daedongyeojido.domain.submission.domain.Submission;
-import team.jeonghokim.daedongyeojido.domain.submission.domain.repository.SubmissionRepository;
+import team.jeonghokim.daedongyeojido.domain.submission.facade.SubmissionFacade;
 import team.jeonghokim.daedongyeojido.domain.user.domain.User;
 import team.jeonghokim.daedongyeojido.domain.user.facade.UserFacade;
 import team.jeonghokim.daedongyeojido.domain.user.presentation.dto.response.QueryApplicationDetailResponse;
@@ -14,15 +13,14 @@ import team.jeonghokim.daedongyeojido.domain.user.presentation.dto.response.Quer
 @Service
 @RequiredArgsConstructor
 public class QueryApplicationDetailService {
-    private final SubmissionRepository submissionRepository;
     private final UserFacade userFacade;
+    private final SubmissionFacade submissionFacade;
 
     @Transactional(readOnly = true)
     public QueryApplicationDetailResponse execute(Long submissionId) {
         User user = userFacade.getCurrentUser();
 
-        Submission submission = submissionRepository.findById(submissionId)
-                .orElseThrow(() -> ApplicationNotFoundException.EXCEPTION);
+        Submission submission = submissionFacade.getApplicationBySubmissionId(submissionId);
 
         if (!submission.getUser().getId().equals(user.getId())) {
             throw ApplicationAccessDeniedException.EXCEPTION;
