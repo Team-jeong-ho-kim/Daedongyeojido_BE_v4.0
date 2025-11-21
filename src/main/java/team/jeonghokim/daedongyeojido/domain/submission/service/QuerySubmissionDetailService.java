d4ac3f9e7a3b8507cb4791fc022d420c367d092c
coehgns <modooboiroo@gmail.com>
2025-11-21 14:02:03 +0900

    feat (#75) :: QuerySubmissionDetailService의 submission 예외 처리 수정 및 지원서 제출 여부 예외 처리 로직 추가

--- src/main/java/team/jeonghokim/daedongyeojido/domain/submission/service/QuerySubmissionDetailService.java
+++ src/main/java/team/jeonghokim/daedongyeojido/domain/submission/service/QuerySubmissionDetailService.java
@@ -3,9 +3,10 @@ package team.jeonghokim.daedongyeojido.domain.submission.service;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import team.jeonghokim.daedongyeojido.domain.application.exception.ApplicationNotFoundException;
+import team.jeonghokim.daedongyeojido.domain.application.exception.ApplicationNotSubmittedException;
 import team.jeonghokim.daedongyeojido.domain.submission.domain.Submission;
 import team.jeonghokim.daedongyeojido.domain.submission.domain.repository.SubmissionRepository;
-import team.jeonghokim.daedongyeojido.domain.submission.exception.SubmissionNotFoundException;
 import team.jeonghokim.daedongyeojido.domain.submission.presentation.dto.response.QuerySubmissionDetailResponse;
 
 @Service
@@ -17,7 +18,11 @@ public class QuerySubmissionDetailService {
     @Transactional(readOnly = true)
     public QuerySubmissionDetailResponse execute(Long submissionId) {
         Submission submission = submissionRepository.findById(submissionId)
-                .orElseThrow(() -> SubmissionNotFoundException.EXCEPTION);
+                .orElseThrow(() -> ApplicationNotFoundException.EXCEPTION);
+
+        if (!(submission.isSubmitted())) {
+            throw ApplicationNotSubmittedException.EXCEPTION;
+        }
 
         return QuerySubmissionDetailResponse.from(submission);
     }
