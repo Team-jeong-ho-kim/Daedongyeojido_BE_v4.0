3d40f0629021fb58364b5b9d6550db3de0674d55
coehgns <modooboiroo@gmail.com>
2025-11-21 13:45:57 +0900

    feat (#75) :: QuerySubmissionDetailResponse

---
+++ src/main/java/team/jeonghokim/daedongyeojido/domain/submission/presentation/dto/response/QuerySubmissionDetailResponse.java
@@ -0,0 +1,30 @@
+package team.jeonghokim.daedongyeojido.domain.submission.presentation.dto.response;
+
+import team.jeonghokim.daedongyeojido.domain.submission.domain.Submission;
+import team.jeonghokim.daedongyeojido.domain.user.domain.enums.Major;
+
+import java.util.List;
+
+public record QuerySubmissionDetailResponse(
+        String userName,
+        String classNumber,
+        String introduction,
+        Major major,
+        List<SubmissionDto> answers
+) {
+
+    public static QuerySubmissionDetailResponse from(Submission submission) {
+        return new QuerySubmissionDetailResponse(
+                submission.getUserName(),
+                submission.getClassNumber(),
+                submission.getIntroduction(),
+                submission.getMajor(),
+                submission.getApplicationAnswers().stream().map(applicationAnswer ->
+                        new SubmissionDto(
+                                applicationAnswer.getApplicationQuestion().getId(),
+                                applicationAnswer.getContent()
+                        ))
+                        .toList()
+        );
+    }
+}
