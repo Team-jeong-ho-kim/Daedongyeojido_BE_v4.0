10a74f5eb40b9293edd6c0cf64b95fa63d226247
박태수 <taesupark0504@gmail.com>
2025-11-21 13:50:06 +0900

    chore (#78) :: @Component로 변경

--- src/main/java/team/jeonghokim/daedongyeojido/domain/submission/facade/SubmissionFacade.java
+++ src/main/java/team/jeonghokim/daedongyeojido/domain/submission/facade/SubmissionFacade.java
@@ -1,12 +1,12 @@
 package team.jeonghokim.daedongyeojido.domain.submission.facade;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.context.annotation.Configuration;
+import org.springframework.stereotype.Component;
 import team.jeonghokim.daedongyeojido.domain.application.exception.ApplicationNotFoundException;
 import team.jeonghokim.daedongyeojido.domain.submission.domain.Submission;
 import team.jeonghokim.daedongyeojido.domain.submission.domain.repository.SubmissionRepository;
 
-@Configuration
+@Component
 @RequiredArgsConstructor
 public class SubmissionFacade {
     private final SubmissionRepository submissionRepository;
