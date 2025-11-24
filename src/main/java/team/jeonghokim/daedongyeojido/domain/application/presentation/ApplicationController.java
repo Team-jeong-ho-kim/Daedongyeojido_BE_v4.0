6bdb84028ed30f9ea8e22dcbd626bc71e78b1bea
박태수 <taesupark0504@gmail.com>
2025-11-24 17:08:47 +0900

    refactor (#84) :: userController => applicationController

--- src/main/java/team/jeonghokim/daedongyeojido/domain/application/presentation/ApplicationController.java
+++ src/main/java/team/jeonghokim/daedongyeojido/domain/application/presentation/ApplicationController.java
@@ -1,11 +1,66 @@
 package team.jeonghokim.daedongyeojido.domain.application.presentation;
 
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
+import team.jeonghokim.daedongyeojido.domain.submission.presentation.dto.request.SubmissionRequest;
+import team.jeonghokim.daedongyeojido.domain.user.presentation.dto.response.QueryApplicationDetailResponse;
+import team.jeonghokim.daedongyeojido.domain.user.presentation.dto.response.QueryApplicationListResponse;
+import team.jeonghokim.daedongyeojido.domain.user.service.*;
 
 @Service
 @RequestMapping("/application")
 @RequiredArgsConstructor
 public class ApplicationController {
+    private final CreateApplicationService createApplicationService;
+    private final QueryApplicationDetailService queryApplicationDetailService;
+    private final QueryApplicationListService queryApplicationListService;
+    private final UpdateApplicationService updateApplicationService;
+    private final DeleteApplicationService deleteApplicationService;
+    private final SubmitApplicationService submitApplicationService;
+    private final CancelApplicationService cancelApplicationService;
+
+    @PostMapping("/{application-form-id}")
+    @ResponseStatus(HttpStatus.CREATED)
+    public void createApplication(@PathVariable("application-form-id") Long applicationFormId, @RequestBody @Valid SubmissionRequest request) {
+        createApplicationService.execute(applicationFormId, request);
+    }
+
+    @GetMapping("/{submission-id}")
+    @ResponseStatus(HttpStatus.OK)
+    public QueryApplicationDetailResponse queryApplicationDetail(@PathVariable("submission-id") Long submissionId) {
+        return queryApplicationDetailService.execute(submissionId);
+    }
+
+    @GetMapping
+    @ResponseStatus(HttpStatus.OK)
+    public QueryApplicationListResponse queryApplicationList() {
+        return queryApplicationListService.execute();
+    }
+
+    @PatchMapping("/{submission-id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void updateApplication(@PathVariable("submission-id") Long submissionId, @RequestBody @Valid SubmissionRequest request) {
+        updateApplicationService.execute(submissionId, request);
+    }
+
+    @DeleteMapping("/{submission-id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void deleteApplication(@PathVariable("submission-id") Long submissionId) {
+        deleteApplicationService.execute(submissionId);
+    }
+
+    @PatchMapping("/submit/{submission-id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void submitApplication(@PathVariable("submission-id") Long submissionId) {
+        submitApplicationService.execute(submissionId);
+    }
+
+    @PatchMapping("/cancel/{submission-id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void cancelApplication(@PathVariable("submission-id") Long submissionId) {
+        cancelApplicationService.execute(submissionId);
+    }
 }
--- src/main/java/team/jeonghokim/daedongyeojido/domain/user/presentation/UserController.java
+++ src/main/java/team/jeonghokim/daedongyeojido/domain/user/presentation/UserController.java
@@ -4,12 +4,9 @@ import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
-import team.jeonghokim.daedongyeojido.domain.submission.presentation.dto.request.SubmissionRequest;
 import team.jeonghokim.daedongyeojido.domain.user.presentation.dto.request.DecideTeamMemberApplicationRequest;
 import team.jeonghokim.daedongyeojido.domain.user.presentation.dto.request.MyInfoRequest;
 import team.jeonghokim.daedongyeojido.domain.user.presentation.dto.request.UpdateMyInfoRequest;
-import team.jeonghokim.daedongyeojido.domain.user.presentation.dto.response.QueryApplicationDetailResponse;
-import team.jeonghokim.daedongyeojido.domain.user.presentation.dto.response.QueryApplicationListResponse;
 import team.jeonghokim.daedongyeojido.domain.user.presentation.dto.response.QueryMyInfoResponse;
 import team.jeonghokim.daedongyeojido.domain.user.service.*;
 
@@ -22,13 +19,6 @@ public class UserController {
     private final QueryMyInfoService queryMyInfoService;
     private final UpdateMyInfoService updateMyInfoService;
     private final DecideTeamMemberApplicationService decideTeamMemberApplicationService;
-    private final CreateApplicationService createApplicationService;
-    private final QueryApplicationDetailService queryApplicationDetailService;
-    private final QueryApplicationListService queryApplicationListService;
-    private final UpdateApplicationService updateApplicationService;
-    private final DeleteApplicationService deleteApplicationService;
-    private final SubmitApplicationService submitApplicationService;
-    private final CancelApplicationService cancelApplicationService;
 
     @PatchMapping("/my-info")
     @ResponseStatus(HttpStatus.NO_CONTENT)
@@ -52,47 +42,5 @@ public class UserController {
     @ResponseStatus(HttpStatus.NO_CONTENT)
     public void approveTeamMember(@RequestBody @Valid DecideTeamMemberApplicationRequest request) {
         decideTeamMemberApplicationService.execute(request);
-    }
-
-    @PostMapping("/application/{application-form-id}")
-    @ResponseStatus(HttpStatus.CREATED)
-    public void createApplication(@PathVariable("application-form-id") Long applicationFormId, @RequestBody @Valid SubmissionRequest request) {
-        createApplicationService.execute(applicationFormId, request);
-    }
-
-    @GetMapping("/application/{submission-id}")
-    @ResponseStatus(HttpStatus.OK)
-    public QueryApplicationDetailResponse queryApplicationDetail(@PathVariable("submission-id") Long submissionId) {
-        return queryApplicationDetailService.execute(submissionId);
-    }
-
-    @GetMapping("/application")
-    @ResponseStatus(HttpStatus.OK)
-    public QueryApplicationListResponse queryApplicationList() {
-        return queryApplicationListService.execute();
-    }
-
-    @PatchMapping("/application/{submission-id}")
-    @ResponseStatus(HttpStatus.NO_CONTENT)
-    public void updateApplication(@PathVariable("submission-id") Long submissionId, @RequestBody @Valid SubmissionRequest request) {
-        updateApplicationService.execute(submissionId, request);
-    }
-
-    @DeleteMapping("/application/{submission-id}")
-    @ResponseStatus(HttpStatus.NO_CONTENT)
-    public void deleteApplication(@PathVariable("submission-id") Long submissionId) {
-        deleteApplicationService.execute(submissionId);
-    }
-
-    @PatchMapping("/application/submit/{submission-id}")
-    @ResponseStatus(HttpStatus.NO_CONTENT)
-    public void submitApplication(@PathVariable("submission-id") Long submissionId) {
-        submitApplicationService.execute(submissionId);
-    }
-
-    @PatchMapping("/application/cancel/{submission-id}")
-    @ResponseStatus(HttpStatus.NO_CONTENT)
-    public void cancelApplication(@PathVariable("submission-id") Long submissionId) {
-        cancelApplicationService.execute(submissionId);
     }
 }
