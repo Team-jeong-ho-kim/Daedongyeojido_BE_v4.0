9aa95c3e6152a50a9ae8768bd3b52903446608f3
박태수 <taesupark0504@gmail.com>
2025-11-24 16:55:51 +0900

    feat (#84) :: applicationController

---
+++ src/main/java/team/jeonghokim/daedongyeojido/domain/application/presentation/ApplicationController.java
@@ -0,0 +1,11 @@
+package team.jeonghokim.daedongyeojido.domain.application.presentation;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Service
+@RequestMapping("/application")
+@RequiredArgsConstructor
+public class ApplicationController {
+}
