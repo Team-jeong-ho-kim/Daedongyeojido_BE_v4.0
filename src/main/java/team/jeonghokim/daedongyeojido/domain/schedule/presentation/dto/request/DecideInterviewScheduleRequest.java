643901935e4d73568821c154a6014d4680dc667a
coehgns <modooboiroo@gmail.com>
2025-11-26 16:11:45 +0900

    feat (#82) :: DecideInterviewScheduleRequest에 @JsonProperty 추가

--- src/main/java/team/jeonghokim/daedongyeojido/domain/schedule/presentation/dto/request/DecideInterviewScheduleRequest.java
+++ src/main/java/team/jeonghokim/daedongyeojido/domain/schedule/presentation/dto/request/DecideInterviewScheduleRequest.java
@@ -1,11 +1,17 @@
 package team.jeonghokim.daedongyeojido.domain.schedule.presentation.dto.request;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 
 public record DecideInterviewScheduleRequest(
+        @JsonProperty("interviewSchedule")
         LocalDate interviewSchedule,
+
         String place,
+
+        @JsonProperty("interviewTime")
         LocalDateTime interviewTime
 ) {
 }
