0162255682a0d3f43ced7ce271a8226f6354279a
coehgns <modooboiroo@gmail.com>
2025-11-26 16:11:07 +0900

    feat (#82) :: ScheduleController에 @RequestBody 추가

--- src/main/java/team/jeonghokim/daedongyeojido/domain/schedule/presentation/ScheduleController.java
+++ src/main/java/team/jeonghokim/daedongyeojido/domain/schedule/presentation/ScheduleController.java
@@ -3,6 +3,7 @@ package team.jeonghokim.daedongyeojido.domain.schedule.presentation;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import team.jeonghokim.daedongyeojido.domain.schedule.presentation.dto.request.DecideInterviewScheduleRequest;
@@ -16,7 +17,7 @@ public class ScheduleController {
     private final DecideInterviewScheduleService decideInterviewScheduleService;
 
     @PostMapping("/{user-id}")
-    public void decideInterviewSchedule(@PathVariable("user-id") Long userId, DecideInterviewScheduleRequest request) {
+    public void decideInterviewSchedule(@PathVariable("user-id") Long userId, @RequestBody DecideInterviewScheduleRequest request) {
         decideInterviewScheduleService.execute(userId, request);
     }
 }
