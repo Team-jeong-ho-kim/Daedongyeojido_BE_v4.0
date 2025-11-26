2f8654a56b42e4bbc298ad7d0a8ec30c26570264
coehgns <modooboiroo@gmail.com>
2025-11-26 13:46:26 +0900

    feat (#82) :: ScheduleController

---
+++ src/main/java/team/jeonghokim/daedongyeojido/domain/schedule/presentation/ScheduleController.java
@@ -0,0 +1,22 @@
+package team.jeonghokim.daedongyeojido.domain.schedule.presentation;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import team.jeonghokim.daedongyeojido.domain.schedule.presentation.dto.request.DecideInterviewScheduleRequest;
+import team.jeonghokim.daedongyeojido.domain.schedule.service.DecideInterviewScheduleService;
+
+@RestController
+@RequestMapping("/schedule")
+@RequiredArgsConstructor
+public class ScheduleController {
+
+    private final DecideInterviewScheduleService decideInterviewScheduleService;
+
+    @PostMapping("/{user-id}")
+    public void decideInterviewSchedule(@PathVariable("user-id") Long userId, DecideInterviewScheduleRequest request) {
+        decideInterviewScheduleService.execute(userId, request);
+    }
+}
