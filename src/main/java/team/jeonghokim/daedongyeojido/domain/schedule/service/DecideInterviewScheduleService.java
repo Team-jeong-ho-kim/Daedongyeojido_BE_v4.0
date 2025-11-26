b925777ea4d86eedc487ebecb7f219fb727a24f3
coehgns <modooboiroo@gmail.com>
2025-11-26 13:46:22 +0900

    feat (#82) :: DecideInterviewScheduleService

---
+++ src/main/java/team/jeonghokim/daedongyeojido/domain/schedule/service/DecideInterviewScheduleService.java
@@ -0,0 +1,43 @@
+package team.jeonghokim.daedongyeojido.domain.schedule.service;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import team.jeonghokim.daedongyeojido.domain.club.exception.AlreadyJoinClubException;
+import team.jeonghokim.daedongyeojido.domain.schedule.domain.Schedule;
+import team.jeonghokim.daedongyeojido.domain.schedule.domain.repository.ScheduleRepository;
+import team.jeonghokim.daedongyeojido.domain.schedule.presentation.dto.request.DecideInterviewScheduleRequest;
+import team.jeonghokim.daedongyeojido.domain.user.domain.User;
+import team.jeonghokim.daedongyeojido.domain.user.domain.repository.UserRepository;
+import team.jeonghokim.daedongyeojido.domain.user.exception.UserNotFoundException;
+import team.jeonghokim.daedongyeojido.domain.user.facade.UserFacade;
+
+@Service
+@RequiredArgsConstructor
+public class DecideInterviewScheduleService {
+
+    private final ScheduleRepository scheduleRepository;
+    private final UserRepository userRepository;
+    private final UserFacade userFacade;
+
+    @Transactional
+    public void execute(Long userId, DecideInterviewScheduleRequest request) {
+        User interviewer = userFacade.getCurrentUser();
+        User applicant = userRepository.findById(userId)
+                .orElseThrow(() -> UserNotFoundException.EXCEPTION);
+
+        if (applicant.getClub() != null) {
+            throw AlreadyJoinClubException.EXCEPTION;
+        }
+
+        Schedule schedule = Schedule.builder()
+                .interviewSchedule(request.interviewSchedule())
+                .place(request.place())
+                .interviewTime(request.interviewTime())
+                .user(applicant)
+                .club(interviewer.getClub())
+                .build();
+
+        scheduleRepository.save(schedule);
+    }
+}
