02fd99116b8ab274e572718df1a9e4e8ae87507e
coehgns <modooboiroo@gmail.com>
2025-11-26 14:13:30 +0900

    chore (#82) :: SecurityConfig에 면접 일정 결정 API 권한 설정

--- src/main/java/team/jeonghokim/daedongyeojido/global/config/SecurityConfig.java
+++ src/main/java/team/jeonghokim/daedongyeojido/global/config/SecurityConfig.java
@@ -65,6 +65,7 @@ public class SecurityConfig {
                         .requestMatchers(HttpMethod.POST,"/user/application/**").hasAnyRole(STUDENT, CLUB_LEADER, CLUB_MEMBER)
                         .requestMatchers(HttpMethod.GET,"/user/application/**").hasAnyRole(STUDENT, CLUB_LEADER, CLUB_MEMBER)
                         .requestMatchers(HttpMethod.DELETE,"/user/application/**").hasAnyRole(STUDENT, CLUB_LEADER, CLUB_MEMBER)
+                        .requestMatchers(HttpMethod.POST, "/schedule/**").hasAnyRole(CLUB_LEADER, CLUB_MEMBER)
                         .anyRequest().authenticated()
                 )
                 .with(new SecurityFilterConfig(jwtTokenProvider, objectMapper), Customizer.withDefaults())
