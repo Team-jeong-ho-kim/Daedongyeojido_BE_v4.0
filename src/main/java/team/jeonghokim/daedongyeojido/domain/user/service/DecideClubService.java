ed19a41e0eeaf553fcf59274306b55613d928b47
박태수 <taesupark0504@gmail.com>
2025-11-29 19:46:09 +0900

    feat (#88) :: applicationAccessDenied 예외 적용

--- src/main/java/team/jeonghokim/daedongyeojido/domain/user/service/DecideClubService.java
+++ src/main/java/team/jeonghokim/daedongyeojido/domain/user/service/DecideClubService.java
@@ -4,6 +4,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import team.jeonghokim.daedongyeojido.domain.application.domain.enums.ApplicationStatus;
+import team.jeonghokim.daedongyeojido.domain.application.exception.ApplicationAccessDeniedException;
 import team.jeonghokim.daedongyeojido.domain.application.exception.ApplicationNotAcceptedException;
 import team.jeonghokim.daedongyeojido.domain.submission.domain.Submission;
 import team.jeonghokim.daedongyeojido.domain.submission.facade.SubmissionFacade;
@@ -11,21 +12,25 @@ import team.jeonghokim.daedongyeojido.domain.user.domain.User;
 import team.jeonghokim.daedongyeojido.domain.user.domain.repository.UserRepository;
 import team.jeonghokim.daedongyeojido.domain.user.exception.AlreadySelectClubException;
 import team.jeonghokim.daedongyeojido.domain.user.exception.UserNotFoundException;
+import team.jeonghokim.daedongyeojido.domain.user.facade.UserFacade;
 import team.jeonghokim.daedongyeojido.domain.user.presentation.dto.request.DecideClubRequest;
 
 @Service
 @RequiredArgsConstructor
 public class DecideClubService {
+    private final UserFacade userFacade;
     private final SubmissionFacade submissionFacade;
     private final UserRepository userRepository;
 
     @Transactional
     public void execute(Long submissionId, DecideClubRequest request) {
+        User applicant = userFacade.getCurrentUser();
 
         Submission submission = submissionFacade.getApplicationBySubmissionId(submissionId);
 
-        User applicant = userRepository.findById(submission.getUser().getId())
-                .orElseThrow(() -> UserNotFoundException.EXCEPTION);
+        if (!applicant.getId().equals(submission.getUser().getId())) {
+            throw ApplicationAccessDeniedException.EXCEPTION;
+        }
 
         if (applicant.getClub() != null) {
             throw AlreadySelectClubException.EXCEPTION;
