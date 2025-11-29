3c434ef83cd8252f1d5aa290d1db6947257909b7
coehgns <modooboiroo@gmail.com>
2025-11-29 19:49:35 +0900

    refactor (#82) :: DecideInterviewScheduleRequest @JsonFormat 추가

--- src/main/java/team/jeonghokim/daedongyeojido/domain/schedule/presentation/dto/request/DecideInterviewScheduleRequest.java
+++ src/main/java/team/jeonghokim/daedongyeojido/domain/schedule/presentation/dto/request/DecideInterviewScheduleRequest.java
@@ -1,5 +1,6 @@
 package team.jeonghokim.daedongyeojido.domain.schedule.presentation.dto.request;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import jakarta.validation.constraints.FutureOrPresent;
 import jakarta.validation.constraints.NotNull;
@@ -12,6 +13,7 @@ public record DecideInterviewScheduleRequest(
         @NotNull(message = "면접 날짜는 필수입니다.")
         @FutureOrPresent(message = "면접 날짜는 과거일 수 없습니다.")
         @JsonProperty("interviewSchedule")
+        @JsonFormat(pattern = "yyyy-MM-dd")
         LocalDate interviewSchedule,
 
         String place,
@@ -19,6 +21,7 @@ public record DecideInterviewScheduleRequest(
         @NotNull(message = "면접 시간은 필수입니다.")
         @FutureOrPresent(message = "면접 시간은 과거일 수 없습니다.")
         @JsonProperty("interviewTime")
+        @JsonFormat(pattern = "HH:mm")
         LocalDateTime interviewTime
 ) {
 }
