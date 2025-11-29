7071a887c02a35955352a05a8be2c79c15cc7608
박태수 <taesupark0504@gmail.com>
2025-11-29 19:16:13 +0900

    feat (#88) :: selectedClub method

--- src/main/java/team/jeonghokim/daedongyeojido/domain/user/domain/User.java
+++ src/main/java/team/jeonghokim/daedongyeojido/domain/user/domain/User.java
@@ -117,4 +117,8 @@ public class User extends BaseIdEntity {
         this.role = Role.CLUB_MEMBER;
         this.club = club;
     }
+
+    public void selectedClub(Club club) {
+        this.club = club;
+    }
 }
