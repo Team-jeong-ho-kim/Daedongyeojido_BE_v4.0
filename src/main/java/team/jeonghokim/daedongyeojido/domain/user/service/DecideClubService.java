3ab3d3fcaa7b677144ddef237f407a0af3caf1bf
박태수 <taesupark0504@gmail.com>
2025-11-29 19:27:05 +0900

    refactor (#88) :: applicant로 변경

--- src/main/java/team/jeonghokim/daedongyeojido/domain/user/service/DecideClubService.java
+++ src/main/java/team/jeonghokim/daedongyeojido/domain/user/service/DecideClubService.java
@@ -3,31 +3,35 @@ package team.jeonghokim.daedongyeojido.domain.user.service;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import team.jeonghokim.daedongyeojido.domain.application.domain.enums.ApplicationStatus;
 import team.jeonghokim.daedongyeojido.domain.submission.domain.Submission;
 import team.jeonghokim.daedongyeojido.domain.submission.facade.SubmissionFacade;
 import team.jeonghokim.daedongyeojido.domain.user.domain.User;
+import team.jeonghokim.daedongyeojido.domain.user.domain.repository.UserRepository;
 import team.jeonghokim.daedongyeojido.domain.user.exception.AlreadySelectClubException;
-import team.jeonghokim.daedongyeojido.domain.user.facade.UserFacade;
+import team.jeonghokim.daedongyeojido.domain.user.exception.UserNotFoundException;
 import team.jeonghokim.daedongyeojido.domain.user.presentation.dto.request.DecideClubRequest;
 
 @Service
 @RequiredArgsConstructor
 public class DecideClubService {
-    private final UserFacade userFacade;
     private final SubmissionFacade submissionFacade;
+    private final UserRepository userRepository;
 
     @Transactional
     public void execute(Long submissionId, DecideClubRequest request) {
-        User user = userFacade.getCurrentUser();
 
         Submission submission = submissionFacade.getApplicationBySubmissionId(submissionId);
 
-        if (user.getClub() != null) {
+        User applicant = userRepository.findById(submission.getUser().getId())
+                .orElseThrow(() -> UserNotFoundException.EXCEPTION);
+
+        if (applicant.getClub() != null) {
             throw AlreadySelectClubException.EXCEPTION;
         }
-
+        
         if (request.getIsSelected()) {
-            user.selectedClub(submission.getApplicationForm().getClub());
+            applicant.selectedClub(submission.getApplicationForm().getClub());
         }
     }
 }
