b93889ada49f9a2576881bf85d26c0ee9d643f07
coehgns <modooboiroo@gmail.com>
2025-11-30 22:34:12 +0900

    feat (#91) :: ScheduleController에 updateInterviewSchedule 추가

--- src/main/java/team/jeonghokim/daedongyeojido/domain/schedule/presentation/ScheduleController.java
+++ src/main/java/team/jeonghokim/daedongyeojido/domain/schedule/presentation/ScheduleController.java
@@ -2,6 +2,7 @@ package team.jeonghokim.daedongyeojido.domain.schedule.presentation;
 
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -9,6 +10,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import team.jeonghokim.daedongyeojido.domain.schedule.presentation.dto.request.InterviewScheduleRequest;
 import team.jeonghokim.daedongyeojido.domain.schedule.service.DecideInterviewScheduleService;
+import team.jeonghokim.daedongyeojido.domain.schedule.service.UpdateInterviewScheduleService;
 
 @RestController
 @RequestMapping("/schedule")
@@ -16,9 +18,15 @@ import team.jeonghokim.daedongyeojido.domain.schedule.service.DecideInterviewSch
 public class ScheduleController {
 
     private final DecideInterviewScheduleService decideInterviewScheduleService;
+    private final UpdateInterviewScheduleService updateInterviewScheduleService;
 
     @PostMapping("/{user-id}")
     public void decideInterviewSchedule(@PathVariable("user-id") Long userId, @RequestBody @Valid InterviewScheduleRequest request) {
         decideInterviewScheduleService.execute(userId, request);
+    }
+
+    @PatchMapping("/{schedule-id}")
+    public void updateInterviewSchedule(@PathVariable("schedule-id") Long scheduleId, @RequestBody @Valid InterviewScheduleRequest request) {
+        updateInterviewScheduleService.execute(scheduleId, request);
     }
 }
