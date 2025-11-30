071b9fb6f90cf59fa26e84a7e5d8ee036a07fb7e
coehgns <modooboiroo@gmail.com>
2025-11-30 22:44:06 +0900

    feat (#91) :: ScheduleController에 @ResponseStatus 추가

--- src/main/java/team/jeonghokim/daedongyeojido/domain/schedule/presentation/ScheduleController.java
+++ src/main/java/team/jeonghokim/daedongyeojido/domain/schedule/presentation/ScheduleController.java
@@ -2,11 +2,13 @@ package team.jeonghokim.daedongyeojido.domain.schedule.presentation;
 
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 import team.jeonghokim.daedongyeojido.domain.schedule.presentation.dto.request.InterviewScheduleRequest;
 import team.jeonghokim.daedongyeojido.domain.schedule.service.DecideInterviewScheduleService;
@@ -21,11 +23,13 @@ public class ScheduleController {
     private final UpdateInterviewScheduleService updateInterviewScheduleService;
 
     @PostMapping("/{user-id}")
+    @ResponseStatus(HttpStatus.OK)
     public void decideInterviewSchedule(@PathVariable("user-id") Long userId, @RequestBody @Valid InterviewScheduleRequest request) {
         decideInterviewScheduleService.execute(userId, request);
     }
 
     @PatchMapping("/{schedule-id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
     public void updateInterviewSchedule(@PathVariable("schedule-id") Long scheduleId, @RequestBody @Valid InterviewScheduleRequest request) {
         updateInterviewScheduleService.execute(scheduleId, request);
     }
