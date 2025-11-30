ef0645e3b9f8a47f42d6a5c5cadc47ceb3462f2f
coehgns <modooboiroo@gmail.com>
2025-11-30 22:42:18 +0900

    feat (#91) :: UpdateInterviewScheduleService에 면접 일정 변경 접근 권한 예외 처리 추가

--- src/main/java/team/jeonghokim/daedongyeojido/domain/schedule/service/UpdateInterviewScheduleService.java
+++ src/main/java/team/jeonghokim/daedongyeojido/domain/schedule/service/UpdateInterviewScheduleService.java
@@ -5,19 +5,28 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import team.jeonghokim.daedongyeojido.domain.schedule.domain.Schedule;
 import team.jeonghokim.daedongyeojido.domain.schedule.domain.repository.ScheduleRepository;
+import team.jeonghokim.daedongyeojido.domain.schedule.exception.InterviewScheduleAccessDeniedException;
 import team.jeonghokim.daedongyeojido.domain.schedule.exception.InterviewScheduleNotFoundException;
 import team.jeonghokim.daedongyeojido.domain.schedule.presentation.dto.request.InterviewScheduleRequest;
+import team.jeonghokim.daedongyeojido.domain.user.domain.User;
+import team.jeonghokim.daedongyeojido.domain.user.facade.UserFacade;
 
 @Service
 @RequiredArgsConstructor
 public class UpdateInterviewScheduleService {
 
     private final ScheduleRepository scheduleRepository;
+    private final UserFacade userFacade;
 
     @Transactional
     public void execute(Long scheduleId, InterviewScheduleRequest request) {
+        User currentUser = userFacade.getCurrentUser();
         Schedule schedule = scheduleRepository.findById(scheduleId)
                 .orElseThrow(() -> InterviewScheduleNotFoundException.EXCEPTION);
+
+        if (!(currentUser.getClub().getId().equals(schedule.getClub().getId()))) {
+            throw InterviewScheduleAccessDeniedException.EXCEPTION;
+        }
 
         schedule.updateSchedule(request);
     }
