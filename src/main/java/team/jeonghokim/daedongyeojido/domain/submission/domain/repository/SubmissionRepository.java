1c300533de6e354504b9bb92ef133a05d646c7ef
coehgns <modooboiroo@gmail.com>
2025-12-01 15:48:01 +0900

    chore (#96) :: SubmissionRepository findByUserIdAndApplicationFormClubId 메서드 삭제

--- src/main/java/team/jeonghokim/daedongyeojido/domain/submission/domain/repository/SubmissionRepository.java
+++ src/main/java/team/jeonghokim/daedongyeojido/domain/submission/domain/repository/SubmissionRepository.java
@@ -3,9 +3,5 @@ package team.jeonghokim.daedongyeojido.domain.submission.domain.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import team.jeonghokim.daedongyeojido.domain.submission.domain.Submission;
 
-import java.util.Optional;
-
 public interface SubmissionRepository extends JpaRepository<Submission, Long>, SubmissionRepositoryCustom {
-
-    Optional<Submission> findByUserIdAndApplicationFormClubId(Long userId, Long applicationFormClubId);
 }
