acf68e37c50bcbe890a9c8f8e68873e3e8580819
박태수 <taesupark0504@gmail.com>
2025-12-01 10:12:47 +0900

    chore (#93) :: feign 관련 설정 모두 infrastructure로 이동

--- src/main/java/team/jeonghokim/daedongyeojido/infrastructure/feign/xquare/XquareClient.java
+++ src/main/java/team/jeonghokim/daedongyeojido/infrastructure/feign/xquare/XquareClient.java
@@ -1,10 +1,10 @@
-package team.jeonghokim.daedongyeojido.global.xquare;
+package team.jeonghokim.daedongyeojido.infrastructure.feign.xquare;
 
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import team.jeonghokim.daedongyeojido.domain.auth.presentation.dto.request.LoginRequest;
-import team.jeonghokim.daedongyeojido.global.xquare.dto.XquareResponse;
+import team.jeonghokim.daedongyeojido.infrastructure.feign.xquare.dto.XquareResponse;
 
 @FeignClient(name = "xquare-login", url = "${das-key.login-api-url}")
 public interface XquareClient {
