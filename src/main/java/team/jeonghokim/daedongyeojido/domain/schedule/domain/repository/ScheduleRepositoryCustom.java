e66e05c0e26abf71c9d15e7dfd699f889c0059a1
coehgns <modooboiroo@gmail.com>
2025-12-01 15:47:03 +0900

    feat (#96) :: ScheduleRepositoryCustom에 findScheduleById 메서드 추가

--- src/main/java/team/jeonghokim/daedongyeojido/domain/schedule/domain/repository/ScheduleRepositoryCustom.java
+++ src/main/java/team/jeonghokim/daedongyeojido/domain/schedule/domain/repository/ScheduleRepositoryCustom.java
@@ -1,10 +1,14 @@
 package team.jeonghokim.daedongyeojido.domain.schedule.domain.repository;
 
+import team.jeonghokim.daedongyeojido.domain.schedule.domain.Schedule;
 import team.jeonghokim.daedongyeojido.domain.schedule.presentation.dto.response.InterviewScheduleResponse;
 
 import java.util.List;
+import java.util.Optional;
 
 interface ScheduleRepositoryCustom {
 
     List<InterviewScheduleResponse> findAllSchedulesByClubId(Long clubId);
+
+    Optional<Schedule> findScheduleById(Long scheduleId);
 }
