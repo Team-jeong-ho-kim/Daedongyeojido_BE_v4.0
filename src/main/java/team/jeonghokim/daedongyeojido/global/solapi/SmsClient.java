43e8f4d99596177ad53558433c184d8aa19d2528
박태수 <taesupark0504@gmail.com>
2025-12-01 18:47:22 +0900

    refactor (#93) :: SmsConfig 설정

--- src/main/java/team/jeonghokim/daedongyeojido/global/solapi/SmsClient.java
+++ src/main/java/team/jeonghokim/daedongyeojido/global/solapi/SmsClient.java
@@ -5,10 +5,12 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import team.jeonghokim.daedongyeojido.global.solapi.dto.request.SmsRequest;
 import team.jeonghokim.daedongyeojido.global.solapi.dto.response.SmsResponse;
+import team.jeonghokim.daedongyeojido.infrastructure.sms.config.SmsConfig;
 
 @FeignClient(
         name = "solapi-sms",
-        url = "${solapi-sms.url}"
+        url = "${solapi-sms.url}",
+        configuration = SmsConfig.class
 )
 public interface SmsClient {
 
