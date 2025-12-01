ed86ab5f743a678ab32d4fbeeb0dad67335651e4
coehgns <modooboiroo@gmail.com>
2025-12-01 16:06:06 +0900

    chore (#96) :: ScheduleRepositoryCustom의 findScheduleById 메서드 삭제

--- src/main/java/team/jeonghokim/daedongyeojido/domain/schedule/domain/repository/ScheduleRepositoryCustom.java
+++ src/main/java/team/jeonghokim/daedongyeojido/domain/schedule/domain/repository/ScheduleRepositoryCustom.java
@@ -1,14 +1,10 @@
 package team.jeonghokim.daedongyeojido.domain.schedule.domain.repository;
 
-import team.jeonghokim.daedongyeojido.domain.schedule.domain.Schedule;
 import team.jeonghokim.daedongyeojido.domain.schedule.presentation.dto.response.InterviewScheduleResponse;
 
 import java.util.List;
-import java.util.Optional;
 
 interface ScheduleRepositoryCustom {
 
     List<InterviewScheduleResponse> findAllSchedulesByClubId(Long clubId);
-
-    Optional<Schedule> findScheduleById(Long scheduleId);
 }
