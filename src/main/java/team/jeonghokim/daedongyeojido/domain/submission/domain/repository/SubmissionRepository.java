95f608a5a335e4e7a0d85bde41115bf2fc60f2c8
coehgns <modooboiroo@gmail.com>
2025-12-01 14:40:12 +0900

    feat (#96) :: SubmissionRepository에 findByUserIdAndApplicationFormClubId 메서드 추가

--- src/main/java/team/jeonghokim/daedongyeojido/domain/submission/domain/repository/SubmissionRepository.java
+++ src/main/java/team/jeonghokim/daedongyeojido/domain/submission/domain/repository/SubmissionRepository.java
@@ -3,5 +3,9 @@ package team.jeonghokim.daedongyeojido.domain.submission.domain.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import team.jeonghokim.daedongyeojido.domain.submission.domain.Submission;
 
+import java.util.Optional;
+
 public interface SubmissionRepository extends JpaRepository<Submission, Long>, SubmissionRepositoryCustom {
+
+    Optional<Submission> findByUserIdAndApplicationFormClubId(Long userId, Long applicationFormClubId);
 }
