87e954dc3c7defa601caffc782b0e7d37a1b6620
박태수 <taesupark0504@gmail.com>
2025-12-01 15:38:49 +0900

    chore (#93) :: content로 네이밍 변경

--- src/main/java/team/jeonghokim/daedongyeojido/infrastructure/sms/type/Message.java
+++ src/main/java/team/jeonghokim/daedongyeojido/infrastructure/sms/type/Message.java
@@ -9,5 +9,5 @@ public enum Message {
     CLUB_FINAL_ACCEPTED("동아리 최종 합격", "축하드립니다! 동아리에 최종 합격하였습니다.");
 
     private final String title;
-    private final String defaultMessage;
+    private final String content;
 }
