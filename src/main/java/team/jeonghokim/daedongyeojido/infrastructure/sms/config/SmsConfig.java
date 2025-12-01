69e21d9a42db46ffbd2da6836e3f1b86bb0eeb79
박태수 <taesupark0504@gmail.com>
2025-12-01 18:47:38 +0900

    refactor (#93) :: @Value 추가

--- src/main/java/team/jeonghokim/daedongyeojido/infrastructure/sms/config/SmsConfig.java
+++ src/main/java/team/jeonghokim/daedongyeojido/infrastructure/sms/config/SmsConfig.java
@@ -1,13 +1,25 @@
 package team.jeonghokim.daedongyeojido.infrastructure.sms.config;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import team.jeonghokim.daedongyeojido.infrastructure.sms.auth.SignatureProvider;
 
 @Configuration
+@RequiredArgsConstructor
 public class SmsConfig {
 
+    @Value("${solapi-sms.accessKey}")
+    private String accessKey;
+
+    @Value("${solapi-sms.secretKey}")
+    private String secretKey;
+
+    private final SignatureProvider signatureProvider;
+
     @Bean
-    public SmsInterceptor smsFeignInterceptor(SmsInterceptor interceptor) {
-        return interceptor;
+    public SmsInterceptor smsInterceptor() {
+        return new SmsInterceptor(signatureProvider, accessKey, secretKey);
     }
 }
