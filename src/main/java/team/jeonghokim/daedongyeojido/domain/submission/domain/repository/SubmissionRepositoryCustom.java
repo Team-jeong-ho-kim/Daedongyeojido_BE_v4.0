a76761cd76d4a50e051f515cb3bc446efd4e3c5c
coehgns <modooboiroo@gmail.com>
2025-12-01 15:48:20 +0900

    feat (#96) :: SubmissionRepositoryCustom에 findByUserIdAndClubId 추가

--- src/main/java/team/jeonghokim/daedongyeojido/domain/submission/domain/repository/SubmissionRepositoryCustom.java
+++ src/main/java/team/jeonghokim/daedongyeojido/domain/submission/domain/repository/SubmissionRepositoryCustom.java
@@ -1,10 +1,12 @@
 package team.jeonghokim.daedongyeojido.domain.submission.domain.repository;
 
 import team.jeonghokim.daedongyeojido.domain.application.presentation.dto.response.ApplicationListResponse;
+import team.jeonghokim.daedongyeojido.domain.submission.domain.Submission;
 import team.jeonghokim.daedongyeojido.domain.submission.presentation.dto.response.ApplicantResponse;
 import team.jeonghokim.daedongyeojido.domain.submission.presentation.dto.response.SubmissionListResponse;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface SubmissionRepositoryCustom {
 
@@ -13,4 +15,6 @@ public interface SubmissionRepositoryCustom {
     List<ApplicationListResponse> findAllByUserId(Long userId);
 
     List<SubmissionListResponse> findAllSubmissionByUserId(Long userId);
+
+    Optional<Submission> findByUserIdAndClubId(Long userId, Long clubId);
 }
