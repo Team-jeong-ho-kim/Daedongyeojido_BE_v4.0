310065647fef5498956f767bb2cf9ee41cdb02c4
coehgns <modooboiroo@gmail.com>
2025-12-01 10:11:52 +0900

    feat (#94) :: ScheduleRepository에 ScheduleRepositoryCustom 상속 받도록 설정

--- src/main/java/team/jeonghokim/daedongyeojido/domain/schedule/domain/repository/ScheduleRepository.java
+++ src/main/java/team/jeonghokim/daedongyeojido/domain/schedule/domain/repository/ScheduleRepository.java
@@ -5,7 +5,7 @@ import team.jeonghokim.daedongyeojido.domain.club.domain.Club;
 import team.jeonghokim.daedongyeojido.domain.schedule.domain.Schedule;
 import team.jeonghokim.daedongyeojido.domain.user.domain.User;
 
-public interface ScheduleRepository extends JpaRepository<Schedule, Long> {
+public interface ScheduleRepository extends JpaRepository<Schedule, Long>, ScheduleRepositoryCustom {
 
     boolean existsByApplicantAndClub(User applicant, Club club);
 }
