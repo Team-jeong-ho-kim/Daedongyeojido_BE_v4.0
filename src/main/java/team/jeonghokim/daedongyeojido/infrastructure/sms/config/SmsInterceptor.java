089d872fbadd1c0ee59106d6c940c299b924e5c2
박태수 <taesupark0504@gmail.com>
2025-12-01 17:27:40 +0900

    chore (#93) :: 활성화

--- src/main/java/team/jeonghokim/daedongyeojido/infrastructure/sms/config/SmsInterceptor.java
+++ src/main/java/team/jeonghokim/daedongyeojido/infrastructure/sms/config/SmsInterceptor.java
@@ -1,53 +1,53 @@
-//package team.jeonghokim.daedongyeojido.infrastructure.sms.config;
-//
-//import feign.RequestInterceptor;
-//import feign.RequestTemplate;
-//import lombok.RequiredArgsConstructor;
-//import org.springframework.beans.factory.annotation.Value;
-//import org.springframework.stereotype.Component;
-//import team.jeonghokim.daedongyeojido.infrastructure.sms.auth.SignatureProvider;
-//
-//import java.util.UUID;
-//
-//@Component
-//@RequiredArgsConstructor
-//public class SmsInterceptor implements RequestInterceptor {
-//
-//    private static final String HEADER_CONTENT_TYPE = "application/json";
-//    private static final String HEADER_AUTH = "Authorization";
-//    private static final String CONTENT_TYPE = "Content-Type";
-//    private static final String HMAC = "HmacSHA256";
-//    private static final String FORMAT = "%s apiKey=%s, date=%d, salt=%s, signature=%s";
-//
-//    private final SignatureProvider signatureProvider;
-//
-//    @Value("${solapi-sms.accessKey}")
-//    private String accessKey;
-//
-//    @Value("${solapi-sms.secretKey}")
-//    private String secretKey;
-//
-//    @Override
-//    public void apply(RequestTemplate template) {
-//
-//        long timestamp = System.currentTimeMillis();
-//        String salt = UUID.randomUUID().toString();
-//
-//        String signature = createSignature(timestamp, salt);
-//
-//        template.header(HEADER_AUTH, authorizationHeader(timestamp, salt, signature));
-//        template.header(CONTENT_TYPE, HEADER_CONTENT_TYPE);
-//    }
-//
-//    private String createSignature(long timestamp, String salt) {
-//        String data = timestamp + salt;
-//
-//        return signatureProvider.generateSignature(secretKey, data);
-//    }
-//
-//    private String authorizationHeader(long timestamp, String salt, String signature) {
-//        return String.format(
-//                FORMAT, HMAC, accessKey, timestamp, salt, signature
-//        );
-//    }
-//}
+package team.jeonghokim.daedongyeojido.infrastructure.sms.config;
+
+import feign.RequestInterceptor;
+import feign.RequestTemplate;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+import team.jeonghokim.daedongyeojido.infrastructure.sms.auth.SignatureProvider;
+
+import java.util.UUID;
+
+@Component
+@RequiredArgsConstructor
+public class SmsInterceptor implements RequestInterceptor {
+
+    private static final String HEADER_CONTENT_TYPE = "application/json";
+    private static final String HEADER_AUTH = "Authorization";
+    private static final String CONTENT_TYPE = "Content-Type";
+    private static final String HMAC = "HmacSHA256";
+    private static final String FORMAT = "%s apiKey=%s, date=%d, salt=%s, signature=%s";
+
+    private final SignatureProvider signatureProvider;
+
+    @Value("${solapi-sms.accessKey}")
+    private String accessKey;
+
+    @Value("${solapi-sms.secretKey}")
+    private String secretKey;
+
+    @Override
+    public void apply(RequestTemplate template) {
+
+        long timestamp = System.currentTimeMillis();
+        String salt = UUID.randomUUID().toString();
+
+        String signature = createSignature(timestamp, salt);
+
+        template.header(HEADER_AUTH, authorizationHeader(timestamp, salt, signature));
+        template.header(CONTENT_TYPE, HEADER_CONTENT_TYPE);
+    }
+
+    private String createSignature(long timestamp, String salt) {
+        String data = timestamp + salt;
+
+        return signatureProvider.generateSignature(secretKey, data);
+    }
+
+    private String authorizationHeader(long timestamp, String salt, String signature) {
+        return String.format(
+                FORMAT, HMAC, accessKey, timestamp, salt, signature
+        );
+    }
+}
--- src/main/java/team/jeonghokim/daedongyeojido/infrastructure/sms/exception/SignatureGenerationException.java
+++ src/main/java/team/jeonghokim/daedongyeojido/infrastructure/sms/exception/SignatureGenerationException.java
@@ -1,12 +1,12 @@
-//package team.jeonghokim.daedongyeojido.infrastructure.sms.exception;
-//
-//import team.jeonghokim.daedongyeojido.global.error.exception.DaedongException;
-//import team.jeonghokim.daedongyeojido.global.error.exception.ErrorCode;
-//
-//public class SignatureGenerationException extends DaedongException {
-//    public static final DaedongException EXCEPTION = new SignatureGenerationException();
-//
-//    private SignatureGenerationException() {
-//        super(ErrorCode.SIGNATURE_GENERATION_EXCEPTION);
-//    }
-//}
+package team.jeonghokim.daedongyeojido.infrastructure.sms.exception;
+
+import team.jeonghokim.daedongyeojido.global.error.exception.DaedongException;
+import team.jeonghokim.daedongyeojido.global.error.exception.ErrorCode;
+
+public class SignatureGenerationException extends DaedongException {
+    public static final DaedongException EXCEPTION = new SignatureGenerationException();
+
+    private SignatureGenerationException() {
+        super(ErrorCode.SIGNATURE_GENERATION_EXCEPTION);
+    }
+}
