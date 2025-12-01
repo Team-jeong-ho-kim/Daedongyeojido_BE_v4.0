d3ae3244e654c80d74c7763f2d1b7dbdf30ae3d3
박태수 <taesupark0504@gmail.com>
2025-12-02 08:55:41 +0900

    feat (#100) :: findAllByClubId

--- src/main/java/team/jeonghokim/daedongyeojido/domain/alarm/domain/repository/AlarmRepositoryCustom.java
+++ src/main/java/team/jeonghokim/daedongyeojido/domain/alarm/domain/repository/AlarmRepositoryCustom.java
@@ -1,4 +1,10 @@
 package team.jeonghokim.daedongyeojido.domain.alarm.domain.repository;
 
+import team.jeonghokim.daedongyeojido.domain.alarm.presentation.dto.response.AlarmResponse;
+
+import java.util.List;
+
 public interface AlarmRepositoryCustom {
+
+    List<AlarmResponse> findAllByClubId(Long clubId);
 }
