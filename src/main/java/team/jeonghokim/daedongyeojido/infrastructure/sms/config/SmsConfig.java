1cf836cf5c5862b3a38c2f6c4401c8593a9f4f15
박태수 <taesupark0504@gmail.com>
2025-12-01 21:18:26 +0900

    feat (#93) :: smsProperties

--- src/main/java/team/jeonghokim/daedongyeojido/infrastructure/sms/config/SmsConfig.java
+++ src/main/java/team/jeonghokim/daedongyeojido/infrastructure/sms/config/SmsConfig.java
@@ -1,25 +1,21 @@
 package team.jeonghokim.daedongyeojido.infrastructure.sms.config;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import team.jeonghokim.daedongyeojido.infrastructure.sms.auth.SignatureProvider;
 
 @Configuration
 @RequiredArgsConstructor
+@EnableConfigurationProperties(SmsProperties.class)
 public class SmsConfig {
 
-    @Value("${solapi-sms.accessKey}")
-    private String accessKey;
-
-    @Value("${solapi-sms.secretKey}")
-    private String secretKey;
-
     private final SignatureProvider signatureProvider;
+    private final SmsProperties properties;
 
     @Bean
     public SmsInterceptor smsInterceptor() {
-        return new SmsInterceptor(signatureProvider, accessKey, secretKey);
+        return new SmsInterceptor(signatureProvider, properties.accessKey(), properties.secretKey());
     }
 }
