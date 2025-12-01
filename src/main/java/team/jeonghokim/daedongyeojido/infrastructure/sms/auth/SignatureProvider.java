3c96f9f20f74636d5605d39e28cde64780fd3424
박태수 <taesupark0504@gmail.com>
2025-12-01 19:48:15 +0900

    fix (#93) :: hex 문자열로 변경

--- src/main/java/team/jeonghokim/daedongyeojido/infrastructure/sms/auth/SignatureProvider.java
+++ src/main/java/team/jeonghokim/daedongyeojido/infrastructure/sms/auth/SignatureProvider.java
@@ -6,11 +6,11 @@ import team.jeonghokim.daedongyeojido.infrastructure.sms.exception.SignatureGene
 import javax.crypto.Mac;
 import javax.crypto.spec.SecretKeySpec;
 import java.nio.charset.StandardCharsets;
-import java.util.Base64;
+import java.util.HexFormat;
 
 @Component
 public class SignatureProvider {
-    private static final String HMAC = "hmac-sha256";
+    private static final String HMAC = "hmacSHA256";
 
     public String generateSignature(String secretKey, String data) {
         try {
@@ -24,10 +24,10 @@ public class SignatureProvider {
             byte[] result = mac.doFinal(data.getBytes(StandardCharsets.UTF_8));
 
             /*
-                byte 형태로는 HTTP 헤더, JSON, URL에 담을 수 없기 때문에
-                byte를 Base64 문자열로 변환해서 반환
+                byte 형태로는 HTTP 헤더에 넣을 수 없기 때문에
+                Solapi가 signature 값으로 요구하는 hex 문자열로 직렬화
              */
-            return Base64.getEncoder().encodeToString(result);
+            return HexFormat.of().formatHex(result);
 
         } catch (Exception e) {
             throw SignatureGenerationException.EXCEPTION;
