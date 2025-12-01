43af726c0d50986ae6b79b9f6b4a8767a5f29023
coehgns <modooboiroo@gmail.com>
2025-12-01 16:06:51 +0900

    chore (#96) :: ScheduleRepositoryCustomImpl에서 findScheduleById 메서드 삭제

--- src/main/java/team/jeonghokim/daedongyeojido/domain/schedule/domain/repository/ScheduleRepositoryCustomImpl.java
+++ src/main/java/team/jeonghokim/daedongyeojido/domain/schedule/domain/repository/ScheduleRepositoryCustomImpl.java
@@ -2,14 +2,11 @@ package team.jeonghokim.daedongyeojido.domain.schedule.domain.repository;
 
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import lombok.RequiredArgsConstructor;
-import team.jeonghokim.daedongyeojido.domain.schedule.domain.Schedule;
 import team.jeonghokim.daedongyeojido.domain.schedule.presentation.dto.response.InterviewScheduleResponse;
 import team.jeonghokim.daedongyeojido.domain.schedule.presentation.dto.response.QInterviewScheduleResponse;
 
 import java.util.List;
-import java.util.Optional;
 
-import static team.jeonghokim.daedongyeojido.domain.club.domain.QClub.club;
 import static team.jeonghokim.daedongyeojido.domain.schedule.domain.QSchedule.schedule;
 import static team.jeonghokim.daedongyeojido.domain.submission.domain.QSubmission.submission;
 import static team.jeonghokim.daedongyeojido.domain.user.domain.QUser.user;
@@ -35,16 +32,5 @@ public class ScheduleRepositoryCustomImpl implements ScheduleRepositoryCustom {
                 .where(submission.applicationForm.club.id.eq(clubId))
                 .orderBy(schedule.interviewSchedule.asc())
                 .fetch();
-    }
-
-    @Override
-    public Optional<Schedule> findScheduleById(Long scheduleId) {
-        return Optional.ofNullable(
-                jpaQueryFactory.selectFrom(schedule)
-                        .join(schedule.club, club).fetchJoin()
-                        .join(schedule.applicant, user).fetchJoin()
-                        .where(schedule.id.eq(scheduleId))
-                        .fetchOne()
-        );
     }
 }
