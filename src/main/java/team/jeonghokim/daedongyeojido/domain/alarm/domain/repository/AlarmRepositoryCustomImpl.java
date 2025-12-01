f2f0dacf4243f987a454167ac4ab1adec9c2539a
박태수 <taesupark0504@gmail.com>
2025-12-02 08:56:03 +0900

    feat (#100) :: findAllByClubId 구현체

--- src/main/java/team/jeonghokim/daedongyeojido/domain/alarm/domain/repository/AlarmRepositoryCustomImpl.java
+++ src/main/java/team/jeonghokim/daedongyeojido/domain/alarm/domain/repository/AlarmRepositoryCustomImpl.java
@@ -2,10 +2,28 @@ package team.jeonghokim.daedongyeojido.domain.alarm.domain.repository;
 
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import lombok.RequiredArgsConstructor;
+import team.jeonghokim.daedongyeojido.domain.alarm.domain.QAlarm;
+import team.jeonghokim.daedongyeojido.domain.alarm.presentation.dto.response.AlarmResponse;
+import team.jeonghokim.daedongyeojido.domain.alarm.presentation.dto.response.QAlarmResponse;
+import team.jeonghokim.daedongyeojido.domain.club.domain.QClub;
+
+import java.util.List;
 
 @RequiredArgsConstructor
 public class AlarmRepositoryCustomImpl implements AlarmRepositoryCustom {
     private final JPAQueryFactory queryFactory;
-    
+    private final QAlarm alarm = QAlarm.alarm;
 
+    @Override
+    public List<AlarmResponse> findAllByClubId(Long clubId) {
+        return queryFactory.select(new QAlarmResponse(
+                alarm.id,
+                alarm.title,
+                alarm.content
+        ))
+                .from(alarm)
+                .join(alarm.club, QClub.club)
+                .where(QClub.club.id.eq(clubId))
+                .fetch();
+    }
 }
