6615d362c59b5a1f2e993c293362fb7a0480330f
박태수 <taesupark0504@gmail.com>
2025-12-02 11:09:26 +0900

    feat (#102) :: queryUserAlarmService

---
+++ src/main/java/team/jeonghokim/daedongyeojido/domain/user/service/QueryUserAlarmService.java
@@ -0,0 +1,28 @@
+package team.jeonghokim.daedongyeojido.domain.user.service;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import team.jeonghokim.daedongyeojido.domain.alarm.domain.repository.AlarmRepository;
+import team.jeonghokim.daedongyeojido.domain.alarm.presentation.dto.response.AlarmResponse;
+import team.jeonghokim.daedongyeojido.domain.alarm.presentation.dto.response.QueryUserAlarmResponse;
+import team.jeonghokim.daedongyeojido.domain.user.domain.User;
+import team.jeonghokim.daedongyeojido.domain.user.facade.UserFacade;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class QueryUserAlarmService {
+    private final UserFacade userFacade;
+    private final AlarmRepository alarmRepository;
+
+    @Transactional(readOnly = true)
+    public QueryUserAlarmResponse execute() {
+        User user = userFacade.getCurrentUser();
+
+        List<AlarmResponse> alarmResponses = alarmRepository.findAllByUserId(user.getId());
+
+        return new QueryUserAlarmResponse(alarmResponses);
+    }
+}
