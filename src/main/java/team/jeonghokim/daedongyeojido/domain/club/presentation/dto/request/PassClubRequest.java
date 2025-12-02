18c40162088b9e096131e0fc0f22f0605e0c516c
coehgns <modooboiroo@gmail.com>
2025-12-02 09:04:19 +0900

    feat (#99) :: PassClubRequest에 @JsonProperty 추가

--- src/main/java/team/jeonghokim/daedongyeojido/domain/club/presentation/dto/request/PassClubRequest.java
+++ src/main/java/team/jeonghokim/daedongyeojido/domain/club/presentation/dto/request/PassClubRequest.java
@@ -1,9 +1,11 @@
 package team.jeonghokim.daedongyeojido.domain.club.presentation.dto.request;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import jakarta.validation.constraints.NotNull;
 
 public record PassClubRequest(
         @NotNull(message = "isPassed 값은 필수입니다.")
+        @JsonProperty("isPassed")
         Boolean isPassed
 ) {
 }
