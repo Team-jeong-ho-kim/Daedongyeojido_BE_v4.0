8bf867805c9f6ca9dc32a1244d9c87ad9bf5b296
박태수 <taesupark0504@gmail.com>
2025-12-12 11:53:26 +0900

    feat (#111) :: 동아리 불합격 message 추가

--- src/main/java/team/jeonghokim/daedongyeojido/infrastructure/sms/type/Message.java
+++ src/main/java/team/jeonghokim/daedongyeojido/infrastructure/sms/type/Message.java
@@ -6,7 +6,8 @@ import lombok.Getter;
 @Getter
 @AllArgsConstructor
 public enum Message {
-    CLUB_FINAL_ACCEPTED("동아리 최종 합격", "축하드립니다! 동아리에 최종 합격하였습니다.");
+    CLUB_FINAL_ACCEPTED("동아리 최종 합격", "축하드립니다! 동아리에 최종 합격하였습니다."),
+    CLUB_FINAL_REJECTED("동아리 최종 불합격", "아쉽지만 지원자님은 불합격하셨습니다.");
 
     private final String title;
     private final String content;
