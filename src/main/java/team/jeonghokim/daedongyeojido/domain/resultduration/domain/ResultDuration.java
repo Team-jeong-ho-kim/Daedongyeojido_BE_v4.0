69e26e564e9100511a3c7b0dda68f83f7ab0931d
박태수 <taesupark0504@gmail.com>
2025-12-13 21:15:35 +0900

    feat (#111) :: resultDuration

---
+++ src/main/java/team/jeonghokim/daedongyeojido/domain/resultduration/domain/ResultDuration.java
@@ -0,0 +1,27 @@
+package team.jeonghokim.daedongyeojido.domain.resultduration.domain;
+
+import jakarta.persistence.Entity;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import team.jeonghokim.daedongyeojido.global.entity.BaseIdEntity;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@NoArgsConstructor
+public class ResultDuration extends BaseIdEntity {
+
+    private LocalDateTime resultDuration;
+
+    private boolean isExecuted;
+
+    public ResultDuration(LocalDateTime resultDuration) {
+        this.resultDuration = resultDuration;
+        this.isExecuted = false;
+    }
+
+    public void execute() {
+        this.isExecuted = true;
+    }
+}
