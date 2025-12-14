601f8ccac8cf32e456d7a344dcb9c2277c6a407f
박태수 <taesupark0504@gmail.com>
2025-12-14 23:03:49 +0900

    feat (#111) :: ResultDurationAlreadySetException

--- src/main/java/team/jeonghokim/daedongyeojido/global/error/exception/ErrorCode.java
+++ src/main/java/team/jeonghokim/daedongyeojido/global/error/exception/ErrorCode.java
@@ -61,6 +61,7 @@ public enum ErrorCode {
 
     // resultDuration
     RESULT_DURATION_NOT_FOUND(404, "발표시간이 설정되지 않았습니다."),
+    RESULT_DURATION_ALREADY_SET(409, "이미 발표시간이 설정되어있습니다."),
     RESULT_DURATION_ALREADY_EXECUTED(400, "이미 결과 처리가 완료되었습니다."),
 
     // s3
