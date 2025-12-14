e53ca5cd958bbce85fbb552d121f55b1bf1f3430
박태수 <taesupark0504@gmail.com>
2025-12-14 22:47:38 +0900

    feat (#111) :: findTopByOrderByIdDesc

--- src/main/java/team/jeonghokim/daedongyeojido/domain/resultduration/domain/repository/ResultDurationRepository.java
+++ src/main/java/team/jeonghokim/daedongyeojido/domain/resultduration/domain/repository/ResultDurationRepository.java
@@ -3,5 +3,9 @@ package team.jeonghokim.daedongyeojido.domain.resultduration.domain.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import team.jeonghokim.daedongyeojido.domain.resultduration.domain.ResultDuration;
 
+import java.util.Optional;
+
 public interface ResultDurationRepository extends JpaRepository<ResultDuration, Long> {
+
+    Optional<ResultDuration> findTopByOrderByIdDesc();
 }
