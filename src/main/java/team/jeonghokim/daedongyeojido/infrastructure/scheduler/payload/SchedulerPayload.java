608a60fec2eff1f3ef728ce5c8d42757e76dc2b2
박태수 <taesupark0504@gmail.com>
2025-12-15 19:18:40 +0900

    chore (#111) :: snake 허용

--- src/main/java/team/jeonghokim/daedongyeojido/infrastructure/scheduler/payload/SchedulerPayload.java
+++ src/main/java/team/jeonghokim/daedongyeojido/infrastructure/scheduler/payload/SchedulerPayload.java
@@ -1,11 +1,12 @@
 package team.jeonghokim.daedongyeojido.infrastructure.scheduler.payload;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Builder;
 
 @Builder
 public record SchedulerPayload(
-        Long submissionId,
-        String phoneNumber,
-        boolean isPassed
+        @JsonProperty("submission_id") Long submissionId,
+        @JsonProperty("phone_number") String phoneNumber,
+        @JsonProperty("is_passed") boolean isPassed
 ) {
 }
