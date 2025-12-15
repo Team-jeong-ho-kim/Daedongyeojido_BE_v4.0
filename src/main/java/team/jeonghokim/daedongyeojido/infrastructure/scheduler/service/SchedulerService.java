d6c2e26697608175128196d375ef73edad6a0a7c
박태수 <taesupark0504@gmail.com>
2025-12-15 11:35:23 +0900

    chore (#111) :: 주석 설명 추가

--- src/main/java/team/jeonghokim/daedongyeojido/infrastructure/scheduler/service/SchedulerService.java
+++ src/main/java/team/jeonghokim/daedongyeojido/infrastructure/scheduler/service/SchedulerService.java
@@ -27,27 +27,33 @@ public class SchedulerService {
     @Scheduled(fixedRate = 10_000)
     public void execute() {
 
+        // 어드민 발표기간 설정 전 스케줄러 대기 상태
         ResultDuration resultDuration = resultDurationRepository.findTopByOrderByIdDesc()
                 .orElse(null);
 
         if (resultDuration == null) return;
 
+        // 발표기간 설정 후 현재 시간과 비교 후 기간이 되지 않았을 경우 대기
         long now = Instant.now().getEpochSecond();
         long duration = resultDuration.getResultDuration().toEpochSecond(ZoneOffset.UTC);
 
         if (now < duration) return;
 
+        // 발표시간이 되었을때 redis에서 모든 메시지를 가져옴
         Set<SchedulerPayload> messages = smsRedisTemplate.opsForZSet()
                 .rangeByScore(RESULT_DURATION_ZSET, 0, now);
 
+        // message가 null일 경우 NPE 방지
         if (messages == null || messages.isEmpty()) return;
 
+        // 문자 발송
         for (SchedulerPayload payload : messages) {
             smsService.send(
                     payload.phoneNumber(),
                     payload.isPassed() ? Message.CLUB_FINAL_ACCEPTED : Message.CLUB_FINAL_REJECTED
             );
 
+            // 문자 발송 후 제거
             smsRedisTemplate.opsForZSet()
                     .remove(RESULT_DURATION_ZSET, payload);
         }
