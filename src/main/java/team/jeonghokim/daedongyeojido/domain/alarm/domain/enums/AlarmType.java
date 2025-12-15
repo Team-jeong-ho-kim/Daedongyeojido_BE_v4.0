b4b2e6af9657b92ef24c9b36d75db9c9cb1922c7
박태수 <taesupark0504@gmail.com>
2025-12-15 22:47:22 +0900

    feat (#111) :: alarmType추가

--- src/main/java/team/jeonghokim/daedongyeojido/domain/alarm/domain/enums/AlarmType.java
+++ src/main/java/team/jeonghokim/daedongyeojido/domain/alarm/domain/enums/AlarmType.java
@@ -10,7 +10,10 @@ import lombok.Getter;
 public enum AlarmType {
 
     CREATE_CLUB("동아리 개설 신청", "%s의 동아리 개설 신청이 접수되었습니다"),
-    DISSOLVE_CLUB("동아리 해체 신청", "%s의 동아리 해체 신청이 접수되었습니다.");
+    DISSOLVE_CLUB("동아리 해체 신청", "%s의 동아리 해체 신청이 접수되었습니다."),
+    CLUB_FINAL_ACCEPTED("동아리 최종 합격", "축하드립니다! %s 동아리에 최종 합격하였습니다."),
+    CLUB_FINAL_REJECTED("동아리 최종 불합격", "아쉽지만 지원자님은 %s 동아리에 불합격하셨습니다.");
+
 
     private final String title;
     private final String content;
