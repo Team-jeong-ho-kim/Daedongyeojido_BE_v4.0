25adf75b613bddba3c19300365beafda1b3916a2
coehgns <modooboiroo@gmail.com>
2025-12-18 19:36:59 +0900

    feat (#113) :: QueryClubDetailResponse 정적 팩토리 메서드 삭제

--- src/main/java/team/jeonghokim/daedongyeojido/domain/club/presentation/dto/response/QueryClubDetailResponse.java
+++ src/main/java/team/jeonghokim/daedongyeojido/domain/club/presentation/dto/response/QueryClubDetailResponse.java
@@ -1,21 +1,9 @@
 package team.jeonghokim.daedongyeojido.domain.club.presentation.dto.response;
 
-import team.jeonghokim.daedongyeojido.domain.club.domain.Club;
-import team.jeonghokim.daedongyeojido.domain.user.domain.User;
-
 import java.util.List;
 
 public record QueryClubDetailResponse(
         ClubDetailDto club,
         List<ClubMembersDto> clubMembers
 ) {
-
-    public static QueryClubDetailResponse of(Club club, List<User> clubMembers) {
-        return new QueryClubDetailResponse(
-                ClubDetailDto.from(club),
-                clubMembers.stream()
-                        .map(ClubMembersDto::from)
-                        .toList()
-        );
-    }
 }
