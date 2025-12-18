24a2205ce85e8d0fd188a2c2383e52c95e7575c8
coehgns <modooboiroo@gmail.com>
2025-12-18 19:37:26 +0900

    feat (#113) :: ClubRepositoryCustom에 findDetailWithMembersById 추가

--- src/main/java/team/jeonghokim/daedongyeojido/domain/club/domain/repository/ClubRepositoryCustom.java
+++ src/main/java/team/jeonghokim/daedongyeojido/domain/club/domain/repository/ClubRepositoryCustom.java
@@ -1,10 +1,14 @@
 package team.jeonghokim.daedongyeojido.domain.club.domain.repository;
 
 import team.jeonghokim.daedongyeojido.domain.club.domain.repository.vo.ClubVO;
+import team.jeonghokim.daedongyeojido.domain.club.presentation.dto.response.QueryClubDetailResponse;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface ClubRepositoryCustom {
 
     List<ClubVO> findAllByIsOpenIsTrue();
+
+    Optional<QueryClubDetailResponse> findDetailWithMembersById(Long clubId);
 }
