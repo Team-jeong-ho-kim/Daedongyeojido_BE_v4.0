45a3c9d7a8e00d9380ceb6f8a83260a6cdb7041a
coehgns <modooboiroo@gmail.com>
2025-12-18 19:02:39 +0900

    refactor (#113) :: QueryClubDetailService에 소속한 유저의 정보를 함께 반환하도록 구현

--- src/main/java/team/jeonghokim/daedongyeojido/domain/club/service/QueryClubDetailService.java
+++ src/main/java/team/jeonghokim/daedongyeojido/domain/club/service/QueryClubDetailService.java
@@ -6,17 +6,23 @@ import org.springframework.transaction.annotation.Transactional;
 import team.jeonghokim.daedongyeojido.domain.club.domain.Club;
 import team.jeonghokim.daedongyeojido.domain.club.facade.ClubFacade;
 import team.jeonghokim.daedongyeojido.domain.club.presentation.dto.response.QueryClubDetailResponse;
+import team.jeonghokim.daedongyeojido.domain.user.domain.User;
+import team.jeonghokim.daedongyeojido.domain.user.domain.repository.UserRepository;
+
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
 public class QueryClubDetailService {
 
     private final ClubFacade clubFacade;
+    private final UserRepository userRepository;
 
     @Transactional(readOnly = true)
     public QueryClubDetailResponse execute(Long clubId) {
         Club club = clubFacade.getClubById(clubId);
+        List<User> clubMembers = userRepository.findAllByClub(club);
 
-        return QueryClubDetailResponse.of(club);
+        return QueryClubDetailResponse.of(club, clubMembers);
     }
 }
