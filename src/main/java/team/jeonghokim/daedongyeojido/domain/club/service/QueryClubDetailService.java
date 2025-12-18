d47739936929f7ae4f777bd72a488bb78b89ddfe
coehgns <modooboiroo@gmail.com>
2025-12-18 19:37:34 +0900

    refactor (#113) :: QueryClubDetailService

--- src/main/java/team/jeonghokim/daedongyeojido/domain/club/service/QueryClubDetailService.java
+++ src/main/java/team/jeonghokim/daedongyeojido/domain/club/service/QueryClubDetailService.java
@@ -3,26 +3,19 @@ package team.jeonghokim.daedongyeojido.domain.club.service;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-import team.jeonghokim.daedongyeojido.domain.club.domain.Club;
-import team.jeonghokim.daedongyeojido.domain.club.facade.ClubFacade;
+import team.jeonghokim.daedongyeojido.domain.club.domain.repository.ClubRepository;
+import team.jeonghokim.daedongyeojido.domain.club.exception.ClubNotFoundException;
 import team.jeonghokim.daedongyeojido.domain.club.presentation.dto.response.QueryClubDetailResponse;
-import team.jeonghokim.daedongyeojido.domain.user.domain.User;
-import team.jeonghokim.daedongyeojido.domain.user.domain.repository.UserRepository;
-
-import java.util.List;
 
 @Service
 @RequiredArgsConstructor
 public class QueryClubDetailService {
 
-    private final ClubFacade clubFacade;
-    private final UserRepository userRepository;
+    private final ClubRepository clubRepository;
 
     @Transactional(readOnly = true)
     public QueryClubDetailResponse execute(Long clubId) {
-        Club club = clubFacade.getClubById(clubId);
-        List<User> clubMembers = userRepository.findAllByClub(club);
-
-        return QueryClubDetailResponse.of(club, clubMembers);
+        return clubRepository.findDetailWithMembersById(clubId)
+                .orElseThrow(() -> ClubNotFoundException.EXCEPTION);
     }
 }
