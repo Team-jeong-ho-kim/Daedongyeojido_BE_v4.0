897ac52e026965c9c01d57f37d401e9bc65e2233
coehgns <modooboiroo@gmail.com>
2025-12-18 19:01:41 +0900

    feat (#113) :: QueryClubDetailResponse

--- src/main/java/team/jeonghokim/daedongyeojido/domain/club/presentation/dto/response/QueryClubDetailResponse.java
+++ src/main/java/team/jeonghokim/daedongyeojido/domain/club/presentation/dto/response/QueryClubDetailResponse.java
@@ -1,29 +1,21 @@
 package team.jeonghokim.daedongyeojido.domain.club.presentation.dto.response;
 
 import team.jeonghokim.daedongyeojido.domain.club.domain.Club;
-import team.jeonghokim.daedongyeojido.domain.club.domain.ClubLink;
-import team.jeonghokim.daedongyeojido.domain.club.domain.ClubMajor;
-import team.jeonghokim.daedongyeojido.domain.user.domain.enums.Major;
+import team.jeonghokim.daedongyeojido.domain.user.domain.User;
 
 import java.util.List;
 
 public record QueryClubDetailResponse(
-        String clubName,
-        String introduction,
-        String oneLiner,
-        String clubImage,
-        List<Major> majors,
-        List<String> links
+        ClubDetailDto club,
+        List<ClubMembersDto> clubMembers
 ) {
 
-    public static QueryClubDetailResponse of(Club club) {
+    public static QueryClubDetailResponse of(Club club, List<User> clubMembers) {
         return new QueryClubDetailResponse(
-                club.getClubName(),
-                club.getIntroduction(),
-                club.getOneLiner(),
-                club.getClubImage(),
-                club.getClubMajors().stream().map(ClubMajor::getMajor).toList(),
-                club.getClubLinks().stream().map(ClubLink::getLink).toList()
+                ClubDetailDto.from(club),
+                clubMembers.stream()
+                        .map(ClubMembersDto::from)
+                        .toList()
         );
     }
 }
