d6d05b55d45f25ad8a9018f9bfdb9daf3b56877b
coehgns <modooboiroo@gmail.com>
2025-12-19 22:43:43 +0900

    refactor (#119) :: ClubController endpoint를 restful하게 수정

--- src/main/java/team/jeonghokim/daedongyeojido/domain/club/presentation/ClubController.java
+++ src/main/java/team/jeonghokim/daedongyeojido/domain/club/presentation/ClubController.java
@@ -3,8 +3,16 @@ package team.jeonghokim.daedongyeojido.domain.club.presentation;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.*;
-import team.jeonghokim.daedongyeojido.domain.alarm.presentation.dto.response.QueryClubAlarmResponse;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
 import team.jeonghokim.daedongyeojido.domain.club.presentation.dto.request.ClubRequest;
 import team.jeonghokim.daedongyeojido.domain.club.presentation.dto.request.PassClubRequest;
 import team.jeonghokim.daedongyeojido.domain.club.presentation.dto.request.TeamMemberRequest;
@@ -87,7 +95,7 @@ public class ClubController {
         return queryClubSubmissionDetailService.execute(submissionId);
     }
 
-    @PatchMapping("/submissions/{submission-id}")
+    @PatchMapping("/pass/{submission-id}")
     @ResponseStatus(HttpStatus.NO_CONTENT)
     public void passClub(@PathVariable("submission-id") Long submissionId, @RequestBody @Valid PassClubRequest request) {
         passClubService.execute(submissionId, request);
