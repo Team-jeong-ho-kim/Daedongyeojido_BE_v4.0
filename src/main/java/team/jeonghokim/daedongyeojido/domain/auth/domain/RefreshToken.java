9e090143c390639ef22ca24d3fee5564af53f189
coehgns <modooboiroo@gmail.com>
2025-12-19 15:16:15 +0900

    chore (#118) :: RefreshToken의 생성자 어노테이션 제거

--- src/main/java/team/jeonghokim/daedongyeojido/domain/auth/domain/RefreshToken.java
+++ src/main/java/team/jeonghokim/daedongyeojido/domain/auth/domain/RefreshToken.java
@@ -1,16 +1,15 @@
 package team.jeonghokim.daedongyeojido.domain.auth.domain;
 
-import lombok.*;
+import lombok.Builder;
+import lombok.Getter;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.redis.core.RedisHash;
 import org.springframework.data.redis.core.TimeToLive;
 import org.springframework.data.redis.core.index.Indexed;
 
-@RedisHash
 @Getter
 @Builder
-@AllArgsConstructor(access = AccessLevel.PRIVATE)
-@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@RedisHash
 public class RefreshToken {
 
     @Id
