62c72311b8e874faf689b43d038867aade1f6588
coehgns <modooboiroo@gmail.com>
2025-12-22 17:01:05 +0900

    refactor (#124) :: QueryUserSubmissionListResponse 정적 팩토리 메서드 적용

--- src/main/java/team/jeonghokim/daedongyeojido/domain/submission/presentation/dto/response/QueryUserSubmissionListResponse.java
+++ src/main/java/team/jeonghokim/daedongyeojido/domain/submission/presentation/dto/response/QueryUserSubmissionListResponse.java
@@ -1,6 +1,15 @@
 package team.jeonghokim.daedongyeojido.domain.submission.presentation.dto.response;
 
+import lombok.Builder;
+
 import java.util.List;
 
-public record QueryUserSubmissionListResponse(List<SubmissionListResponse> submissionListResponses) {
+@Builder
+public record QueryUserSubmissionListResponse(List<SubmissionListResponse> submissions) {
+
+    public static QueryUserSubmissionListResponse from(List<SubmissionListResponse> submissions) {
+        return QueryUserSubmissionListResponse.builder()
+                .submissions(submissions)
+                .build();
+    }
 }
