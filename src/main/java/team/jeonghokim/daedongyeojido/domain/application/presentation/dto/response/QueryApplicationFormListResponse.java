a8a1d1d312981581832d412eb50e3160a28f179a
coehgns <modooboiroo@gmail.com>
2025-12-22 17:29:46 +0900

    refactor (#124) :: QueryApplicationFormListResponse new를 사용하여 객체 생성하도록 수정

--- src/main/java/team/jeonghokim/daedongyeojido/domain/application/presentation/dto/response/QueryApplicationFormListResponse.java
+++ src/main/java/team/jeonghokim/daedongyeojido/domain/application/presentation/dto/response/QueryApplicationFormListResponse.java
@@ -1,15 +1,10 @@
 package team.jeonghokim.daedongyeojido.domain.application.presentation.dto.response;
 
-import lombok.Builder;
-
 import java.util.List;
 
-@Builder
 public record QueryApplicationFormListResponse(List<ApplicationFormResponse> applicationForms) {
 
     public static QueryApplicationFormListResponse from(List<ApplicationFormResponse> applicationForms) {
-        return QueryApplicationFormListResponse.builder()
-                .applicationForms(applicationForms)
-                .build();
+        return new QueryApplicationFormListResponse(applicationForms);
     }
 }
