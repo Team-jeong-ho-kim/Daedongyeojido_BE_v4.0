6ee7275e12dd15a346d4e909cee80b57303817ed
coehgns <modooboiroo@gmail.com>
2025-12-22 17:30:25 +0900

    refactor (#124) :: QueryUserSubmissionListResponse new를 사용하여 객체 생성하도록 수정

--- src/main/java/team/jeonghokim/daedongyeojido/domain/submission/presentation/dto/response/QueryUserSubmissionListResponse.java
+++ src/main/java/team/jeonghokim/daedongyeojido/domain/submission/presentation/dto/response/QueryUserSubmissionListResponse.java
@@ -1,15 +1,10 @@
 package team.jeonghokim.daedongyeojido.domain.submission.presentation.dto.response;
 
-import lombok.Builder;
-
 import java.util.List;
 
-@Builder
 public record QueryUserSubmissionListResponse(List<SubmissionListResponse> submissions) {
 
     public static QueryUserSubmissionListResponse from(List<SubmissionListResponse> submissions) {
-        return QueryUserSubmissionListResponse.builder()
-                .submissions(submissions)
-                .build();
+        return new QueryUserSubmissionListResponse(submissions);
     }
 }
