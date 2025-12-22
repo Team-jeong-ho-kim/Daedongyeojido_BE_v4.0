227de577064c5a6706cab50ad7928fdd19e2314d
coehgns <modooboiroo@gmail.com>
2025-12-22 17:30:05 +0900

    refactor (#124) :: QueryClubListResponse new를 사용하여 객체 생성하도록 수정

--- src/main/java/team/jeonghokim/daedongyeojido/domain/club/presentation/dto/response/QueryClubListResponse.java
+++ src/main/java/team/jeonghokim/daedongyeojido/domain/club/presentation/dto/response/QueryClubListResponse.java
@@ -1,15 +1,12 @@
 package team.jeonghokim.daedongyeojido.domain.club.presentation.dto.response;
 
-import lombok.Builder;
 import team.jeonghokim.daedongyeojido.domain.club.domain.repository.vo.ClubVO;
 
 import java.util.List;
 
-@Builder
 public record QueryClubListResponse(List<ClubVO> clubs) {
+
     public static QueryClubListResponse from(List<ClubVO> clubs) {
-        return QueryClubListResponse.builder()
-                .clubs(clubs)
-                .build();
+        return new QueryClubListResponse(clubs);
     }
 }
