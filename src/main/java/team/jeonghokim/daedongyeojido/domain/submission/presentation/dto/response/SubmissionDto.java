2f7b2403df1916c660715cd8ac99f80871dcd66e
coehgns <modooboiroo@gmail.com>
2025-12-22 17:01:20 +0900

    refactor (#124) :: SubmissionDto에 정적 팩토리 메서드 추가

--- src/main/java/team/jeonghokim/daedongyeojido/domain/submission/presentation/dto/response/SubmissionDto.java
+++ src/main/java/team/jeonghokim/daedongyeojido/domain/submission/presentation/dto/response/SubmissionDto.java
@@ -1,7 +1,18 @@
 package team.jeonghokim.daedongyeojido.domain.submission.presentation.dto.response;
 
+import lombok.Builder;
+import team.jeonghokim.daedongyeojido.domain.application.domain.ApplicationAnswer;
+
+@Builder
 public record SubmissionDto(
         Long questionId,
         String content
 ) {
+
+    public static SubmissionDto from(ApplicationAnswer applicationAnswer) {
+        return SubmissionDto.builder()
+                .questionId(applicationAnswer.getApplicationQuestion().getId())
+                .content(applicationAnswer.getContent())
+                .build();
+    }
 }
