d1db0e060bba69f5d49aaa7edf4f6921608c7d23
coehgns <modooboiroo@gmail.com>
2025-12-22 17:30:21 +0900

    refactor (#124) :: QueryUserAlarmResponse new를 사용하여 객체 생성하도록 수정

--- src/main/java/team/jeonghokim/daedongyeojido/domain/alarm/presentation/dto/response/QueryUserAlarmResponse.java
+++ src/main/java/team/jeonghokim/daedongyeojido/domain/alarm/presentation/dto/response/QueryUserAlarmResponse.java
@@ -1,15 +1,10 @@
 package team.jeonghokim.daedongyeojido.domain.alarm.presentation.dto.response;
 
-import lombok.Builder;
-
 import java.util.List;
 
-@Builder
 public record QueryUserAlarmResponse(List<AlarmResponse> alarms) {
 
     public static QueryUserAlarmResponse from(List<AlarmResponse> alarms) {
-        return QueryUserAlarmResponse.builder()
-                .alarms(alarms)
-                .build();
+        return new QueryUserAlarmResponse(alarms);
     }
 }
