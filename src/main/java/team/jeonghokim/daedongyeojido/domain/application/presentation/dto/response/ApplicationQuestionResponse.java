b4c59b5d27d37f8a3368bce62827a239e7a0e227
coehgns <modooboiroo@gmail.com>
2025-12-22 17:06:48 +0900

    refactor (#124) :: ApplicationQuestionResponse 정적 팩토리 메서드 of -> from 수정

--- src/main/java/team/jeonghokim/daedongyeojido/domain/application/presentation/dto/response/ApplicationQuestionResponse.java
+++ src/main/java/team/jeonghokim/daedongyeojido/domain/application/presentation/dto/response/ApplicationQuestionResponse.java
@@ -1,15 +1,18 @@
 package team.jeonghokim.daedongyeojido.domain.application.presentation.dto.response;
 
+import lombok.Builder;
 import team.jeonghokim.daedongyeojido.domain.application.domain.ApplicationQuestion;
 
+@Builder
 public record ApplicationQuestionResponse(
         Long applicationQuestionId,
         String content
 ) {
-    public static ApplicationQuestionResponse of(ApplicationQuestion question) {
-        return new ApplicationQuestionResponse(
-                question.getId(),
-                question.getContent()
-        );
+
+    public static ApplicationQuestionResponse from(ApplicationQuestion question) {
+        return ApplicationQuestionResponse.builder()
+                .applicationQuestionId(question.getId())
+                .content(question.getContent())
+                .build();
     }
 }
