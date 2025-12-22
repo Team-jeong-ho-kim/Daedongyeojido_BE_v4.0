b0d7973bb1c0c2d79f3744d12458999581c3bc79
coehgns <modooboiroo@gmail.com>
2025-12-22 17:29:38 +0900

    refactor (#124) :: QueryAnnouncementListResponse new를 사용하여 객체 생성하도록 수정

--- src/main/java/team/jeonghokim/daedongyeojido/domain/announcement/presentation/dto/response/QueryAnnouncementListResponse.java
+++ src/main/java/team/jeonghokim/daedongyeojido/domain/announcement/presentation/dto/response/QueryAnnouncementListResponse.java
@@ -1,15 +1,10 @@
 package team.jeonghokim.daedongyeojido.domain.announcement.presentation.dto.response;
 
-import lombok.Builder;
-
 import java.util.List;
 
-@Builder
 public record QueryAnnouncementListResponse(List<AnnouncementResponse> announcements) {
 
     public static QueryAnnouncementListResponse from(List<AnnouncementResponse> announcements) {
-        return QueryAnnouncementListResponse.builder()
-                .announcements(announcements)
-                .build();
+        return new QueryAnnouncementListResponse(announcements);
     }
 }
