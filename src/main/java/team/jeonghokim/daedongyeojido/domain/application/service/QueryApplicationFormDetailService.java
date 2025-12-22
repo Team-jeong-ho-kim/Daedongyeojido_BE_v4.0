9b84990b00576202064a2b75377c5c1ed1468d9a
coehgns <modooboiroo@gmail.com>
2025-12-22 17:07:18 +0900

    refactor (#124) :: QueryApplicationFormDetailService 반환 dto 네이밍 수정

--- src/main/java/team/jeonghokim/daedongyeojido/domain/application/service/QueryApplicationFormDetailService.java
+++ src/main/java/team/jeonghokim/daedongyeojido/domain/application/service/QueryApplicationFormDetailService.java
@@ -4,22 +4,20 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import team.jeonghokim.daedongyeojido.domain.application.domain.ApplicationForm;
-import team.jeonghokim.daedongyeojido.domain.application.domain.repository.ApplicationFormRepository;
 import team.jeonghokim.daedongyeojido.domain.application.exception.ApplicationFormAccessDeniedException;
 import team.jeonghokim.daedongyeojido.domain.application.facade.ApplicationFormFacade;
-import team.jeonghokim.daedongyeojido.domain.application.presentation.dto.response.ApplicationFormDetailResponse;
+import team.jeonghokim.daedongyeojido.domain.application.presentation.dto.response.QueryApplicationFormDetailResponse;
 import team.jeonghokim.daedongyeojido.domain.user.domain.User;
 import team.jeonghokim.daedongyeojido.domain.user.facade.UserFacade;
 
 @Service
 @RequiredArgsConstructor
 public class QueryApplicationFormDetailService {
-    private final ApplicationFormRepository applicationFormRepository;
     private final ApplicationFormFacade applicationFormFacade;
     private final UserFacade userFacade;
 
     @Transactional(readOnly = true)
-    public ApplicationFormDetailResponse execute(Long applicationFormId) {
+    public QueryApplicationFormDetailResponse execute(Long applicationFormId) {
 
         User user = userFacade.getCurrentUser();
         ApplicationForm applicationForm = applicationFormFacade.getApplicationById(applicationFormId);
@@ -28,6 +26,6 @@ public class QueryApplicationFormDetailService {
             throw ApplicationFormAccessDeniedException.EXCEPTION;
         }
 
-        return ApplicationFormDetailResponse.of(applicationForm);
+        return QueryApplicationFormDetailResponse.from(applicationForm);
     }
 }
