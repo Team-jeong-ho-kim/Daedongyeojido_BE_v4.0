8b805459716f5d7c190794850e48416fd7e6b8db
coehgns <modooboiroo@gmail.com>
2025-12-22 17:30:15 +0900

    refactor (#124) :: QueryInterviewScheduleListResponse new를 사용하여 객체 생성하도록 수정

--- src/main/java/team/jeonghokim/daedongyeojido/domain/schedule/presentation/dto/response/QueryInterviewScheduleListResponse.java
+++ src/main/java/team/jeonghokim/daedongyeojido/domain/schedule/presentation/dto/response/QueryInterviewScheduleListResponse.java
@@ -1,15 +1,10 @@
 package team.jeonghokim.daedongyeojido.domain.schedule.presentation.dto.response;
 
-import lombok.Builder;
-
 import java.util.List;
 
-@Builder
 public record QueryInterviewScheduleListResponse(List<InterviewScheduleResponse> schedules) {
 
     public static QueryInterviewScheduleListResponse from(List<InterviewScheduleResponse> schedules) {
-        return QueryInterviewScheduleListResponse.builder()
-                .schedules(schedules)
-                .build();
+        return new QueryInterviewScheduleListResponse(schedules);
     }
 }
