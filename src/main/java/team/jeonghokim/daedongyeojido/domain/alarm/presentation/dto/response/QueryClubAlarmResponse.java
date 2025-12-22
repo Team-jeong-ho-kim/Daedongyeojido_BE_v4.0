eaa1fb0c761f8cddc9bba9f5f8fcdd8934c6e509
coehgns <modooboiroo@gmail.com>
2025-12-22 17:29:52 +0900

    refactor (#124) :: QueryClubAlarmResponse new를 사용하여 객체 생성하도록 수정

--- src/main/java/team/jeonghokim/daedongyeojido/domain/alarm/presentation/dto/response/QueryClubAlarmResponse.java
+++ src/main/java/team/jeonghokim/daedongyeojido/domain/alarm/presentation/dto/response/QueryClubAlarmResponse.java
@@ -1,15 +1,10 @@
 package team.jeonghokim.daedongyeojido.domain.alarm.presentation.dto.response;
 
-import lombok.Builder;
-
 import java.util.List;
 
-@Builder
 public record QueryClubAlarmResponse(List<AlarmResponse> alarms) {
 
     public static QueryClubAlarmResponse from(List<AlarmResponse> alarms) {
-        return QueryClubAlarmResponse.builder()
-                .alarms(alarms)
-                .build();
+        return new QueryClubAlarmResponse(alarms);
     }
 }
