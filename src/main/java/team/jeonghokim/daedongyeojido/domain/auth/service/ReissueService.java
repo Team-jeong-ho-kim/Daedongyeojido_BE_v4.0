3e34356c912e3e7ed66253908bf6c041511fbe2a
coehgns <modooboiroo@gmail.com>
2025-12-22 12:14:44 +0900

    refactor (#118) :: ReissueService

--- src/main/java/team/jeonghokim/daedongyeojido/domain/auth/service/ReissueService.java
+++ src/main/java/team/jeonghokim/daedongyeojido/domain/auth/service/ReissueService.java
@@ -3,7 +3,11 @@ package team.jeonghokim.daedongyeojido.domain.auth.service;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import team.jeonghokim.daedongyeojido.domain.auth.domain.repository.RefreshTokenRepository;
+import team.jeonghokim.daedongyeojido.domain.auth.exception.RefreshTokenNotFoundException;
 import team.jeonghokim.daedongyeojido.domain.auth.presentation.dto.response.TokenResponse;
+import team.jeonghokim.daedongyeojido.global.security.exception.InvalidTokenException;
+import team.jeonghokim.daedongyeojido.global.security.jwt.JwtProperties;
 import team.jeonghokim.daedongyeojido.global.security.jwt.JwtTokenProvider;
 
 @Service
@@ -11,9 +15,24 @@ import team.jeonghokim.daedongyeojido.global.security.jwt.JwtTokenProvider;
 public class ReissueService {
 
     private final JwtTokenProvider jwtTokenProvider;
+    private final RefreshTokenRepository refreshTokenRepository;
+    private final JwtProperties jwtProperties;
 
     @Transactional
     public TokenResponse execute(String refreshToken) {
-        return jwtTokenProvider.reissueToken(refreshToken);
+
+        if (jwtTokenProvider.isNotRefreshToken(refreshToken)) {
+            throw InvalidTokenException.EXCEPTION;
+        }
+
+        return refreshTokenRepository.findByRefreshToken(refreshToken)
+                .map(token -> {
+                    String accountId = token.getAccountId();
+                    TokenResponse tokenResponse = jwtTokenProvider.receiveToken(accountId);
+
+                    token.update(tokenResponse.refreshToken(), jwtProperties.getRefreshExpiration());
+                    return tokenResponse;
+                })
+                .orElseThrow(() -> RefreshTokenNotFoundException.EXCEPTION);
     }
 }
