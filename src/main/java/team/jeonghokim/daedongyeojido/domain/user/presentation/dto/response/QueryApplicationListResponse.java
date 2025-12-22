80d3193694284dedea21fb9c36a8742da9dd96d0
coehgns <modooboiroo@gmail.com>
2025-12-22 17:24:58 +0900

    refactor (#124) :: QueryApplicationListResponse new 사용하여 객체를 생성하도록 수정

--- src/main/java/team/jeonghokim/daedongyeojido/domain/user/presentation/dto/response/QueryApplicationListResponse.java
+++ src/main/java/team/jeonghokim/daedongyeojido/domain/user/presentation/dto/response/QueryApplicationListResponse.java
@@ -1,16 +1,12 @@
 package team.jeonghokim.daedongyeojido.domain.user.presentation.dto.response;
 
-import lombok.Builder;
 import team.jeonghokim.daedongyeojido.domain.application.presentation.dto.response.ApplicationListResponse;
 
 import java.util.List;
 
-@Builder
 public record QueryApplicationListResponse(List<ApplicationListResponse> applications) {
 
     public static QueryApplicationListResponse from(List<ApplicationListResponse> applications) {
-        return QueryApplicationListResponse.builder()
-                .applications(applications)
-                .build();
+        return new QueryApplicationListResponse(applications);
     }
 }
