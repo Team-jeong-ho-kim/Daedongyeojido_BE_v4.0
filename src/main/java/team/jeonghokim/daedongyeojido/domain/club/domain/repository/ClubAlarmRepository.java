f9264972518be4ba7a0b3c88256b7e99fda8c7b4
박태수 <taesupark0504@gmail.com>
2025-12-23 20:11:04 +0900

    refactor (#114) :: alarmRepositoryCustom 상속

--- src/main/java/team/jeonghokim/daedongyeojido/domain/club/domain/repository/ClubAlarmRepository.java
+++ src/main/java/team/jeonghokim/daedongyeojido/domain/club/domain/repository/ClubAlarmRepository.java
@@ -1,7 +1,8 @@
 package team.jeonghokim.daedongyeojido.domain.club.domain.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import team.jeonghokim.daedongyeojido.domain.alarm.domain.repository.AlarmRepositoryCustom;
 import team.jeonghokim.daedongyeojido.domain.club.domain.ClubAlarm;
 
-public interface ClubAlarmRepository extends JpaRepository<ClubAlarm, Long> {
+public interface ClubAlarmRepository extends JpaRepository<ClubAlarm, Long>, AlarmRepositoryCustom {
 }
--- src/main/java/team/jeonghokim/daedongyeojido/domain/user/domain/repository/UserAlarmRepository.java
+++ src/main/java/team/jeonghokim/daedongyeojido/domain/user/domain/repository/UserAlarmRepository.java
@@ -1,7 +1,8 @@
 package team.jeonghokim.daedongyeojido.domain.user.domain.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import team.jeonghokim.daedongyeojido.domain.alarm.domain.repository.AlarmRepositoryCustom;
 import team.jeonghokim.daedongyeojido.domain.user.domain.UserAlarm;
 
-public interface UserAlarmRepository extends JpaRepository<UserAlarm, Long> {
+public interface UserAlarmRepository extends JpaRepository<UserAlarm, Long>, AlarmRepositoryCustom {
 }
