59c10a606e5c1dbda673aca0edb36089e04c4681
박태수 <taesupark0504@gmail.com>
2025-12-23 20:07:04 +0900

    feat (#114) :: clubAlarmRepository

---
+++ src/main/java/team/jeonghokim/daedongyeojido/domain/club/domain/repository/ClubAlarmRepository.java
@@ -0,0 +1,7 @@
+package team.jeonghokim.daedongyeojido.domain.club.domain.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import team.jeonghokim.daedongyeojido.domain.club.domain.ClubAlarm;
+
+public interface ClubAlarmRepository extends JpaRepository<ClubAlarm, Long> {
+}
