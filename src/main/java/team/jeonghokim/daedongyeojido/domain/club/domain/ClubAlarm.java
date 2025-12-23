c7af01f575ede04ab6334c8de081b4bbe6598d4d
박태수 <taesupark0504@gmail.com>
2025-12-23 20:04:47 +0900

    feat (#114) :: clubAlarm

---
+++ src/main/java/team/jeonghokim/daedongyeojido/domain/club/domain/ClubAlarm.java
@@ -0,0 +1,28 @@
+package team.jeonghokim.daedongyeojido.domain.club.domain;
+
+import jakarta.persistence.*;
+import lombok.*;
+import team.jeonghokim.daedongyeojido.domain.alarm.domain.enums.AlarmType;
+import team.jeonghokim.daedongyeojido.global.entity.BaseTimeIdEntity;
+
+@Entity(name = "tbl_club_alarm")
+@Getter
+@Builder
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+public class ClubAlarm extends BaseTimeIdEntity {
+
+    @Column(length = 500, nullable = false)
+    private String title;
+
+    @Column(length = 300, nullable = false)
+    private String content;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "club_id", nullable = false)
+    private Club club;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private AlarmType alarmType;
+}
