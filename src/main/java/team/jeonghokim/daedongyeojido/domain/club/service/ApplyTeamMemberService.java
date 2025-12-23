a070b1b34705cfd9ea0d827e6b7b34a223145e86
박태수 <taesupark0504@gmail.com>
2025-12-23 14:59:32 +0900

    feat (#114) :: 팀원 추가 신청 시 알람 적용

--- src/main/java/team/jeonghokim/daedongyeojido/domain/club/service/ApplyTeamMemberService.java
+++ src/main/java/team/jeonghokim/daedongyeojido/domain/club/service/ApplyTeamMemberService.java
@@ -3,6 +3,9 @@ package team.jeonghokim.daedongyeojido.domain.club.service;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import team.jeonghokim.daedongyeojido.domain.alarm.domain.Alarm;
+import team.jeonghokim.daedongyeojido.domain.alarm.domain.enums.AlarmType;
+import team.jeonghokim.daedongyeojido.domain.club.domain.Club;
 import team.jeonghokim.daedongyeojido.domain.club.presentation.dto.request.TeamMemberRequest;
 import team.jeonghokim.daedongyeojido.domain.user.domain.User;
 import team.jeonghokim.daedongyeojido.domain.user.domain.UserApplication;
@@ -30,5 +33,19 @@ public class ApplyTeamMemberService {
                 .isApproved(false)
                 .club(user.getClub())
                 .build());
+
+        createAlarm(user.getClub(), userApplication);
+    }
+
+    private void createAlarm(Club club, User userApplication) {
+        Alarm alarm = Alarm.builder()
+                .title(AlarmType.CLUB_MEMBER_APPLY.getTitle())
+                .content(AlarmType.CLUB_MEMBER_APPLY.format(club.getClubName()))
+                .club(club)
+                .receiver(userApplication)
+                .alarmType(AlarmType.CLUB_MEMBER_APPLY)
+                .build();
+
+        userApplication.getAlarms().add(alarm);
     }
 }
