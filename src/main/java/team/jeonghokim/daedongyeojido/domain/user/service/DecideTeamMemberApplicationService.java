805e0962036d1864867c1f7008468df18e3ac760
박태수 <taesupark0504@gmail.com>
2025-12-23 16:10:57 +0900

    feat (#114) :: 동아리 팀원 합류 여부 결정 api 알람 적용

--- src/main/java/team/jeonghokim/daedongyeojido/domain/user/service/DecideTeamMemberApplicationService.java
+++ src/main/java/team/jeonghokim/daedongyeojido/domain/user/service/DecideTeamMemberApplicationService.java
@@ -3,6 +3,9 @@ package team.jeonghokim.daedongyeojido.domain.user.service;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import team.jeonghokim.daedongyeojido.domain.alarm.domain.Alarm;
+import team.jeonghokim.daedongyeojido.domain.alarm.domain.enums.AlarmType;
+import team.jeonghokim.daedongyeojido.domain.club.domain.Club;
 import team.jeonghokim.daedongyeojido.domain.user.domain.User;
 import team.jeonghokim.daedongyeojido.domain.user.domain.UserApplication;
 import team.jeonghokim.daedongyeojido.domain.user.domain.repository.UserApplicationRepository;
@@ -26,6 +29,33 @@ public class DecideTeamMemberApplicationService {
         if (request.getIsApproved()) {
             user.approvedTeamMember(userApplication.getClub());
             userApplication.approved();
+            joinClub(userApplication.getClub(), user);
+        } else {
+            refuseClub(userApplication.getClub(), user);
         }
+    }
+
+    private void joinClub(Club club, User user) {
+        Alarm alarm = Alarm.builder()
+                .title(AlarmType.USER_JOINED_CLUB.getTitle())
+                .content(AlarmType.USER_JOINED_CLUB.format(user.getUserName()))
+                .club(club)
+                .receiver(user)
+                .alarmType(AlarmType.USER_JOINED_CLUB)
+                .build();
+
+        club.getAlarms().add(alarm);
+    }
+
+    private void refuseClub(Club club, User user) {
+        Alarm alarm = Alarm.builder()
+                .title(AlarmType.USER_REFUSED_CLUB.getTitle())
+                .content(AlarmType.USER_REFUSED_CLUB.format(user.getUserName()))
+                .club(club)
+                .receiver(user)
+                .alarmType(AlarmType.USER_REFUSED_CLUB)
+                .build();
+
+        club.getAlarms().add(alarm);
     }
 }
