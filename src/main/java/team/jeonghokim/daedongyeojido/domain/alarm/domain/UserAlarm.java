c24d0a4d669b47b1d3da3825f7816d9da8cbf28b
박태수 <taesupark0504@gmail.com>
2025-12-26 08:32:47 +0900

    refactor (#114) :: userAlarm review 적용

--- src/main/java/team/jeonghokim/daedongyeojido/domain/alarm/domain/UserAlarm.java
+++ src/main/java/team/jeonghokim/daedongyeojido/domain/alarm/domain/UserAlarm.java
@@ -7,10 +7,9 @@ import team.jeonghokim.daedongyeojido.domain.user.domain.User;
 import team.jeonghokim.daedongyeojido.global.entity.BaseTimeIdEntity;
 
 @Entity
+@Table(name = "tbl_user_alarm")
 @Getter
-@Builder
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
-@AllArgsConstructor(access = AccessLevel.PRIVATE)
 public class UserAlarm extends BaseTimeIdEntity {
 
     @Column(length = 50, nullable = false)
@@ -26,4 +25,12 @@ public class UserAlarm extends BaseTimeIdEntity {
     @Enumerated(EnumType.STRING)
     @Column(nullable = false)
     private AlarmType alarmType;
+
+    @Builder
+    public UserAlarm(String title, String content, User receiver, AlarmType alarmType) {
+        this.title = title;
+        this.content = content;
+        this.receiver = receiver;
+        this.alarmType = alarmType;
+    }
 }
