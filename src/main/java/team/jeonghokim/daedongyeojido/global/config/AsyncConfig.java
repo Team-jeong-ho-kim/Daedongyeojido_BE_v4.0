50813a316c48a42f491c127ec34beec3b63453eb
박태수 <taesupark0504@gmail.com>
2025-12-29 00:26:07 +0900

    feat (#129) :: AsyncConfig

---
+++ src/main/java/team/jeonghokim/daedongyeojido/global/config/AsyncConfig.java
@@ -0,0 +1,9 @@
+package team.jeonghokim.daedongyeojido.global.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableAsync;
+
+@Configuration
+@EnableAsync
+public class AsyncConfig {
+}
