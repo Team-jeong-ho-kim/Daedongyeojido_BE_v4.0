0d10d51281eb799dbd722acf1346caa1e12cc5c7
박태수 <taesupark0504@gmail.com>
2025-12-29 08:36:59 +0900

    refactor (#129) :: ThreadPoolTaskExecutor 기반 비동기 작업 커스텀

--- src/main/java/team/jeonghokim/daedongyeojido/global/config/AsyncConfig.java
+++ src/main/java/team/jeonghokim/daedongyeojido/global/config/AsyncConfig.java
@@ -1,9 +1,28 @@
 package team.jeonghokim.daedongyeojido.global.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.AsyncConfigurer;
 import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
+
+import java.util.concurrent.Executor;
 
 @Configuration
 @EnableAsync
-public class AsyncConfig {
+public class AsyncConfig implements AsyncConfigurer {
+
+    private static final String PREFIX = "Async-";
+
+    @Override
+    public Executor getAsyncExecutor() {
+        ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
+        executor.setCorePoolSize(5);
+        executor.setMaxPoolSize(10);
+        executor.setQueueCapacity(5);
+        executor.setThreadNamePrefix(PREFIX);
+        executor.setWaitForTasksToCompleteOnShutdown(true);
+        executor.setAwaitTerminationSeconds(60);
+        executor.initialize();
+        return executor;
+    }
 }
