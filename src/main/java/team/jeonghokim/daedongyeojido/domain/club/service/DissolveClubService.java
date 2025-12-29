3625a1d2f46d8e8a454d8fe7478a0147aa60259f
박태수 <taesupark0504@gmail.com>
2025-12-29 12:23:18 +0900

    refactor (#129) :: 동아리 해체 신청 API event 기반 알림 적용

--- src/main/java/team/jeonghokim/daedongyeojido/domain/club/service/DissolveClubService.java
+++ src/main/java/team/jeonghokim/daedongyeojido/domain/club/service/DissolveClubService.java
@@ -1,15 +1,15 @@
 package team.jeonghokim.daedongyeojido.domain.club.service;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import team.jeonghokim.daedongyeojido.domain.alarm.domain.enums.AlarmType;
 import team.jeonghokim.daedongyeojido.domain.club.domain.Club;
 import team.jeonghokim.daedongyeojido.domain.club.exception.UserNotInClubException;
 import team.jeonghokim.daedongyeojido.domain.user.domain.User;
-import team.jeonghokim.daedongyeojido.domain.alarm.domain.UserAlarm;
-import team.jeonghokim.daedongyeojido.domain.alarm.domain.repository.UserAlarmRepository;
 import team.jeonghokim.daedongyeojido.domain.user.facade.UserFacade;
+import team.jeonghokim.daedongyeojido.infrastructure.event.domain.user.UserAlarmEvent;
 
 import java.util.Optional;
 
@@ -18,7 +18,7 @@ import java.util.Optional;
 public class DissolveClubService {
 
     private final UserFacade userFacade;
-    private final UserAlarmRepository userAlarmRepository;
+    private final ApplicationEventPublisher eventPublisher;
 
     @Transactional
     public void execute() {
@@ -27,13 +27,11 @@ public class DissolveClubService {
         Club club = Optional.ofNullable(receiver.getClub())
                 .orElseThrow(() -> UserNotInClubException.EXCEPTION);
 
-        UserAlarm alarm = UserAlarm.builder()
-                .title(AlarmType.DISSOLVE_CLUB_APPLY.formatTitle(club.getClubName()))
-                .content(AlarmType.DISSOLVE_CLUB_APPLY.formatContent(club.getClubName()))
-                .receiver(receiver)
-                .alarmType(AlarmType.DISSOLVE_CLUB_APPLY)
-                .build();
-
-        userAlarmRepository.save(alarm);
+        eventPublisher.publishEvent(UserAlarmEvent.builder()
+                        .title(AlarmType.DISSOLVE_CLUB_APPLY.formatTitle(club.getClubName()))
+                        .content(AlarmType.DISSOLVE_CLUB_APPLY.formatContent(club.getClubName()))
+                        .userId(receiver.getId())
+                        .alarmType(AlarmType.DISSOLVE_CLUB_APPLY)
+                .build());
     }
 }
