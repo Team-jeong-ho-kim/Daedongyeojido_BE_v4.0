fffdd4511b13e6d13bc91f039e886fc0983ac65c
박태수 <taesupark0504@gmail.com>
2025-12-29 16:21:34 +0900

    chore (#129) :: sms, alarm 패키지로 listener 분리

--- src/main/java/team/jeonghokim/daedongyeojido/infrastructure/event/listner/alarm/AlarmEventListener.java
+++ src/main/java/team/jeonghokim/daedongyeojido/infrastructure/event/listner/alarm/AlarmEventListener.java
@@ -1,4 +1,4 @@
-package team.jeonghokim.daedongyeojido.infrastructure.event.listner;
+package team.jeonghokim.daedongyeojido.infrastructure.event.listner.alarm;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
