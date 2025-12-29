94504ebe1d1e285d0a9981e416dac93fed147083
박태수 <taesupark0504@gmail.com>
2025-12-29 11:54:41 +0900

    feat (#129) :: SmsEventListener

---
+++ src/main/java/team/jeonghokim/daedongyeojido/infrastructure/event/listner/SmsEventListener.java
@@ -0,0 +1,35 @@
+package team.jeonghokim.daedongyeojido.infrastructure.event.listner;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.scheduling.annotation.Async;
+import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.transaction.event.TransactionPhase;
+import org.springframework.transaction.event.TransactionalEventListener;
+import team.jeonghokim.daedongyeojido.infrastructure.event.domain.user.UserSmsEvent;
+import team.jeonghokim.daedongyeojido.infrastructure.sms.service.SmsService;
+
+@Slf4j
+@Component
+@RequiredArgsConstructor
+public class SmsEventListener {
+    private final SmsService smsService;
+
+    @Async
+    @TransactionalEventListener(phase = TransactionPhase.AFTER_COMMIT)
+    @Transactional(propagation = Propagation.REQUIRES_NEW)
+    public void handleUserSmsEvent(UserSmsEvent event) {
+        try {
+            smsService.send(
+                    event.phoneNumber(),
+                    event.message(),
+                    event.clubName()
+            );
+        } catch (Exception e) {
+            log.error("유저 SMS 이벤트 실패: phoneNumber={} message={}",
+                    event.phoneNumber(), event.message(), e);
+        }
+    }
+}
