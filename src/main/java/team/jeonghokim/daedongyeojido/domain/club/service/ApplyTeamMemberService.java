e830b77d8e12f1055da5ab0d5809d65d6de10d0b
박태수 <taesupark0504@gmail.com>
2025-12-29 12:21:26 +0900

    refactor (#129) :: 팀원 추가 신청 API event 기반 알림 적용

--- src/main/java/team/jeonghokim/daedongyeojido/domain/club/service/ApplyTeamMemberService.java
+++ src/main/java/team/jeonghokim/daedongyeojido/domain/club/service/ApplyTeamMemberService.java
@@ -1,18 +1,19 @@
 package team.jeonghokim.daedongyeojido.domain.club.service;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import team.jeonghokim.daedongyeojido.domain.alarm.domain.enums.AlarmType;
 import team.jeonghokim.daedongyeojido.domain.club.domain.Club;
 import team.jeonghokim.daedongyeojido.domain.club.presentation.dto.request.TeamMemberRequest;
 import team.jeonghokim.daedongyeojido.domain.user.domain.User;
-import team.jeonghokim.daedongyeojido.domain.alarm.domain.UserAlarm;
 import team.jeonghokim.daedongyeojido.domain.user.domain.UserApplication;
 import team.jeonghokim.daedongyeojido.domain.user.domain.repository.UserApplicationRepository;
 import team.jeonghokim.daedongyeojido.domain.user.domain.repository.UserRepository;
 import team.jeonghokim.daedongyeojido.domain.user.exception.UserNotFoundException;
 import team.jeonghokim.daedongyeojido.domain.user.facade.UserFacade;
+import team.jeonghokim.daedongyeojido.infrastructure.event.domain.user.UserAlarmEvent;
 
 @Service
 @RequiredArgsConstructor
@@ -20,6 +21,7 @@ public class ApplyTeamMemberService {
     private final UserApplicationRepository userApplicationRepository;
     private final UserRepository userRepository;
     private final UserFacade userFacade;
+    private final ApplicationEventPublisher eventPublisher;
 
     @Transactional
     public void execute(TeamMemberRequest request) {
@@ -38,13 +40,11 @@ public class ApplyTeamMemberService {
     }
 
     private void createAlarm(Club club, User userApplication) {
-        UserAlarm alarm = UserAlarm.builder()
-                .title(AlarmType.CLUB_MEMBER_APPLY.formatTitle(club.getClubName()))
-                .content(AlarmType.CLUB_MEMBER_APPLY.formatContent(club.getClubName()))
-                .receiver(userApplication)
-                .alarmType(AlarmType.CLUB_MEMBER_APPLY)
-                .build();
-
-        userApplication.getAlarms().add(alarm);
+        eventPublisher.publishEvent(UserAlarmEvent.builder()
+                        .title(AlarmType.CLUB_MEMBER_APPLY.formatTitle(club.getClubName()))
+                        .content(AlarmType.CLUB_MEMBER_APPLY.formatContent(club.getClubName()))
+                        .userId(userApplication.getId())
+                        .alarmType(AlarmType.CLUB_MEMBER_APPLY)
+                .build());
     }
 }
