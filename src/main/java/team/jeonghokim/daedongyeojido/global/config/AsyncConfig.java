7570fd571895dda1936c62bcf5153ee5728a8269
박태수 <taesupark0504@gmail.com>
2025-12-29 15:43:43 +0900

    refactor (#129) :: @EnableRetry 어노테이션 추가

--- src/main/java/team/jeonghokim/daedongyeojido/global/config/AsyncConfig.java
+++ src/main/java/team/jeonghokim/daedongyeojido/global/config/AsyncConfig.java
@@ -1,6 +1,7 @@
 package team.jeonghokim.daedongyeojido.global.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.retry.annotation.EnableRetry;
 import org.springframework.scheduling.annotation.AsyncConfigurer;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
@@ -9,6 +10,7 @@ import java.util.concurrent.Executor;
 
 @Configuration
 @EnableAsync
+@EnableRetry
 public class AsyncConfig implements AsyncConfigurer {
 
     private static final String PREFIX = "Async-";
