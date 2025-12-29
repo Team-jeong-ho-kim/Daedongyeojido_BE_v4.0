8d4b8eaf96cd0dca7ff5be6ac19c3d5d080504c8
박태수 <taesupark0504@gmail.com>
2025-12-29 22:26:45 +0900

    refactor (#129) :: 기본 SMS handle 메서드 Retry 적용

--- src/main/java/team/jeonghokim/daedongyeojido/infrastructure/event/listener/sms/SmsEventListener.java
+++ src/main/java/team/jeonghokim/daedongyeojido/infrastructure/event/listener/sms/SmsEventListener.java
@@ -3,14 +3,21 @@ package team.jeonghokim.daedongyeojido.infrastructure.event.listener.sms;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.retry.annotation.Backoff;
+import org.springframework.retry.annotation.Recover;
+import org.springframework.retry.annotation.Retryable;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.transaction.event.TransactionPhase;
 import org.springframework.transaction.event.TransactionalEventListener;
+import team.jeonghokim.daedongyeojido.infrastructure.event.domain.club.ClubAlarmEvent;
 import team.jeonghokim.daedongyeojido.infrastructure.event.domain.user.LargeScaleSmsEvent;
 import team.jeonghokim.daedongyeojido.infrastructure.event.domain.user.UserSmsEvent;
+import team.jeonghokim.daedongyeojido.infrastructure.event.exception.AlarmEventFinalFailedException;
+import team.jeonghokim.daedongyeojido.infrastructure.event.exception.HttpApiException;
+import team.jeonghokim.daedongyeojido.infrastructure.event.exception.SmsEventFinalFailedException;
 import team.jeonghokim.daedongyeojido.infrastructure.scheduler.payload.SchedulerPayload;
 import team.jeonghokim.daedongyeojido.infrastructure.sms.service.SmsService;
 
@@ -23,7 +30,14 @@ public class SmsEventListener {
     private final SmsService smsService;
     private final RedisTemplate<String, SchedulerPayload> smsRedisTemplate;
 
+    private static final String SMS_EVENT_RETRY = "recoverSmsEvent";
+
     @Async
+    @Retryable(
+            retryFor = HttpApiException.class,
+            recover = SMS_EVENT_RETRY,
+            backoff = @Backoff(delay = 500, multiplier = 2)
+    )
     @TransactionalEventListener(phase = TransactionPhase.AFTER_COMMIT)
     @Transactional(propagation = Propagation.REQUIRES_NEW)
     public void handleUserSmsEvent(UserSmsEvent event) {
@@ -35,8 +49,9 @@ public class SmsEventListener {
             );
 
         } catch (Exception e) {
-            log.error("유저 SMS 이벤트 실패: phoneNumber={} message={}",
+            log.warn("유저 SMS 이벤트 실패: phoneNumber={} message={}",
                     event.phoneNumber(), event.message(), e);
+            throw new HttpApiException(e);
         }
     }
 
@@ -58,5 +73,13 @@ public class SmsEventListener {
             log.error("유저 SMS 이벤트 실패: phoneNumber={} message={}",
                     event.phoneNumber(), event.message(), e);
         }
+    }
+
+    @Recover
+    public void recoverSmsEvent(HttpApiException e, UserSmsEvent event) {
+        log.error("SMS 이벤트 최종 실패: phoneNumber={} message={}",
+                event.phoneNumber(), event.message(), e);
+
+        throw new SmsEventFinalFailedException();
     }
 }
