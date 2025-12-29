c930675a6ad35656375ae5c510c76d11bdfd532b
박태수 <taesupark0504@gmail.com>
2025-12-29 22:18:55 +0900

    feat (#129) :: LargeScaleSmsEvent

---
+++ src/main/java/team/jeonghokim/daedongyeojido/infrastructure/event/domain/user/LargeScaleSmsEvent.java
@@ -0,0 +1,14 @@
+package team.jeonghokim.daedongyeojido.infrastructure.event.domain.user;
+
+import lombok.Builder;
+import team.jeonghokim.daedongyeojido.infrastructure.scheduler.payload.SchedulerPayload;
+import team.jeonghokim.daedongyeojido.infrastructure.sms.type.Message;
+
+@Builder
+public record LargeScaleSmsEvent(
+        String phoneNumber,
+        Message message,
+        String clubName,
+        SchedulerPayload payload
+) {
+}
