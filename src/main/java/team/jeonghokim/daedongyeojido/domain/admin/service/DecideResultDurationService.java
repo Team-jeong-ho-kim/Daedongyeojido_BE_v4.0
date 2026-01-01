231807a07622be471f7f738da86ed7f65f3c98b3
박태수 <taesupark0504@gmail.com>
2026-01-01 15:59:11 +0900

    refactor (#138) :: 발표 기간 설정 시 taskScheduler에 발표기간 등록

--- src/main/java/team/jeonghokim/daedongyeojido/domain/admin/service/DecideResultDurationService.java
+++ src/main/java/team/jeonghokim/daedongyeojido/domain/admin/service/DecideResultDurationService.java
@@ -1,18 +1,27 @@
 package team.jeonghokim.daedongyeojido.domain.admin.service;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.scheduling.TaskScheduler;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import team.jeonghokim.daedongyeojido.domain.admin.presentation.dto.request.DecideResultDurationRequest;
 import team.jeonghokim.daedongyeojido.domain.resultduration.domain.ResultDuration;
 import team.jeonghokim.daedongyeojido.domain.resultduration.domain.repository.ResultDurationRepository;
 import team.jeonghokim.daedongyeojido.domain.resultduration.exception.ResultDurationAlreadySetException;
+import team.jeonghokim.daedongyeojido.infrastructure.scheduler.service.SchedulerService;
+
+import java.time.Instant;
+import java.time.ZoneId;
 
 @Service
 @RequiredArgsConstructor
 public class DecideResultDurationService {
 
     private final ResultDurationRepository resultDurationRepository;
+    private final TaskScheduler taskScheduler;
+    private final SchedulerService schedulerService;
+
+    private static final String TIME_ZONE = "Asia/Seoul";
 
     @Transactional
     public void execute(DecideResultDurationRequest request) {
@@ -21,6 +30,13 @@ public class DecideResultDurationService {
             throw ResultDurationAlreadySetException.EXCEPTION;
         }
 
-        resultDurationRepository.save(new ResultDuration(request.resultDuration()));
+        ResultDuration resultDuration = resultDurationRepository
+                .save(new ResultDuration(request.resultDuration()));
+
+        Instant executeTime = resultDuration.getResultDuration()
+                .atZone(ZoneId.of(TIME_ZONE))
+                .toInstant();
+
+        taskScheduler.schedule(schedulerService::execute, executeTime);
     }
 }
