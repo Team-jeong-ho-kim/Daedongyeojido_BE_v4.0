4ba8f18a5a3360e08240cd32f8c7e244f3e4d81e
박태수 <taesupark0504@gmail.com>
2026-01-05 17:19:09 +0900

    chore (#130) :: 메서드 규칙 맞추기

--- src/main/java/team/jeonghokim/daedongyeojido/infrastructure/sms/type/Message.java
+++ src/main/java/team/jeonghokim/daedongyeojido/infrastructure/sms/type/Message.java
@@ -6,6 +6,7 @@ import lombok.Getter;
 @Getter
 @AllArgsConstructor
 public enum Message {
+
     INTERVIEW_SCHEDULE_DECIDED("면접 일정 안내", "%s 동아리 면접 일정이 확정되었습니다. 자세한 면접 일정은 대동여지도에 방문하셔서 마이페이지/지원내역/면접일정조회 에서 확인해주세요."),
     INTERVIEW_SCHEDULE_CHANGED("면접 일정 변경", "%s 동아리 면접 일정이 변경되었습니다. 자세한 변경 사항은 대동여지도에 방문하셔서 마이페이지/지원내역/면접일정조회 에서 확인해주세요."),
     CLUB_FINAL_ACCEPTED("동아리 최종 합격", "축하드립니다! %s 동아리에 최종 합격하였습니다. 대동여지도에 방문하셔서 알림페이지에서 해당 동아리 합류 여부를 결정해주세요!"),
@@ -15,6 +16,7 @@ public enum Message {
     private final String content;
 
     public String format(Object... args) {
+
         return content.formatted(args);
     }
 }
