2653b358bcad19543931cc4a3bec1f2bc5de2b24
박태수 <taesupark0504@gmail.com>
2026-01-05 17:11:08 +0900

    feat (#130) :: 면접 일정 수정 api SMS 이벤트 발행

--- src/main/java/team/jeonghokim/daedongyeojido/domain/schedule/service/UpdateInterviewScheduleService.java
+++ src/main/java/team/jeonghokim/daedongyeojido/domain/schedule/service/UpdateInterviewScheduleService.java
@@ -1,8 +1,10 @@
 package team.jeonghokim.daedongyeojido.domain.schedule.service;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import team.jeonghokim.daedongyeojido.domain.club.domain.Club;
 import team.jeonghokim.daedongyeojido.domain.schedule.domain.Schedule;
 import team.jeonghokim.daedongyeojido.domain.schedule.domain.repository.ScheduleRepository;
 import team.jeonghokim.daedongyeojido.domain.schedule.exception.InterviewScheduleAccessDeniedException;
@@ -10,6 +12,8 @@ import team.jeonghokim.daedongyeojido.domain.schedule.exception.InterviewSchedul
 import team.jeonghokim.daedongyeojido.domain.schedule.presentation.dto.request.InterviewScheduleRequest;
 import team.jeonghokim.daedongyeojido.domain.user.domain.User;
 import team.jeonghokim.daedongyeojido.domain.user.facade.UserFacade;
+import team.jeonghokim.daedongyeojido.infrastructure.event.domain.user.UserSmsEvent;
+import team.jeonghokim.daedongyeojido.infrastructure.sms.type.Message;
 
 @Service
 @RequiredArgsConstructor
@@ -17,19 +21,31 @@ public class UpdateInterviewScheduleService {
 
     private final ScheduleRepository scheduleRepository;
     private final UserFacade userFacade;
+    private final ApplicationEventPublisher applicationEventPublisher;
 
     @Transactional
     public void execute(Long scheduleId, InterviewScheduleRequest request) {
 
-        User currentUser = userFacade.getCurrentUser();
+        User interviewer = userFacade.getCurrentUser();
 
         Schedule schedule = scheduleRepository.findById(scheduleId)
                 .orElseThrow(() -> InterviewScheduleNotFoundException.EXCEPTION);
 
-        if (!(currentUser.getClub().getId().equals(schedule.getClub().getId()))) {
+        if (!(interviewer.getClub().getId().equals(schedule.getClub().getId()))) {
             throw InterviewScheduleAccessDeniedException.EXCEPTION;
         }
 
         schedule.updateSchedule(request);
+
+        executeScheduleSMS(interviewer.getClub(), schedule.getApplicant());
+    }
+
+    private void executeScheduleSMS(Club club, User user) {
+
+        applicationEventPublisher.publishEvent(UserSmsEvent.builder()
+                        .clubName(club.getClubName())
+                        .message(Message.INTERVIEW_SCHEDULE_CHANGED)
+                        .phoneNumber(user.getPhoneNumber())
+                .build());
     }
 }
