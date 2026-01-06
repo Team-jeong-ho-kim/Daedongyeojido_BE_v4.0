93c609b59bce0cbbf4917af915ec4cdd69f6aa0b
박태수 <taesupark0504@gmail.com>
2026-01-06 17:16:46 +0900

    chore (#138) :: resultDuration status적용

--- src/main/java/team/jeonghokim/daedongyeojido/domain/resultduration/domain/ResultDuration.java
+++ src/main/java/team/jeonghokim/daedongyeojido/domain/resultduration/domain/ResultDuration.java
@@ -1,9 +1,12 @@
 package team.jeonghokim.daedongyeojido.domain.resultduration.domain;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import team.jeonghokim.daedongyeojido.domain.resultduration.domain.enums.Status;
 import team.jeonghokim.daedongyeojido.global.entity.BaseIdEntity;
 
 import java.time.LocalDateTime;
@@ -15,8 +18,12 @@ public class ResultDuration extends BaseIdEntity {
 
     private LocalDateTime resultDuration;
 
+    @Enumerated(EnumType.STRING)
+    private Status status;
+
     public ResultDuration(LocalDateTime resultDuration) {
         this.resultDuration = resultDuration;
+        this.status = Status.PENDING;
     }
 
     public void update(LocalDateTime resultDuration) {
