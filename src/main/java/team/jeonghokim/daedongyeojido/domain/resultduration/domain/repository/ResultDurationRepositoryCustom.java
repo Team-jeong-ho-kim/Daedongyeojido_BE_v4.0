3a72cec78183fbe4313760843da9d198aacbfc25
박태수 <taesupark0504@gmail.com>
2026-01-06 20:08:58 +0900

    feat (#138) :: findPendingResultDuration

--- src/main/java/team/jeonghokim/daedongyeojido/domain/resultduration/domain/repository/ResultDurationRepositoryCustom.java
+++ src/main/java/team/jeonghokim/daedongyeojido/domain/resultduration/domain/repository/ResultDurationRepositoryCustom.java
@@ -1,4 +1,10 @@
 package team.jeonghokim.daedongyeojido.domain.resultduration.domain.repository;
 
+import team.jeonghokim.daedongyeojido.domain.resultduration.domain.ResultDuration;
+
+import java.util.Optional;
+
 public interface ResultDurationRepositoryCustom {
+
+    Optional<ResultDuration> findPendingResultDuration();
 }
--- src/main/java/team/jeonghokim/daedongyeojido/domain/resultduration/domain/repository/ResultDurationRepositoryCustomImpl.java
+++ src/main/java/team/jeonghokim/daedongyeojido/domain/resultduration/domain/repository/ResultDurationRepositoryCustomImpl.java
@@ -2,8 +2,25 @@ package team.jeonghokim.daedongyeojido.domain.resultduration.domain.repository;
 
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import lombok.RequiredArgsConstructor;
+import team.jeonghokim.daedongyeojido.domain.resultduration.domain.QResultDuration;
+import team.jeonghokim.daedongyeojido.domain.resultduration.domain.ResultDuration;
+import team.jeonghokim.daedongyeojido.domain.resultduration.domain.enums.Status;
+
+import java.util.Optional;
 
 @RequiredArgsConstructor
 public class ResultDurationRepositoryCustomImpl implements ResultDurationRepositoryCustom {
-    private final JPAQueryFactory queryFactory;
+
+    private final JPAQueryFactory jpaQueryFactory;
+    private final QResultDuration resultDuration = QResultDuration.resultDuration;
+
+    @Override
+    public Optional<ResultDuration> findPendingResultDuration() {
+        return Optional.ofNullable(
+                jpaQueryFactory
+                        .selectFrom(resultDuration)
+                        .where(resultDuration.status.eq(Status.PENDING))
+                        .fetchOne()
+        );
+    }
 }
