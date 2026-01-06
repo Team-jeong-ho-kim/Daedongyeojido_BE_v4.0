f355b64b771d5092ad72b59f2a86e362baf35617
박태수 <taesupark0504@gmail.com>
2026-01-06 17:17:08 +0900

    feat (#138) :: existsByStatus 메서드

--- src/main/java/team/jeonghokim/daedongyeojido/domain/resultduration/domain/repository/ResultDurationRepository.java
+++ src/main/java/team/jeonghokim/daedongyeojido/domain/resultduration/domain/repository/ResultDurationRepository.java
@@ -2,6 +2,7 @@ package team.jeonghokim.daedongyeojido.domain.resultduration.domain.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import team.jeonghokim.daedongyeojido.domain.resultduration.domain.ResultDuration;
+import team.jeonghokim.daedongyeojido.domain.resultduration.domain.enums.Status;
 import team.jeonghokim.daedongyeojido.domain.resultduration.presentation.dto.response.ResultDurationResponse;
 
 import java.util.Optional;
@@ -11,4 +12,6 @@ public interface ResultDurationRepository extends JpaRepository<ResultDuration, 
     Optional<ResultDuration> findTopByOrderByIdDesc();
 
     Optional<ResultDurationResponse> findFirstBy();
+
+    Boolean existsByStatus(Status status);
 }
