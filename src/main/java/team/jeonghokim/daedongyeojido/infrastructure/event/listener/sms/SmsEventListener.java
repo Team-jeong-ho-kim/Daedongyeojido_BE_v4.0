55af5b8d862e51727dd34944381379076ccf3a8b
박태수 <taesupark0504@gmail.com>
2026-01-06 19:30:25 +0900

    feat (#138) :: initSmsEvent

--- src/main/java/team/jeonghokim/daedongyeojido/infrastructure/event/listener/sms/SmsEventListener.java
+++ src/main/java/team/jeonghokim/daedongyeojido/infrastructure/event/listener/sms/SmsEventListener.java
@@ -2,11 +2,13 @@ package team.jeonghokim.daedongyeojido.infrastructure.event.listener.sms;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
 import org.springframework.context.event.EventListener;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.retry.annotation.Backoff;
 import org.springframework.retry.annotation.Recover;
 import org.springframework.retry.annotation.Retryable;
+import org.springframework.scheduling.TaskScheduler;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.annotation.Propagation;
@@ -15,12 +17,19 @@ import org.springframework.transaction.event.TransactionPhase;
 import org.springframework.transaction.event.TransactionalEventListener;
 import org.springframework.web.client.HttpServerErrorException;
 import org.springframework.web.client.ResourceAccessException;
+import team.jeonghokim.daedongyeojido.domain.resultduration.domain.ResultDuration;
+import team.jeonghokim.daedongyeojido.domain.resultduration.domain.repository.ResultDurationRepository;
+import team.jeonghokim.daedongyeojido.domain.resultduration.exception.ResultDurationNotFoundException;
 import team.jeonghokim.daedongyeojido.infrastructure.event.domain.user.LargeScaleSmsEvent;
 import team.jeonghokim.daedongyeojido.infrastructure.event.domain.user.UserSmsEvent;
 import team.jeonghokim.daedongyeojido.infrastructure.event.exception.HttpApiException;
 import team.jeonghokim.daedongyeojido.infrastructure.event.exception.SmsEventFinalFailedException;
 import team.jeonghokim.daedongyeojido.infrastructure.scheduler.payload.SchedulerPayload;
+import team.jeonghokim.daedongyeojido.infrastructure.scheduler.service.SchedulerService;
 import team.jeonghokim.daedongyeojido.infrastructure.sms.service.SmsService;
+
+import java.time.Instant;
+import java.time.ZoneId;
 
 import static team.jeonghokim.daedongyeojido.infrastructure.scheduler.service.SchedulerService.RESULT_DURATION_ZSET;
 
@@ -28,11 +37,16 @@ import static team.jeonghokim.daedongyeojido.infrastructure.scheduler.service.Sc
 @Component
 @RequiredArgsConstructor
 public class SmsEventListener {
+
     private final SmsService smsService;
     private final RedisTemplate<String, SchedulerPayload> smsRedisTemplate;
+    private final SchedulerService schedulerService;
+    private final ResultDurationRepository resultDurationRepository;
+    private final TaskScheduler taskScheduler;
 
     private static final String SMS_EVENT_RETRY = "recoverSmsEvent";
     private static final String LARGE_SCALE_EVENT_RETRY = "recoverLargeScaleSmsEvent";
+    private static final String TIME_ZONE = "Asia/Seoul";
 
     @Async
     @Retryable(
@@ -89,6 +103,19 @@ public class SmsEventListener {
 
             throw new HttpApiException(e);
         }
+    }
+
+    @EventListener(ApplicationReadyEvent.class)
+    public void initSmsSchedule() {
+
+        ResultDuration resultDuration = resultDurationRepository.findTopByOrderByIdDesc()
+                .orElseThrow(() -> ResultDurationNotFoundException.EXCEPTION);
+
+        Instant executeTime = resultDuration.getResultDuration()
+                .atZone(ZoneId.of(TIME_ZONE))
+                .toInstant();
+
+        taskScheduler.schedule(schedulerService::execute, executeTime);
     }
 
     @Recover
