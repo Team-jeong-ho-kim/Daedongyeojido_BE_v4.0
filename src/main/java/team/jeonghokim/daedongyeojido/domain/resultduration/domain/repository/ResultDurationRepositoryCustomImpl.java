a80e94229a23c75905ad345f11c98a214c2afa01
박태수 <taesupark0504@gmail.com>
2026-01-07 11:36:25 +0900

    chore (#138) :: @Repository 추가

--- src/main/java/team/jeonghokim/daedongyeojido/domain/resultduration/domain/repository/ResultDurationRepositoryCustomImpl.java
+++ src/main/java/team/jeonghokim/daedongyeojido/domain/resultduration/domain/repository/ResultDurationRepositoryCustomImpl.java
@@ -3,18 +3,20 @@ package team.jeonghokim.daedongyeojido.domain.resultduration.domain.repository;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import jakarta.persistence.LockModeType;
 import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
 import team.jeonghokim.daedongyeojido.domain.resultduration.domain.QResultDuration;
 import team.jeonghokim.daedongyeojido.domain.resultduration.domain.ResultDuration;
 import team.jeonghokim.daedongyeojido.domain.resultduration.domain.enums.Status;
 
 import java.util.Optional;
 
+@Repository
 @RequiredArgsConstructor
 public class ResultDurationRepositoryCustomImpl implements ResultDurationRepositoryCustom {
 
     private final JPAQueryFactory jpaQueryFactory;
     private final QResultDuration resultDuration = QResultDuration.resultDuration;
-    
+
     @Override
     public Optional<ResultDuration> findPendingResultDurationForUpdate() {
         return Optional.ofNullable(
