032b5e8b7a45e384fbf101ee3ca499bc5310c8eb
박태수 <taesupark0504@gmail.com>
2026-01-08 14:16:57 +0900

    feat (#138) :: event에 resultDuration추가

--- src/main/java/team/jeonghokim/daedongyeojido/infrastructure/event/domain/user/LargeScaleSmsEvent.java
+++ src/main/java/team/jeonghokim/daedongyeojido/infrastructure/event/domain/user/LargeScaleSmsEvent.java
@@ -1,6 +1,7 @@
 package team.jeonghokim.daedongyeojido.infrastructure.event.domain.user;
 
 import lombok.Builder;
+import team.jeonghokim.daedongyeojido.domain.resultduration.domain.ResultDuration;
 import team.jeonghokim.daedongyeojido.infrastructure.scheduler.payload.SchedulerPayload;
 import team.jeonghokim.daedongyeojido.infrastructure.sms.type.Message;
 
@@ -9,6 +10,7 @@ public record LargeScaleSmsEvent(
         String phoneNumber,
         Message message,
         String clubName,
-        SchedulerPayload payload
+        SchedulerPayload payload,
+        ResultDuration resultDuration
 ) {
 }
