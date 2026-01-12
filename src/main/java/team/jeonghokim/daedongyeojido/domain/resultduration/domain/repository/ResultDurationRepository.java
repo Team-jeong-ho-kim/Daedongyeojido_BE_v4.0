0294297b31b2279257f9addd96ad8a65614084be
박태수 <taesupark0504@gmail.com>
2026-01-12 18:58:39 +0900

    refactor (#148) :: 여러 작업 대량 처리로 Executor 과도화 방지 Backpressure전략

--- src/main/java/team/jeonghokim/daedongyeojido/domain/resultduration/domain/repository/ResultDurationRepository.java
+++ src/main/java/team/jeonghokim/daedongyeojido/domain/resultduration/domain/repository/ResultDurationRepository.java
@@ -3,7 +3,6 @@ package team.jeonghokim.daedongyeojido.domain.resultduration.domain.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import team.jeonghokim.daedongyeojido.domain.resultduration.domain.ResultDuration;
 import team.jeonghokim.daedongyeojido.domain.resultduration.domain.enums.Status;
-import team.jeonghokim.daedongyeojido.domain.resultduration.presentation.dto.response.ResultDurationResponse;
 
 import java.util.Optional;
 
