902877465e3f792f92a4c4966d729e46f623c42c
박태수 <taesupark0504@gmail.com>
2026-01-13 11:08:42 +0900

    chore (#147) :: userFacade admin관련 제거

--- src/main/java/team/jeonghokim/daedongyeojido/domain/user/facade/UserFacade.java
+++ src/main/java/team/jeonghokim/daedongyeojido/domain/user/facade/UserFacade.java
@@ -3,9 +3,6 @@ package team.jeonghokim.daedongyeojido.domain.user.facade;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Component;
-import team.jeonghokim.daedongyeojido.domain.admin.domain.Admin;
-import team.jeonghokim.daedongyeojido.domain.admin.domain.repository.AdminRepository;
-import team.jeonghokim.daedongyeojido.domain.admin.exeption.AdminNotFoundException;
 import team.jeonghokim.daedongyeojido.domain.user.domain.User;
 import team.jeonghokim.daedongyeojido.domain.user.domain.repository.UserRepository;
 import team.jeonghokim.daedongyeojido.domain.user.exception.UserNotFoundException;
@@ -15,7 +12,6 @@ import team.jeonghokim.daedongyeojido.domain.user.exception.UserNotFoundExceptio
 public class UserFacade {
 
     private final UserRepository userRepository;
-    private final AdminRepository adminRepository;
 
     public User getCurrentUser() {
 
@@ -23,14 +19,6 @@ public class UserFacade {
 
         return userRepository.findByAccountId(accountId)
                 .orElseThrow(() -> UserNotFoundException.EXCEPTION);
-    }
-
-    public Admin getCurrentAdmin() {
-
-        String accountId = SecurityContextHolder.getContext().getAuthentication().getName();
-
-        return adminRepository.findByAccountId(accountId)
-                .orElseThrow(() -> AdminNotFoundException.EXCEPTION);
     }
 
 }
