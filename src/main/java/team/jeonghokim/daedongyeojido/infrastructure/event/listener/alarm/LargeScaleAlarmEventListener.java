e0bda8a7f3a9cfa0490cb4089881c3a107e4ea9c
박태수 <taesupark0504@gmail.com>
2026-01-15 14:06:04 +0900

    refactor (#145) :: 인프라 실패 예외로 Retry 설정

--- src/main/java/team/jeonghokim/daedongyeojido/infrastructure/event/listener/alarm/LargeScaleAlarmEventListener.java
+++ src/main/java/team/jeonghokim/daedongyeojido/infrastructure/event/listener/alarm/LargeScaleAlarmEventListener.java
@@ -2,6 +2,8 @@ package team.jeonghokim.daedongyeojido.infrastructure.event.listener.alarm;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.dao.DataAccessException;
+import org.springframework.data.redis.RedisConnectionFailureException;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.retry.annotation.Backoff;
 import org.springframework.retry.annotation.Recover;
@@ -12,8 +14,6 @@ import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.transaction.event.TransactionPhase;
 import org.springframework.transaction.event.TransactionalEventListener;
-import org.springframework.web.client.HttpServerErrorException;
-import org.springframework.web.client.ResourceAccessException;
 import team.jeonghokim.daedongyeojido.domain.admin.service.DecideResultDurationService;
 import team.jeonghokim.daedongyeojido.domain.alarm.domain.UserAlarm;
 import team.jeonghokim.daedongyeojido.domain.alarm.domain.repository.UserAlarmRepository;
@@ -42,7 +42,7 @@ public class LargeScaleAlarmEventListener {
 
     @Async("largeScaleAlarmExecutor")
     @Retryable(
-            retryFor = HttpApiException.class,
+            retryFor = {DataAccessException.class, RedisConnectionFailureException.class },
             recover = LARGE_SCALE_ALARM_EVENT_RETRY,
             maxAttempts = 5,
             backoff = @Backoff(delay = 500, multiplier = 2)
@@ -67,8 +67,7 @@ public class LargeScaleAlarmEventListener {
 
             decideResultDurationService.executeAlarmScheduler(event.resultDuration());
 
-        } catch (HttpServerErrorException |
-                ResourceAccessException e) {
+        } catch (Exception e) {
 
             log.error("유저 알람 이벤트 실패 userId={} alarmType={}", event.userId(), event.alarmType(), e);
 
@@ -77,7 +76,7 @@ public class LargeScaleAlarmEventListener {
     }
 
     @Recover
-    public void recoverLargeScaleAlarmEvent(HttpApiException e, LargeScaleAlarmEvent event) {
+    public void recoverLargeScaleAlarmEvent(Exception e, LargeScaleAlarmEvent event) {
 
         alarmRedisTemplate.opsForZSet()
                 .remove(RESULT_DURATION_ALARM_ZSET, event.payload());
