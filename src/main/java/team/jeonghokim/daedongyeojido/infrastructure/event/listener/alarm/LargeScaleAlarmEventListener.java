a53b137f2b5e9f0c13408936899c1a2726873bcd
박태수 <taesupark0504@gmail.com>
2026-01-15 09:29:40 +0900

    feat (#145) :: handleLargeScaleAlarmEvent  method

---
+++ src/main/java/team/jeonghokim/daedongyeojido/infrastructure/event/listener/alarm/LargeScaleAlarmEventListener.java
@@ -0,0 +1,70 @@
+package team.jeonghokim.daedongyeojido.infrastructure.event.listener.alarm;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.retry.annotation.Backoff;
+import org.springframework.retry.annotation.Retryable;
+import org.springframework.scheduling.annotation.Async;
+import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.transaction.event.TransactionPhase;
+import org.springframework.transaction.event.TransactionalEventListener;
+import org.springframework.web.client.HttpServerErrorException;
+import org.springframework.web.client.ResourceAccessException;
+import team.jeonghokim.daedongyeojido.domain.alarm.domain.UserAlarm;
+import team.jeonghokim.daedongyeojido.domain.alarm.domain.repository.UserAlarmRepository;
+import team.jeonghokim.daedongyeojido.domain.user.domain.User;
+import team.jeonghokim.daedongyeojido.domain.user.domain.repository.UserRepository;
+import team.jeonghokim.daedongyeojido.infrastructure.event.domain.user.LargeScaleAlarmEvent;
+import team.jeonghokim.daedongyeojido.infrastructure.event.exception.HttpApiException;
+import team.jeonghokim.daedongyeojido.infrastructure.scheduler.payload.SchedulerAlarmPayload;
+
+import static team.jeonghokim.daedongyeojido.infrastructure.scheduler.service.SchedulerService.RESULT_DURATION_ZSET;
+
+@Slf4j
+@Component
+@RequiredArgsConstructor
+public class LargeScaleAlarmEventListener {
+
+    private final UserAlarmRepository userAlarmRepository;
+    private final UserRepository userRepository;
+    private final RedisTemplate<String, SchedulerAlarmPayload> smsRedisTemplate;
+
+    private static final String LARGE_SCALE_ALARM_EVENT_RETRY = "recoverLargeScaleAlarmEvent";
+
+    @Async("largeScaleAlarmExecutor")
+    @Retryable(
+            retryFor = HttpApiException.class,
+            recover = LARGE_SCALE_ALARM_EVENT_RETRY,
+            maxAttempts = 5,
+            backoff = @Backoff(delay = 500, multiplier = 2)
+    )
+    @TransactionalEventListener(phase = TransactionPhase.AFTER_COMMIT)
+    @Transactional(propagation = Propagation.REQUIRES_NEW)
+    public void handleLargeScaleAlarmEvent(LargeScaleAlarmEvent event) {
+
+        try {
+            User receiver = userRepository.findById(event.userId())
+                    .orElseThrow();
+
+            userAlarmRepository.save(UserAlarm.builder()
+                    .title(event.title())
+                    .content(event.content())
+                    .receiver(receiver)
+                    .alarmType(event.alarmType())
+                    .build());
+
+            smsRedisTemplate.opsForZSet()
+                    .remove(RESULT_DURATION_ZSET, event.payload());
+
+        } catch (HttpServerErrorException |
+                ResourceAccessException e) {
+
+            log.error("유저 알람 이벤트 실패 userId={} alarmType={}", event.userId(), event.alarmType(), e);
+
+            throw new HttpApiException(e);
+        }
+    }
+}
