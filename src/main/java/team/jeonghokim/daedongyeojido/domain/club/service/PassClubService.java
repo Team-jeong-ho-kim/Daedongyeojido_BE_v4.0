901ddf157eea889fe0ba3a1dd76ef631883b5076
박태수 <taesupark0504@gmail.com>
2026-01-15 11:27:15 +0900

    feat (#145) :: 동아리 합격/불합격 여부에 따라 ALARM event ZSET에 저장

--- src/main/java/team/jeonghokim/daedongyeojido/domain/club/service/PassClubService.java
+++ src/main/java/team/jeonghokim/daedongyeojido/domain/club/service/PassClubService.java
@@ -4,6 +4,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import team.jeonghokim.daedongyeojido.domain.alarm.domain.enums.AlarmType;
 import team.jeonghokim.daedongyeojido.domain.application.exception.ApplicationNotFoundException;
 import team.jeonghokim.daedongyeojido.domain.club.exception.AlreadyJoinClubException;
 import team.jeonghokim.daedongyeojido.domain.club.exception.ClubAccessDeniedException;
@@ -11,6 +12,7 @@ import team.jeonghokim.daedongyeojido.domain.club.presentation.dto.request.PassC
 import team.jeonghokim.daedongyeojido.domain.resultduration.domain.ResultDuration;
 import team.jeonghokim.daedongyeojido.domain.resultduration.domain.repository.ResultDurationRepository;
 import team.jeonghokim.daedongyeojido.domain.resultduration.exception.ResultDurationNotFoundException;
+import team.jeonghokim.daedongyeojido.infrastructure.scheduler.payload.SchedulerAlarmPayload;
 import team.jeonghokim.daedongyeojido.infrastructure.scheduler.payload.SchedulerSmsPayload;
 import team.jeonghokim.daedongyeojido.domain.submission.domain.Submission;
 import team.jeonghokim.daedongyeojido.domain.submission.domain.repository.SubmissionRepository;
@@ -26,9 +28,11 @@ public class PassClubService {
     private final UserFacade userFacade;
     private final SubmissionRepository submissionRepository;
     private final ResultDurationRepository resultDurationRepository;
-    private final RedisTemplate<String, SchedulerSmsPayload> redisTemplate;
+    private final RedisTemplate<String, SchedulerSmsPayload> smsRedisTemplate;
+    private final RedisTemplate<String, SchedulerAlarmPayload> alarmRedisTemplate;
 
-    private static final String RESULT_DURATION_ZSET = "club:result-duration";
+    private static final String RESULT_DURATION_SMS_ZSET = "club:result-duration-sms";
+    private static final String RESULT_DURATION_ALARM_ZSET = "club:result-duration-alarm";
     public static final String SEOUL_TIME_ZONE = "Asia/Seoul";
 
     @Transactional
@@ -44,6 +48,8 @@ public class PassClubService {
         submission.applyPassResult(request.isPassed());
         
         saveSMS(submission, request.isPassed());
+
+        saveAlarm(submission, request.isPassed());
     }
 
     private void validate(User user, Submission submission) {
@@ -73,7 +79,26 @@ public class PassClubService {
                 .clubName(submission.getApplicationForm().getClub().getClubName())
                 .build();
 
-        redisTemplate.opsForZSet()
-                .add(RESULT_DURATION_ZSET, payload, score);
+        smsRedisTemplate.opsForZSet()
+                .add(RESULT_DURATION_SMS_ZSET, payload, score);
+    }
+
+    private void saveAlarm(Submission submission, boolean isPassed) {
+
+        ResultDuration resultDuration = resultDurationRepository.findTopByOrderByIdDesc()
+                .orElseThrow(() -> ResultDurationNotFoundException.EXCEPTION);
+
+        long score = resultDuration.getResultDurationTime()
+                .atZone(ZoneId.of(SEOUL_TIME_ZONE))
+                .toEpochSecond();
+
+        SchedulerAlarmPayload payload = SchedulerAlarmPayload.builder()
+                    .alarmType(isPassed ? AlarmType.CLUB_FINAL_ACCEPTED : AlarmType.CLUB_FINAL_REJECTED)
+                    .club(submission.getApplicationForm().getClub())
+                    .userId(submission.getUser().getId())
+                .build();
+
+        alarmRedisTemplate.opsForZSet()
+                .add(RESULT_DURATION_ALARM_ZSET, payload, score);
     }
 }
--- src/main/java/team/jeonghokim/daedongyeojido/infrastructure/scheduler/payload/SchedulerAlarmPayload.java
+++ src/main/java/team/jeonghokim/daedongyeojido/infrastructure/scheduler/payload/SchedulerAlarmPayload.java
@@ -1,15 +1,15 @@
 package team.jeonghokim.daedongyeojido.infrastructure.scheduler.payload;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Builder;
 import team.jeonghokim.daedongyeojido.domain.alarm.domain.enums.AlarmType;
 import team.jeonghokim.daedongyeojido.domain.club.domain.Club;
-import team.jeonghokim.daedongyeojido.domain.user.domain.User;
 
 @Builder
 public record SchedulerAlarmPayload(
 
-        User user,
+        Long userId,
         Club club,
-        AlarmType alarmType
+        @JsonProperty("is_passed") AlarmType alarmType
 ) {
 }
