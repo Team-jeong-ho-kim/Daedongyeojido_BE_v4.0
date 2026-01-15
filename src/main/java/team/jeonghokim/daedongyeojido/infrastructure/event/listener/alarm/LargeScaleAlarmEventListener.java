97fc4197bcf45b2196df85c290531201d5245d90
박태수 <taesupark0504@gmail.com>
2026-01-15 09:44:26 +0900

    feat (#145) :: largeScaleAlarmEvent 최종 실패 메서드

--- src/main/java/team/jeonghokim/daedongyeojido/infrastructure/event/listener/alarm/LargeScaleAlarmEventListener.java
+++ src/main/java/team/jeonghokim/daedongyeojido/infrastructure/event/listener/alarm/LargeScaleAlarmEventListener.java
@@ -4,6 +4,7 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.retry.annotation.Backoff;
+import org.springframework.retry.annotation.Recover;
 import org.springframework.retry.annotation.Retryable;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Component;
@@ -19,7 +20,10 @@ import team.jeonghokim.daedongyeojido.domain.user.domain.User;
 import team.jeonghokim.daedongyeojido.domain.user.domain.repository.UserRepository;
 import team.jeonghokim.daedongyeojido.infrastructure.event.domain.user.LargeScaleAlarmEvent;
 import team.jeonghokim.daedongyeojido.infrastructure.event.exception.HttpApiException;
+import team.jeonghokim.daedongyeojido.infrastructure.event.exception.SmsEventFinalFailedException;
 import team.jeonghokim.daedongyeojido.infrastructure.scheduler.payload.SchedulerAlarmPayload;
+
+import java.time.Instant;
 
 import static team.jeonghokim.daedongyeojido.infrastructure.scheduler.service.SchedulerService.RESULT_DURATION_ZSET;
 
@@ -33,6 +37,7 @@ public class LargeScaleAlarmEventListener {
     private final RedisTemplate<String, SchedulerAlarmPayload> smsRedisTemplate;
 
     private static final String LARGE_SCALE_ALARM_EVENT_RETRY = "recoverLargeScaleAlarmEvent";
+    private static final String FAILED_ZSET  = "user:result-duration:failed";
 
     @Async("largeScaleAlarmExecutor")
     @Retryable(
@@ -66,5 +71,19 @@ public class LargeScaleAlarmEventListener {
 
             throw new HttpApiException(e);
         }
+    }
+
+    @Recover
+    public void recoverLargeScaleAlarmEvent(HttpApiException e, LargeScaleAlarmEvent event) {
+
+        smsRedisTemplate.opsForZSet()
+                .remove(RESULT_DURATION_ZSET, event.payload());
+
+        smsRedisTemplate.opsForZSet()
+                .add(FAILED_ZSET, event.payload(), Instant.now().getEpochSecond());
+
+        log.error("유저 알람 이벤트 최종 실패 userId={} alarmType={}", event.userId(), event.alarmType(), e);
+
+        throw new SmsEventFinalFailedException(e);
     }
 }
