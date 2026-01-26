389414ac4fa159247d39c981962517e73bcf6c7a
박태수 <taesupark0504@gmail.com>
2026-01-26 09:02:39 +0900

    refactor (#174) :: update/create request로 분리

---
+++ src/main/java/team/jeonghokim/daedongyeojido/domain/club/presentation/dto/request/UpdateClubRequest.java
@@ -0,0 +1,33 @@
+package team.jeonghokim.daedongyeojido.domain.club.presentation.dto.request;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+import org.hibernate.validator.constraints.URL;
+import org.springframework.web.multipart.MultipartFile;
+import team.jeonghokim.daedongyeojido.domain.user.domain.enums.Major;
+
+import java.util.List;
+
+public record UpdateClubRequest(
+
+        @NotBlank(message = "동아리 이름은 필수입니다.")
+        @Size(max = 20, message = "동아리 이름은 최대 20자까지 작성할 수 있습니다.")
+        String clubName,
+
+        MultipartFile clubImage,
+
+        @NotBlank(message = "한 줄 소개는 필수입니다.")
+        @Size(max = 30, message = "한 줄 소개는 최대 30자까지 작성할 수 있습니다.")
+        String oneLiner,
+
+        @NotBlank(message = "동아리 소개는 필수입니다.")
+        @Size(max = 500, message = "동아리 소개는 최대 500자까지 작성할 수 있습니다.")
+        String introduction,
+
+        @NotNull(message = "전공 리스트는 필수입니다.")
+        List<Major> major,
+
+        List<@URL(message = "유효한 URL 형식이어야 합니다.") String> link,
+        ) {
+}
