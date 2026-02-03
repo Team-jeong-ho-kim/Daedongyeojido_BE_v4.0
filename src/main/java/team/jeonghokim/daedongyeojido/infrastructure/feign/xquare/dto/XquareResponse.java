8bebe495d384d6c2ae839d0456a758df4658216c
coehgns <modooboiroo@gmail.com>
2026-02-03 17:12:49 +0900

    refactor (#181) :: Xquare 응답 DTO @JsonProperty 제거

--- src/main/java/team/jeonghokim/daedongyeojido/infrastructure/feign/xquare/dto/XquareResponse.java
+++ src/main/java/team/jeonghokim/daedongyeojido/infrastructure/feign/xquare/dto/XquareResponse.java
@@ -1,14 +1,11 @@
 package team.jeonghokim.daedongyeojido.infrastructure.feign.xquare.dto;
 
-import com.fasterxml.jackson.annotation.JsonProperty;
-
 import java.time.LocalDate;
 import java.util.UUID;
 
 public record XquareResponse(
         UUID id,
 
-        @JsonProperty("account_id")
         String accountId,
 
         String password,
@@ -17,15 +14,12 @@ public record XquareResponse(
 
         Integer grade,
 
-        @JsonProperty("class_num")
         Integer classNum,
 
         Integer num,
 
-        @JsonProperty("user_role")
         String userRole,
 
-        @JsonProperty("birth_day")
         LocalDate birthDay,
 
         String profileImgUrl,
