1fca6da5460cb3f58fcce2f779537e6c287bdd09
coehgns <modooboiroo@gmail.com>
2026-02-03 17:12:50 +0900

    refactor (#181) :: 유저 결정 DTO @JsonProperty 제거

--- src/main/java/team/jeonghokim/daedongyeojido/domain/user/presentation/dto/request/DecideClubRequest.java
+++ src/main/java/team/jeonghokim/daedongyeojido/domain/user/presentation/dto/request/DecideClubRequest.java
@@ -1,6 +1,5 @@
 package team.jeonghokim.daedongyeojido.domain.user.presentation.dto.request;
 
-import com.fasterxml.jackson.annotation.JsonProperty;
 import jakarta.validation.constraints.NotNull;
 import lombok.Getter;
 
@@ -8,6 +7,5 @@ import lombok.Getter;
 public class DecideClubRequest {
 
     @NotNull(message = "동아리 선택 여부를 결정해주세요.")
-    @JsonProperty("isSelected")
     private Boolean isSelected;
 }
