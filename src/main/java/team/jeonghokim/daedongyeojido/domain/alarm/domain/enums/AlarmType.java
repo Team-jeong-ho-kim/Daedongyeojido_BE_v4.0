f63018122715cda674c4fb923cd87f084a2fcb64
박태수 <taesupark0504@gmail.com>
2026-02-05 22:52:48 +0900

    refactor :: 동아리원 합류 신청 AlarmCategory 변경

--- src/main/java/team/jeonghokim/daedongyeojido/domain/alarm/domain/enums/AlarmType.java
+++ src/main/java/team/jeonghokim/daedongyeojido/domain/alarm/domain/enums/AlarmType.java
@@ -13,7 +13,7 @@ public enum AlarmType {
     DISSOLVE_CLUB_APPLY("%s 해체 신청", "%s의 동아리 해체 신청이 접수되었습니다.", AlarmCategory.COMMON),
     CLUB_FINAL_ACCEPTED("%s 최종 합격", "축하드립니다! %s 동아리에 최종 합격하였습니다.", AlarmCategory.CLUB_ACCEPTED),
     CLUB_FINAL_REJECTED("%s 최종 불합격", "아쉽지만 지원자님은 %s 동아리에 불합격하셨습니다.", AlarmCategory.COMMON),
-    CLUB_MEMBER_APPLY("%s 동아리원 합류 신청", "%s 동아리에 동아리원으로 합류 신청이 도착하였습니다. 합류 / 거절 여부를 결정해주세요.", AlarmCategory.CLUB_ACCEPTED),
+    CLUB_MEMBER_APPLY("%s 동아리원 합류 신청", "%s 동아리에 동아리원으로 합류 신청이 도착하였습니다. 합류 / 거절 여부를 결정해주세요.", AlarmCategory.CLUB_MEMBER_APPLY),
     CLUB_CREATION_ACCEPTED("%s 개설 성공", "%s 동아리가 성공적으로 개설되었습니다.", AlarmCategory.COMMON),
     CLUB_CREATION_REJECTED("%s 개설 실패", "%s 동아리의 개설 신청이 거부되었습니다.", AlarmCategory.COMMON),
     CLUB_DISSOLUTION_ACCEPTED("%s 해체 성공", "%s 동아리가 성공적으로 해체되었습니다.", AlarmCategory.COMMON),
