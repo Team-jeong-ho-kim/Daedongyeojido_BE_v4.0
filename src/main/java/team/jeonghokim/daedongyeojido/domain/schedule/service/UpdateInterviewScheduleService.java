ae5f06b4f399a351e5405adb84ce54c3665ec349
박태수 <taesupark0504@gmail.com>
2026-02-11 21:59:02 +0900

    feat :: 면접 일정 변경 api 알림 적용

--- src/main/java/team/jeonghokim/daedongyeojido/domain/schedule/service/UpdateInterviewScheduleService.java
+++ src/main/java/team/jeonghokim/daedongyeojido/domain/schedule/service/UpdateInterviewScheduleService.java
@@ -4,6 +4,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import team.jeonghokim.daedongyeojido.domain.alarm.domain.enums.AlarmType;
 import team.jeonghokim.daedongyeojido.domain.club.domain.Club;
 import team.jeonghokim.daedongyeojido.domain.schedule.domain.Schedule;
 import team.jeonghokim.daedongyeojido.domain.schedule.domain.repository.ScheduleRepository;
@@ -12,6 +13,7 @@ import team.jeonghokim.daedongyeojido.domain.schedule.exception.InterviewSchedul
 import team.jeonghokim.daedongyeojido.domain.schedule.presentation.dto.request.InterviewScheduleRequest;
 import team.jeonghokim.daedongyeojido.domain.user.domain.User;
 import team.jeonghokim.daedongyeojido.domain.user.facade.UserFacade;
+import team.jeonghokim.daedongyeojido.infrastructure.event.alarm.event.UserAlarmEvent;
 import team.jeonghokim.daedongyeojido.infrastructure.event.sms.event.UserSmsEvent;
 import team.jeonghokim.daedongyeojido.infrastructure.sms.type.Message;
 
@@ -38,6 +40,8 @@ public class UpdateInterviewScheduleService {
         schedule.updateSchedule(request);
 
         executeScheduleSMS(interviewer.getClub(), schedule.getApplicant());
+
+        executeScheduleAlarm(schedule, interviewer, schedule.getApplicant());
     }
 
     private void executeScheduleSMS(Club club, User user) {
@@ -46,6 +50,21 @@ public class UpdateInterviewScheduleService {
                         .clubName(club.getClubName())
                         .message(Message.INTERVIEW_SCHEDULE_CHANGED)
                         .phoneNumber(user.getPhoneNumber())
+                .build());
+    }
+
+    private void executeScheduleAlarm(Schedule schedule, User interviewer, User applicant) {
+
+        applicationEventPublisher.publishEvent(UserAlarmEvent.builder()
+                .title(AlarmType.INTERVIEW_SCHEDULE_CHANGED.formatTitle(interviewer.getClub().getClubName()))
+                .content(AlarmType.INTERVIEW_SCHEDULE_CHANGED.formatContent(
+                        interviewer.getClub().getClubName(),
+                        schedule.getInterviewSchedule(),
+                        schedule.getInterviewTime(),
+                        schedule.getPlace()))
+                .userId(applicant.getId())
+                .alarmType(AlarmType.INTERVIEW_SCHEDULE_CHANGED)
+                .category(AlarmType.INTERVIEW_SCHEDULE_CHANGED.getCategory())
                 .build());
     }
 }
