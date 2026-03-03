345f23ce7b7f301a0497f983a29d534ced8200cd
박태수 <taesupark0504@gmail.com>
2026-03-04 08:28:35 +0900

    chore (#212) :: getter 추가

--- src/main/java/team/jeonghokim/daedongyeojido/domain/clubcreationform/domain/ClubCreationForm.java
+++ src/main/java/team/jeonghokim/daedongyeojido/domain/clubcreationform/domain/ClubCreationForm.java
@@ -4,10 +4,12 @@ import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import lombok.AccessLevel;
 import lombok.Builder;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
 import team.jeonghokim.daedongyeojido.global.entity.BaseIdEntity;
 
 @Entity
+@Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class ClubCreationForm extends BaseIdEntity {
 
