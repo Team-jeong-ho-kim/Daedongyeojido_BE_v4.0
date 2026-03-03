b3d2515e03481acf87fb010f6ed534105e6258f8
박태수 <taesupark0504@gmail.com>
2026-03-03 15:24:35 +0900

    feat (#212) :: UploadClubCreationFormRequest

---
+++ src/main/java/team/jeonghokim/daedongyeojido/domain/admin/presentation/dto/request/UploadClubCreationFormRequest.java
@@ -0,0 +1,15 @@
+package team.jeonghokim.daedongyeojido.domain.admin.presentation.dto.request;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import org.springframework.web.multipart.MultipartFile;
+
+public record UploadClubCreationFormRequest(
+
+        @NotBlank(message = "동아리 신청 양식 제목을 입력해주세요.")
+        String fileName,
+
+        @NotNull(message = "동아리 신청 양식을 첨부해주세요.")
+        MultipartFile fileUrl
+) {
+}
