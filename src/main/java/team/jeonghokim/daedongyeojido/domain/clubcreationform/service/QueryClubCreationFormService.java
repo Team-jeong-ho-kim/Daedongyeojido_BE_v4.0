fd7cbac5d5169a1060c4b67cd2d75c5ce7065802
박태수 <taesupark0504@gmail.com>
2026-03-04 19:52:44 +0900

    feat (#220) :: 동아리 지원 양식 조회 api

---
+++ src/main/java/team/jeonghokim/daedongyeojido/domain/clubcreationform/service/QueryClubCreationFormService.java
@@ -0,0 +1,24 @@
+package team.jeonghokim.daedongyeojido.domain.clubcreationform.service;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import team.jeonghokim.daedongyeojido.domain.clubcreationform.domain.ClubCreationForm;
+import team.jeonghokim.daedongyeojido.domain.clubcreationform.domain.repository.ClubCreationFormRepository;
+import team.jeonghokim.daedongyeojido.domain.clubcreationform.exception.ClubCreationFormNotFoundException;
+import team.jeonghokim.daedongyeojido.domain.clubcreationform.presentation.dto.response.ClubCreationFormResponse;
+
+@Service
+@RequiredArgsConstructor
+public class QueryClubCreationFormService {
+    private final ClubCreationFormRepository clubCreationFormRepository;
+
+    @Transactional(readOnly = true)
+    public ClubCreationFormResponse execute(Long clubCreationFormId) {
+
+        ClubCreationForm clubCreationForm = clubCreationFormRepository.findById(clubCreationFormId)
+                .orElseThrow(() -> ClubCreationFormNotFoundException.EXCEPTION);
+
+        return new ClubCreationFormResponse(clubCreationForm.getFileName(), clubCreationForm.getFileUrl());
+    }
+}
