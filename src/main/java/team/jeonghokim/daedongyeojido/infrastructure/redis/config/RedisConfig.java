981dc73d0428bc14049186d19e008f44f7a72e93
coehgns <modooboiroo@gmail.com>
2026-03-04 09:44:09 +0900

    feat :: add redis database configuration for env-based separation

--- src/main/java/team/jeonghokim/daedongyeojido/infrastructure/redis/config/RedisConfig.java
+++ src/main/java/team/jeonghokim/daedongyeojido/infrastructure/redis/config/RedisConfig.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.RedisKeyValueAdapter;
 import org.springframework.data.redis.core.RedisTemplate;
@@ -30,10 +31,15 @@ public class RedisConfig {
     @Value("${redis.port}")
     private Integer port;
 
+    @Value("${redis.database}")
+    private Integer database;
+
     private final ObjectMapper objectMapper;
 
     @Bean
     public RedisConnectionFactory redisConnectionFactory() {
+        RedisStandaloneConfiguration config = new RedisStandaloneConfiguration(host, port);
+        config.setDatabase(database);
         return new LettuceConnectionFactory(host, port);
     }
 
