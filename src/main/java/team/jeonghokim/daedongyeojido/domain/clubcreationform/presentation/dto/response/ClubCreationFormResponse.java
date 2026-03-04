2924c088a689538c130bcc4a44a26dddd0d949e6
박태수 <taesupark0504@gmail.com>
2026-03-04 19:42:50 +0900

    feat (#220) clubCreationFormResponse

---
+++ src/main/java/team/jeonghokim/daedongyeojido/domain/clubcreationform/presentation/dto/response/ClubCreationFormResponse.java
@@ -0,0 +1,8 @@
+package team.jeonghokim.daedongyeojido.domain.clubcreationform.presentation.dto.response;
+
+public record ClubCreationFormResponse(
+
+        String fileName,
+        String fileUrl
+) {
+}
