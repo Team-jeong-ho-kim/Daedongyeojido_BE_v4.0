f8b2cc477edab6d645388de6f627e44af735df93
박태수 <taesupark0504@gmail.com>
2026-03-13 00:40:12 +0900

    feat (#241) :: teacherRepository

---
+++ src/main/java/team/jeonghokim/daedongyeojido/domain/teacher/domain/repository/TeacherRepository.java
@@ -0,0 +1,10 @@
+package team.jeonghokim.daedongyeojido.domain.teacher.domain.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import team.jeonghokim.daedongyeojido.domain.teacher.domain.Teacher;
+
+import java.util.Optional;
+
+public interface TeacherRepository extends JpaRepository<Teacher, Long> {
+
+}
