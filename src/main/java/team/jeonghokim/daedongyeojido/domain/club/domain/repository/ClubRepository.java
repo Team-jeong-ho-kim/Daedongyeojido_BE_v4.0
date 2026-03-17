738491e807eeb3989d48577ea8c86cf9a3a11ad7
coehgns <modooboiroo@gmail.com>
2026-03-17 21:56:20 +0900

    feat (#254) :: ClubRepository에 findByTeacherAccountId 메서드 추가

--- src/main/java/team/jeonghokim/daedongyeojido/domain/club/domain/repository/ClubRepository.java
+++ src/main/java/team/jeonghokim/daedongyeojido/domain/club/domain/repository/ClubRepository.java
@@ -4,9 +4,13 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import team.jeonghokim.daedongyeojido.domain.club.domain.Club;
 import team.jeonghokim.daedongyeojido.domain.user.domain.User;
 
+import java.util.Optional;
+
 public interface ClubRepository extends JpaRepository<Club, Long>, ClubRepositoryCustom {
 
     boolean existsByClubName(String clubName);
 
     boolean existsByClubApplicant(User clubApplicant);
+
+    Optional<Club> findByTeacherAccountId(String accountId);
 }
