2ce4ab9fb2d6321b58250b87c389665232f61111
박태수 <taesupark0504@gmail.com>
2026-03-23 11:36:27 +0900

    fix :: allow joined users to query submission history

--- src/main/java/team/jeonghokim/daedongyeojido/global/config/SecurityConfig.java
+++ src/main/java/team/jeonghokim/daedongyeojido/global/config/SecurityConfig.java
@@ -67,7 +67,7 @@ public class SecurityConfig {
                         .requestMatchers(HttpMethod.GET, "/users").hasAnyRole(STUDENT, CLUB_MEMBER, CLUB_LEADER)
                         .requestMatchers(HttpMethod.PATCH, "/users").hasAnyRole(STUDENT, CLUB_MEMBER, CLUB_LEADER)
                         .requestMatchers(HttpMethod.PATCH, "/users/members").hasRole(STUDENT)
-                        .requestMatchers(HttpMethod.GET, "/users/submissions").hasRole(STUDENT)
+                        .requestMatchers(HttpMethod.GET, "/users/submissions").hasAnyRole(STUDENT, CLUB_MEMBER, CLUB_LEADER)
                         .requestMatchers(HttpMethod.PATCH, "/users/submissions/**").hasRole(STUDENT)
 
                         // application
