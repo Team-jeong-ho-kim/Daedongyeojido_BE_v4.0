d84c3788de30a20bc74b71f4419947baa1aff994
oijwef098234 <oijwef098234@gmail.com>
2026-04-21 23:49:08 +0900

    feat(#289) :: 어노테이션 수정

--- src/main/java/team/jeonghokim/daedongyeojido/domain/teacher/presentation/dto/request/CreateOnePagerUrlFormRequest.java
+++ src/main/java/team/jeonghokim/daedongyeojido/domain/teacher/presentation/dto/request/CreateOnePagerUrlFormRequest.java
@@ -1,7 +1,6 @@
 package team.jeonghokim.daedongyeojido.domain.teacher.presentation.dto.request;
 
 import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
 
 public record CreateOnePagerUrlFormRequest(
         @NotBlank(message = "제목을 공백으로 둘 수 없습니다.")
@@ -10,7 +9,7 @@ public record CreateOnePagerUrlFormRequest(
         @NotBlank(message = "담당교사이름을 비워둘 순 없습니다.")
         String teacherName,
 
-        @NotNull(message = "구글 폼 링크를 첨부해주세요.")
+        @NotBlank(message = "구글 폼 링크를 첨부해주세요.")
         String formUrl,
 
         @NotBlank(message = "원페이져 마감일을 지정해주세요.")
