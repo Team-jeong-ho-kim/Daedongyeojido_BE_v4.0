4fce68d707cb1fe83f4f1aa0a630f79df3036656
oijwef098234 <oijwef098234@gmail.com>
2026-04-21 23:18:45 +0900

    feat(#288) :: 원페이져 url 형식 업로드 dto 구현

---
+++ src/main/java/team/jeonghokim/daedongyeojido/domain/teacher/presentation/dto/request/CreateOnePagerUrlFormRequest.java
@@ -0,0 +1,22 @@
+package team.jeonghokim.daedongyeojido.domain.teacher.presentation.dto.request;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+
+public record CreateOnePagerUrlFormRequest(
+        @NotBlank(message = "제목을 공백으로 둘 수 없습니다.")
+        String title,
+
+        @NotBlank(message = "담당교사이름을 비워둘 순 없습니다.")
+        String teacherName,
+
+        @NotNull(message = "구글 폼 링크를 첨부해주세요.")
+        String formUrl,
+
+        @NotBlank(message = "원페이져 마감일을 지정해주세요.")
+        String onePagerDuration,
+
+        @NotBlank(message = "원페이져 설명을 기재해주세요.")
+        String description
+) {
+}
