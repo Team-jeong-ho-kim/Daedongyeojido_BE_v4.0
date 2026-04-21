2df7c69f9a9305eaa3997aae427c4280640d4ae1
oijwef098234 <oijwef098234@gmail.com>
2026-04-21 23:22:06 +0900

    feat(#288) :: 원페이져 url 형식 업로드 클래스 controller 매핑

--- src/main/java/team/jeonghokim/daedongyeojido/domain/teacher/presentation/TeacherController.java
+++ src/main/java/team/jeonghokim/daedongyeojido/domain/teacher/presentation/TeacherController.java
@@ -4,9 +4,11 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 import team.jeonghokim.daedongyeojido.domain.teacher.presentation.dto.request.CreateOnePagerFileFormRequest;
+import team.jeonghokim.daedongyeojido.domain.teacher.presentation.dto.request.CreateOnePagerUrlFormRequest;
 import team.jeonghokim.daedongyeojido.domain.teacher.presentation.dto.response.QueryTeacherListResponse;
 import team.jeonghokim.daedongyeojido.domain.teacher.presentation.dto.response.QueryTeacherMyInfoResponse;
 import team.jeonghokim.daedongyeojido.domain.teacher.service.CreateOnePagerFileFormService;
+import team.jeonghokim.daedongyeojido.domain.teacher.service.CreateOnePagerUrlFormService;
 import team.jeonghokim.daedongyeojido.domain.teacher.service.QueryAvailableTeacherListService;
 import team.jeonghokim.daedongyeojido.domain.teacher.service.QueryTeacherMyInfoService;
 
@@ -18,6 +20,7 @@ public class TeacherController {
     private final QueryAvailableTeacherListService queryAvailableTeacherListService;
     private final QueryTeacherMyInfoService queryTeacherMyInfoService;
     private final CreateOnePagerFileFormService createOnePagerFormService;
+    private final CreateOnePagerUrlFormService createOnePagerUrlFormService;
 
     @GetMapping
     @ResponseStatus(HttpStatus.OK)
@@ -31,9 +34,15 @@ public class TeacherController {
         return queryTeacherMyInfoService.execute();
     }
 
-    @PostMapping("/onepager/forms")
+    @PostMapping("/onepager/forms-file")
     @ResponseStatus(HttpStatus.CREATED)
     public void createOnePagerForm(@ModelAttribute CreateOnePagerFileFormRequest createOnePagerFormRequest) {
         createOnePagerFormService.execute(createOnePagerFormRequest);
+    }
+
+    @PostMapping("/onepager/forms-url")
+    @ResponseStatus(HttpStatus.CREATED)
+    public void createOnePagerForm(@RequestBody CreateOnePagerUrlFormRequest createOnePagerUrlFormRequest) {
+        createOnePagerUrlFormService.execute(createOnePagerUrlFormRequest);
     }
 }
