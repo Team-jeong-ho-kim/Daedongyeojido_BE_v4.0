f2d8ed6fa8faf01c32bf7875256c01cda6267ca4
oijwef098234 <oijwef098234@gmail.com>
2026-04-21 23:20:09 +0900

    feat(#288) :: 원페이져 url 형식 업로그 클래스 구현

--- src/main/java/team/jeonghokim/daedongyeojido/domain/teacher/service/CreateOnePagerUrlFormService.java
+++ src/main/java/team/jeonghokim/daedongyeojido/domain/teacher/service/CreateOnePagerUrlFormService.java
@@ -3,26 +3,22 @@ package team.jeonghokim.daedongyeojido.domain.teacher.service;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-import team.jeonghokim.daedongyeojido.domain.file.domain.repository.FileRepository;
 import team.jeonghokim.daedongyeojido.domain.onepager.domain.OnePager;
 import team.jeonghokim.daedongyeojido.domain.onepager.domain.repository.OnePagerRepository;
-import team.jeonghokim.daedongyeojido.domain.teacher.presentation.dto.request.CreateOnePagerFileFormRequest;
-import team.jeonghokim.daedongyeojido.infrastructure.s3.service.S3Service;
+import team.jeonghokim.daedongyeojido.domain.teacher.presentation.dto.request.CreateOnePagerUrlFormRequest;
 
 @Service
 @RequiredArgsConstructor
 public class CreateOnePagerUrlFormService {
-    private final FileRepository fileRepository;
-    private final S3Service s3Service;
     private final OnePagerRepository onePagerRepository;
 
     @Transactional
-    public void execute(CreateOnePagerFileFormRequest request) {
+    public void execute(CreateOnePagerUrlFormRequest request) {
         OnePager onePager = OnePager.builder()
                 .title(request.title())
                 .description(request.description())
                 .formFile(null)
-                .formUrl(request.)
+                .formUrl(request.formUrl())
                 .teacherName(request.teacherName())
                 .onePagerDuration(request.onePagerDuration())
                 .build();
