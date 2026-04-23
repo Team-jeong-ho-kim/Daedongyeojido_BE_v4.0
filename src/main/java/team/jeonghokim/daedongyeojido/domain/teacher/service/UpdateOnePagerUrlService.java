49914b95605874edda1a094f6dd23c9e7735a054
oijwef098234 <oijwef098234@gmail.com>
2026-04-24 07:57:10 +0900

    feat(#290) :: 원페이져 url 양식 수정 메서드 구현

---
+++ src/main/java/team/jeonghokim/daedongyeojido/domain/teacher/service/UpdateOnePagerUrlService.java
@@ -0,0 +1,33 @@
+package team.jeonghokim.daedongyeojido.domain.teacher.service;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import team.jeonghokim.daedongyeojido.domain.onepager.domain.OnePager;
+import team.jeonghokim.daedongyeojido.domain.onepager.domain.repository.OnePagerRepository;
+import team.jeonghokim.daedongyeojido.domain.onepager.exception.OnePagerNotFoundException;
+import team.jeonghokim.daedongyeojido.domain.teacher.presentation.dto.request.OnePagerUrlFormRequest;
+
+@Service
+@RequiredArgsConstructor
+public class UpdateOnePagerUrlService {
+    private final OnePagerRepository onePagerRepository;
+
+    @Transactional
+    public void execute(OnePagerUrlFormRequest onePagerUrlFormRequest, Long onePagerId) {
+        OnePager onePager = onePagerRepository.findById(onePagerId)
+                .orElseThrow(() -> OnePagerNotFoundException.EXCEPTION);
+
+        onePager.update(
+                onePagerUrlFormRequest.title(),
+                onePagerUrlFormRequest.description(),
+                null,
+                onePagerUrlFormRequest.formUrl(),
+                onePagerUrlFormRequest.teacherName(),
+                onePagerUrlFormRequest.onePagerDurationType(),
+                onePagerUrlFormRequest.onePagerDuration()
+        );
+
+        onePagerRepository.save(onePager);
+    }
+}
