0c84602ed950a09fdedf1cfa4542dde05265fa53
oijwef098234 <oijwef098234@gmail.com>
2026-04-24 02:09:12 +0900

    feat(#290) :: 원페이져 양식 수정 메서드 구햔

--- src/main/java/team/jeonghokim/daedongyeojido/domain/teacher/service/UpdateOnePagerFileService.java
+++ src/main/java/team/jeonghokim/daedongyeojido/domain/teacher/service/UpdateOnePagerFileService.java
@@ -2,17 +2,40 @@ package team.jeonghokim.daedongyeojido.domain.teacher.service;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import team.jeonghokim.daedongyeojido.domain.file.domain.File;
+import team.jeonghokim.daedongyeojido.domain.file.domain.repository.FileRepository;
+import team.jeonghokim.daedongyeojido.domain.file.exception.FileNotFoundException;
 import team.jeonghokim.daedongyeojido.domain.onepager.domain.OnePager;
 import team.jeonghokim.daedongyeojido.domain.onepager.domain.repository.OnePagerRepository;
+import team.jeonghokim.daedongyeojido.domain.onepager.exception.OnePagerNotFoundException;
 import team.jeonghokim.daedongyeojido.domain.teacher.presentation.dto.request.OnePagerFileFormRequest;
 
 @Service
 @RequiredArgsConstructor
 public class UpdateOnePagerFileService {
     private final OnePagerRepository onePagerRepository;
+    private final FileRepository fileRepository;
 
+
+    @Transactional
     public void execute(OnePagerFileFormRequest onePagerFileFormRequest, Long onePagerId) {
         OnePager onePager = onePagerRepository.findById(onePagerId)
-                .orElseThrow(() -> );
+                .orElseThrow(() -> OnePagerNotFoundException.EXCEPTION);
+
+        File file = fileRepository.findByFileName(onePagerFileFormRequest.formFile().getOriginalFilename())
+                .orElseThrow(() -> FileNotFoundException.EXCEPTION);
+
+        onePager.update(
+                onePagerFileFormRequest.title(),
+                onePagerFileFormRequest.description(),
+                file,
+                null,
+                onePagerFileFormRequest.teacherName(),
+                onePagerFileFormRequest.onePagerDurationType(),
+                onePagerFileFormRequest.onePagerDuration()
+        );
+
+        onePagerRepository.save(onePager);
     }
 }
