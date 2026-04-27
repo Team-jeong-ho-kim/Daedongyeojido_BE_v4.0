b21ead201f5753775486d3f911a01661cceb26e1
oijwef098234 <oijwef098234@gmail.com>
2026-04-27 22:36:15 +0900

    feat(#292) :: 원페이져 상태 변경 메서드 구현

--- src/main/java/team/jeonghokim/daedongyeojido/domain/teacher/service/UpdateOnePagerStateService.java
+++ src/main/java/team/jeonghokim/daedongyeojido/domain/teacher/service/UpdateOnePagerStateService.java
@@ -2,8 +2,10 @@ package team.jeonghokim.daedongyeojido.domain.teacher.service;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import team.jeonghokim.daedongyeojido.domain.onepager.domain.OnePager;
 import team.jeonghokim.daedongyeojido.domain.onepager.domain.repository.OnePagerRepository;
+import team.jeonghokim.daedongyeojido.domain.onepager.exception.OnePagerInvalidException;
 import team.jeonghokim.daedongyeojido.domain.onepager.exception.OnePagerNotFoundException;
 import team.jeonghokim.daedongyeojido.domain.teacher.presentation.dto.request.ChangeOnePagerStateRequest;
 
@@ -12,15 +14,15 @@ import team.jeonghokim.daedongyeojido.domain.teacher.presentation.dto.request.Ch
 public class UpdateOnePagerStateService {
     private final OnePagerRepository onePagerRepository;
 
+    @Transactional
     public void execute(ChangeOnePagerStateRequest request) {
         OnePager onePager = onePagerRepository.findById(request.onePagerId())
             .orElseThrow(() -> OnePagerNotFoundException.EXCEPTION);
 
         if(onePager.getFormFile() == null) {
-
+            throw OnePagerInvalidException.EXCEPTION;
         }
 
         onePager.changeOnePagerState(request.onePagerState());
-
     }
 }
