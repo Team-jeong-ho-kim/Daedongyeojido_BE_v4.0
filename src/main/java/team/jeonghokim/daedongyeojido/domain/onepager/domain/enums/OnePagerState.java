8118b55c77fdf75b49acc4f8612da543280a8843
oijwef098234 <oijwef098234@gmail.com>
2026-04-27 22:50:45 +0900

    feat(#292) :: 원페이져 enum 양식 통일

--- src/main/java/team/jeonghokim/daedongyeojido/domain/onepager/domain/enums/OnePagerState.java
+++ src/main/java/team/jeonghokim/daedongyeojido/domain/onepager/domain/enums/OnePagerState.java
@@ -1,5 +1,6 @@
 package team.jeonghokim.daedongyeojido.domain.onepager.domain.enums;
 
+import com.fasterxml.jackson.annotation.JsonValue;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 
@@ -11,5 +12,6 @@ public enum OnePagerState {
     SUBMITTED("제출됨"),
     CANCELED("취소됨");
 
+    @JsonValue
     private final String description;
 }
