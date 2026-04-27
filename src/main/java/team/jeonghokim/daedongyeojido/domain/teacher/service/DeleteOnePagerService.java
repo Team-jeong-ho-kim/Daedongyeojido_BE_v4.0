d7bb899a144f1d7033956b72b38b34559feda00d
oijwef098234 <oijwef098234@gmail.com>
2026-04-27 17:25:29 +0900

    feat(#291) :: 고아데이터 발생 로직 해결

--- src/main/java/team/jeonghokim/daedongyeojido/domain/teacher/service/DeleteOnePagerService.java
+++ src/main/java/team/jeonghokim/daedongyeojido/domain/teacher/service/DeleteOnePagerService.java
@@ -3,6 +3,7 @@ package team.jeonghokim.daedongyeojido.domain.teacher.service;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import team.jeonghokim.daedongyeojido.domain.file.domain.repository.FileRepository;
 import team.jeonghokim.daedongyeojido.domain.onepager.domain.OnePager;
 import team.jeonghokim.daedongyeojido.domain.onepager.domain.repository.OnePagerRepository;
 import team.jeonghokim.daedongyeojido.domain.onepager.exception.OnePagerNotFoundException;
@@ -11,11 +12,16 @@ import team.jeonghokim.daedongyeojido.domain.onepager.exception.OnePagerNotFound
 @RequiredArgsConstructor
 public class DeleteOnePagerService {
     private final OnePagerRepository onePagerRepository;
+    private final FileRepository fileRepository;
 
     @Transactional
     public void execute(Long onePagerId){
         OnePager onePager = onePagerRepository.findById(onePagerId)
             .orElseThrow(() -> OnePagerNotFoundException.EXCEPTION);
+
+        if(onePager.getFormFile() != null) {
+            fileRepository.delete(onePager.getFormFile());
+        }
 
         onePagerRepository.delete(onePager);
     }
