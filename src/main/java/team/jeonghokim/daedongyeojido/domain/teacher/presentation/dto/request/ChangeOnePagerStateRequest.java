bb590b1651486128991633450598a750b9d4b8ef
oijwef098234 <oijwef098234@gmail.com>
2026-04-28 09:49:44 +0900

    feat(#292) :: null 검증 어노테이션 추가

--- src/main/java/team/jeonghokim/daedongyeojido/domain/teacher/presentation/dto/request/ChangeOnePagerStateRequest.java
+++ src/main/java/team/jeonghokim/daedongyeojido/domain/teacher/presentation/dto/request/ChangeOnePagerStateRequest.java
@@ -1,8 +1,10 @@
 package team.jeonghokim.daedongyeojido.domain.teacher.presentation.dto.request;
 
+import jakarta.validation.constraints.NotNull;
 import team.jeonghokim.daedongyeojido.domain.onepager.domain.enums.OnePagerState;
 
 public record ChangeOnePagerStateRequest(
+    @NotNull(message = "원페이져 상태를 선택해주세요.")
     OnePagerState onePagerState
 ) {
 }
