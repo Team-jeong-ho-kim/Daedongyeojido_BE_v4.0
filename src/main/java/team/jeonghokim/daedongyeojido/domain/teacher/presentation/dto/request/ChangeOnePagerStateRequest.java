196a6fb8225a63025a79260205dc0c59c6db7c5f
oijwef098234 <oijwef098234@gmail.com>
2026-04-28 11:39:58 +0900

    fix(#303) :: 사유 기재 요청 dto 구현

--- src/main/java/team/jeonghokim/daedongyeojido/domain/teacher/presentation/dto/request/ChangeOnePagerStateRequest.java
+++ src/main/java/team/jeonghokim/daedongyeojido/domain/teacher/presentation/dto/request/ChangeOnePagerStateRequest.java
@@ -5,6 +5,8 @@ import team.jeonghokim.daedongyeojido.domain.onepager.domain.enums.OnePagerState
 
 public record ChangeOnePagerStateRequest(
     @NotNull(message = "원페이져 상태를 선택해주세요.")
-    OnePagerState onePagerState
+    OnePagerState onePagerState,
+    
+    String reason
 ) {
 }
