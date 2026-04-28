57fa8faada69814b1a7e78f7d807625a4d606d82
oijwef098234 <oijwef098234@gmail.com>
2026-04-28 11:12:46 +0900

    feat(#292) :: enum 역할 분리

--- src/main/java/team/jeonghokim/daedongyeojido/domain/onepager/domain/enums/OnePagerState.java
+++ src/main/java/team/jeonghokim/daedongyeojido/domain/onepager/domain/enums/OnePagerState.java
@@ -1,17 +1,8 @@
 package team.jeonghokim.daedongyeojido.domain.onepager.domain.enums;
 
-import com.fasterxml.jackson.annotation.JsonValue;
-import lombok.Getter;
-import lombok.RequiredArgsConstructor;
-
-@Getter
-@RequiredArgsConstructor
 public enum OnePagerState {
-    APPROVED("승인됨"),
-    REJECTED("반려됨"),
-    SUBMITTED("제출됨"),
-    CANCELED("취소됨");
-
-    @JsonValue
-    private final String description;
+    APPROVED,
+    REJECTED,
+    SUBMITTED,
+    CANCELED
 }
