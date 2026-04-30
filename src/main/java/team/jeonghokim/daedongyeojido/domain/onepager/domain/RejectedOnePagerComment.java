21ff2c6b980392fea63572da8288709c6f095057
oijwef098234 <oijwef098234@gmail.com>
2026-04-30 22:17:27 +0900

    feat(#305) :: 코멘트 객체 생성자 구현

--- src/main/java/team/jeonghokim/daedongyeojido/domain/onepager/domain/RejectedOnePagerComment.java
+++ src/main/java/team/jeonghokim/daedongyeojido/domain/onepager/domain/RejectedOnePagerComment.java
@@ -2,6 +2,7 @@ package team.jeonghokim.daedongyeojido.domain.onepager.domain;
 
 import jakarta.persistence.*;
 import lombok.AccessLevel;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import team.jeonghokim.daedongyeojido.global.entity.BaseIdEntity;
@@ -20,4 +21,15 @@ public class RejectedOnePagerComment extends BaseIdEntity {
 
     @Column(nullable = false, length = 4)
     private String commentWriter;
+
+    @Builder
+    public RejectedOnePagerComment(
+        String comment,
+        OnePager onePager,
+        String commentWriter
+    ) {
+        this.comment = comment;
+        this.onePager = onePager;
+        this.commentWriter = commentWriter;
+    }
 }
--- src/main/java/team/jeonghokim/daedongyeojido/domain/teacher/service/CreateRejectedOnePagerCommentService.java
+++ src/main/java/team/jeonghokim/daedongyeojido/domain/teacher/service/CreateRejectedOnePagerCommentService.java
@@ -4,13 +4,14 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import team.jeonghokim.daedongyeojido.domain.onepager.domain.OnePager;
 import team.jeonghokim.daedongyeojido.domain.onepager.domain.repository.OnePagerRepository;
+import team.jeonghokim.daedongyeojido.domain.teacher.presentation.dto.request.CommentRequest;
 
 @Service
 @RequiredArgsConstructor
 public class CreateRejectedOnePagerCommentService {
     private final OnePagerRepository onePagerRepository;
 
-    public void execute() {
-        onePagerRepository.save(onePager);
+    public void execute(CommentRequest request) {
+
     }
 }
