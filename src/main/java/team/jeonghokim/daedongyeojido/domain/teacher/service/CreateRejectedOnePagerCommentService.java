14e40cec572716c4b855ef24add4fbfffa12b793
oijwef098234 <oijwef098234@gmail.com>
2026-04-30 22:39:20 +0900

    feat(#305) :: 코멘트 생성 메서드 구현

--- src/main/java/team/jeonghokim/daedongyeojido/domain/teacher/service/CreateRejectedOnePagerCommentService.java
+++ src/main/java/team/jeonghokim/daedongyeojido/domain/teacher/service/CreateRejectedOnePagerCommentService.java
@@ -1,17 +1,32 @@
 package team.jeonghokim.daedongyeojido.domain.teacher.service;
 
+import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import team.jeonghokim.daedongyeojido.domain.onepager.domain.OnePager;
+import team.jeonghokim.daedongyeojido.domain.onepager.domain.RejectedOnePagerComment;
 import team.jeonghokim.daedongyeojido.domain.onepager.domain.repository.OnePagerRepository;
+import team.jeonghokim.daedongyeojido.domain.onepager.domain.repository.RejectedOnePagerCommentRepository;
+import team.jeonghokim.daedongyeojido.domain.onepager.exception.OnePagerNotFoundException;
 import team.jeonghokim.daedongyeojido.domain.teacher.presentation.dto.request.CommentRequest;
 
 @Service
 @RequiredArgsConstructor
 public class CreateRejectedOnePagerCommentService {
     private final OnePagerRepository onePagerRepository;
+    private final RejectedOnePagerCommentRepository rejectedOnePagerCommentRepository;
 
+    @Transactional
     public void execute(CommentRequest request) {
+        OnePager onePager = onePagerRepository.findById(request.onePagerId())
+            .orElseThrow(() -> OnePagerNotFoundException.EXCEPTION);
 
+        RejectedOnePagerComment comment = RejectedOnePagerComment.builder()
+            .comment(request.comment())
+            .commentWriter(request.commentWriter())
+            .onePager(onePager)
+            .build();
+
+        rejectedOnePagerCommentRepository.save(comment);
     }
 }
