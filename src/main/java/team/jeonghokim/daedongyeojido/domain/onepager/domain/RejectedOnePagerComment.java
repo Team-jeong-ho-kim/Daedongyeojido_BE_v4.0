8411e0b9853f959d24b2e5de944c0fc75a15be95
oijwef098234 <oijwef098234@gmail.com>
2026-05-01 13:19:25 +0900

    feat(#305) :: 불필요한 와일드 카드 삭제

--- src/main/java/team/jeonghokim/daedongyeojido/domain/onepager/domain/RejectedOnePagerComment.java
+++ src/main/java/team/jeonghokim/daedongyeojido/domain/onepager/domain/RejectedOnePagerComment.java
@@ -1,6 +1,11 @@
 package team.jeonghokim.daedongyeojido.domain.onepager.domain;
 
-import jakarta.persistence.*;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.FetchType;
 import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
