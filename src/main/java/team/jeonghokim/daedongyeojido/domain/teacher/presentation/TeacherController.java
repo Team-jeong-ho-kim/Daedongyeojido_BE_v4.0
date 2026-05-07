6534499452b810656fda79b12da6a8e1f1e8b0d2
oijwef098234 <oijwef098234@gmail.com>
2026-05-07 22:18:41 +0900

    feat(#295) :: 어노테이션 변경

--- src/main/java/team/jeonghokim/daedongyeojido/domain/teacher/presentation/TeacherController.java
+++ src/main/java/team/jeonghokim/daedongyeojido/domain/teacher/presentation/TeacherController.java
@@ -70,7 +70,7 @@ public class TeacherController {
     @ResponseStatus(HttpStatus.NO_CONTENT)
     public void updateOnePagerFileForm(
             @PathVariable("form-id") Long formId,
-            @RequestBody @Valid OnePagerFileFormRequest request
+            @ModelAttribute @Valid OnePagerFileFormRequest request
     ) {
         updateOnePagerFileService.execute(request, formId);
     }
