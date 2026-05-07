6a3ed699f26b5edbea3412dfda3890a2ba447321
oijwef098234 <oijwef098234@gmail.com>
2026-05-07 17:53:54 +0900

    feat(#295) :: 예외 처리 클래스 구현

--- src/main/java/team/jeonghokim/daedongyeojido/domain/onepager/service/SubmitOnePagerFileUploadService.java
+++ src/main/java/team/jeonghokim/daedongyeojido/domain/onepager/service/SubmitOnePagerFileUploadService.java
@@ -34,6 +34,10 @@ public class SubmitOnePagerFileUploadService {
 
         String clubName = submitUser.getClub().getClubName();
 
+        if(clubName == null) {
+
+        }
+
         SubmitOnePager submitOnePager = SubmitOnePager.builder()
             .clubName(clubName)
             .onePagerState(OnePagerState.SUBMITTED)
