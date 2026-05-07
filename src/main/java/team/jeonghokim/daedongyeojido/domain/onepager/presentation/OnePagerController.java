b77f5469590581799c0cf8244f7297cfd222e840
oijwef098234 <oijwef098234@gmail.com>
2026-05-07 17:49:23 +0900

    feat(#295) :: 매핑 어노테이션 변경

--- src/main/java/team/jeonghokim/daedongyeojido/domain/onepager/presentation/OnePagerController.java
+++ src/main/java/team/jeonghokim/daedongyeojido/domain/onepager/presentation/OnePagerController.java
@@ -28,7 +28,7 @@ public class OnePagerController {
     @PostMapping("/{form-id}/submissions")
     @ResponseStatus(HttpStatus.CREATED)
     public void createSubmission(
-        @RequestBody @Valid SubmitOnePagerRequest request,
+        @ModelAttribute @Valid SubmitOnePagerRequest request,
         @PathVariable("form-id") Long submissionId
     ) {
         createSubmitOnePagerService.execute(request, submissionId);
