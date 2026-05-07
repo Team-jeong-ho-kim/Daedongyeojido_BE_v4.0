31ed533f94fbb3605f32e56088e937979ecaca3a
oijwef098234 <oijwef098234@gmail.com>
2026-05-07 17:18:37 +0900

    feat(#295) :: 원페이져 제출 메서드 구현

--- src/main/java/team/jeonghokim/daedongyeojido/domain/onepager/service/CreateSubmitOnePagerService.java
+++ src/main/java/team/jeonghokim/daedongyeojido/domain/onepager/service/CreateSubmitOnePagerService.java
@@ -3,12 +3,17 @@ package team.jeonghokim.daedongyeojido.domain.onepager.service;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import team.jeonghokim.daedongyeojido.domain.file.domain.File;
+import team.jeonghokim.daedongyeojido.domain.file.domain.repository.FileRepository;
+import team.jeonghokim.daedongyeojido.domain.file.exception.AlreadyFileExistsException;
 import team.jeonghokim.daedongyeojido.domain.onepager.domain.SubmitOnePager;
 import team.jeonghokim.daedongyeojido.domain.onepager.domain.enums.OnePagerState;
 import team.jeonghokim.daedongyeojido.domain.onepager.domain.repository.SubmitOnePagerRepository;
 import team.jeonghokim.daedongyeojido.domain.onepager.presentation.dto.request.SubmitOnePagerRequest;
 import team.jeonghokim.daedongyeojido.domain.user.domain.User;
 import team.jeonghokim.daedongyeojido.domain.user.facade.UserFacade;
+import team.jeonghokim.daedongyeojido.infrastructure.s3.service.S3Service;
+import team.jeonghokim.daedongyeojido.infrastructure.s3.type.FileType;
 
 import java.time.LocalDate;
 
@@ -16,18 +21,35 @@ import java.time.LocalDate;
 @RequiredArgsConstructor
 public class CreateSubmitOnePagerService {
     private final SubmitOnePagerRepository submitOnePagerRepository;
+    private final FileRepository fileRepository;
+    private final S3Service s3Service;
     private final UserFacade userFacade;
 
     @Transactional
-    public void execute(SubmitOnePagerRequest request) {
+    public void execute(SubmitOnePagerRequest request, Long onePagerId) {
         User submitUser = userFacade.getCurrentUser();
+
+        String fileName = request.submitFile().getName();
+
+        fileRepository.findByFileName(fileName).ifPresent(file -> {
+            throw AlreadyFileExistsException.EXCEPTION;
+        });
+
+        String fileUrl = s3Service.upload(request.submitFile(), FileType.DOCUMENT);
+
+        File file = File.builder()
+            .fileUrl(fileUrl)
+            .fileName(fileName)
+            .build();
+
+        fileRepository.save(file);
 
         String clubName = submitUser.getClub().getClubName();
 
         SubmitOnePager submitOnePager = SubmitOnePager.builder()
             .clubName(clubName)
             .onePagerState(OnePagerState.SUBMITTED)
-            .submitFile(request.submitFile())
+            .submitFile(file)
             .submitDate(LocalDate.now())
             .build();
 
