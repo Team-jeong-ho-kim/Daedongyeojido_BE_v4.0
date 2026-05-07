f6876c0362908eba16aba183f65defa8538fdb26
oijwef098234 <oijwef098234@gmail.com>
2026-05-07 09:24:32 +0900

    feat(#295) :: 원페이져 제출 dto 필드 수정

--- src/main/java/team/jeonghokim/daedongyeojido/domain/onepager/service/CreateSubmitOnePagerService.java
+++ src/main/java/team/jeonghokim/daedongyeojido/domain/onepager/service/CreateSubmitOnePagerService.java
@@ -2,15 +2,29 @@ package team.jeonghokim.daedongyeojido.domain.onepager.service;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import team.jeonghokim.daedongyeojido.domain.onepager.domain.SubmitOnePager;
+import team.jeonghokim.daedongyeojido.domain.onepager.domain.enums.OnePagerState;
 import team.jeonghokim.daedongyeojido.domain.onepager.domain.repository.OnePagerRepository;
 import team.jeonghokim.daedongyeojido.domain.onepager.presentation.dto.request.SubmitOnePagerRequest;
+import team.jeonghokim.daedongyeojido.domain.user.domain.User;
+import team.jeonghokim.daedongyeojido.domain.user.facade.UserFacade;
 
 @Service
 @RequiredArgsConstructor
 public class CreateSubmitOnePagerService {
     private final OnePagerRepository onePagerRepository;
+    private final UserFacade userFacade;
 
     public void execute(SubmitOnePagerRequest request) {
+        User submitUser = userFacade.getCurrentUser();
 
+        String clubName = submitUser.getClub().getClubName();
+
+        SubmitOnePager submitOnePager = SubmitOnePager.builder()
+            .clubName(clubName)
+            .onePagerState(OnePagerState.SUBMITTED)
+            .submitFile(request.submitFile())
+            .submitDate()
+            .build();
     }
 }
