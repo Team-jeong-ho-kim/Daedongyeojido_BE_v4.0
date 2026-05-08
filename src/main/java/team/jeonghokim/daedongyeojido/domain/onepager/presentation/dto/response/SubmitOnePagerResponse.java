beb36aa6af0acb072de595034c67d536876ca812
oijwef098234 <oijwef098234@gmail.com>
2026-05-08 19:04:15 +0900

    feat(#294) :: 제출된 폼 반환 객체 구현

--- src/main/java/team/jeonghokim/daedongyeojido/domain/onepager/presentation/dto/response/SubmitOnePagerResponse.java
+++ src/main/java/team/jeonghokim/daedongyeojido/domain/onepager/presentation/dto/response/SubmitOnePagerResponse.java
@@ -3,12 +3,23 @@ package team.jeonghokim.daedongyeojido.domain.onepager.presentation.dto.response
 import team.jeonghokim.daedongyeojido.domain.onepager.domain.enums.OnePagerState;
 
 import java.time.LocalDateTime;
+import java.util.List;
 
 public record SubmitOnePagerResponse(
     String clubName,
     OnePagerState onePagerState,
     String submitFileUrl,
     LocalDateTime submitDate,
-    List<>
+    List<SubmitCommentResponse> submitComments
 ) {
+    public static SubmitOnePagerResponse of(
+        String clubName,
+        OnePagerState onePagerState,
+        String submitFileUrl,
+        LocalDateTime submitDate,
+        List<SubmitCommentResponse> submitComments
+    ) {
+        return new SubmitOnePagerResponse(
+            clubName, onePagerState, submitFileUrl, submitDate, submitComments);
+    }
 }
