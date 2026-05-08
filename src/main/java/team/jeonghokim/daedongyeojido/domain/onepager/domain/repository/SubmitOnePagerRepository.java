824017e1667dbc49da0552cbfc81710a7a255741
oijwef098234 <oijwef098234@gmail.com>
2026-05-08 19:11:20 +0900

    feat(#294) :: 제출된 폼 원페이져로 전체조회 메서드 구현

--- src/main/java/team/jeonghokim/daedongyeojido/domain/onepager/domain/repository/SubmitOnePagerRepository.java
+++ src/main/java/team/jeonghokim/daedongyeojido/domain/onepager/domain/repository/SubmitOnePagerRepository.java
@@ -1,7 +1,11 @@
 package team.jeonghokim.daedongyeojido.domain.onepager.domain.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import team.jeonghokim.daedongyeojido.domain.onepager.domain.OnePager;
 import team.jeonghokim.daedongyeojido.domain.onepager.domain.SubmitOnePager;
 
+import java.util.List;
+
 public interface SubmitOnePagerRepository extends JpaRepository<SubmitOnePager, Long> {
+    List<SubmitOnePager> findByFormOnePager(OnePager formOnePager);
 }
--- src/main/java/team/jeonghokim/daedongyeojido/domain/onepager/service/QuerySubmitOnePagerService.java
+++ src/main/java/team/jeonghokim/daedongyeojido/domain/onepager/service/QuerySubmitOnePagerService.java
@@ -2,8 +2,14 @@ package team.jeonghokim.daedongyeojido.domain.onepager.service;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import team.jeonghokim.daedongyeojido.domain.onepager.domain.OnePager;
+import team.jeonghokim.daedongyeojido.domain.onepager.domain.SubmitOnePager;
 import team.jeonghokim.daedongyeojido.domain.onepager.domain.repository.OnePagerRepository;
 import team.jeonghokim.daedongyeojido.domain.onepager.domain.repository.SubmitOnePagerRepository;
+import team.jeonghokim.daedongyeojido.domain.onepager.exception.OnePagerNotFoundException;
+import team.jeonghokim.daedongyeojido.domain.onepager.presentation.dto.response.QueryListSubmitOnePagerResponse;
+
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
@@ -11,5 +17,10 @@ public class QuerySubmitOnePagerService {
     private final OnePagerRepository onePagerRepository;
     private final SubmitOnePagerRepository submitOnePagerRepository;
 
-    public
+    public QueryListSubmitOnePagerResponse execute(Long onePagerId) {
+        OnePager onePager = onePagerRepository.findById(onePagerId)
+            .orElseThrow(() -> OnePagerNotFoundException.EXCEPTION);
+
+        List<SubmitOnePager> submitOnePagerList = submitOnePagerRepository
+    }
 }
