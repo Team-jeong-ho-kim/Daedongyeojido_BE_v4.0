5b090415d8d751dfbcf7cd5fa32ed17ba50f672a
kn4tti <gaden119000@gmail.com>
2026-05-08 18:49:08 +0900

    refactor(#297) :: QueryDSL로 변경

--- src/main/java/team/jeonghokim/daedongyeojido/domain/onepager/domain/repository/SubmitOnePagerRepository.java
+++ src/main/java/team/jeonghokim/daedongyeojido/domain/onepager/domain/repository/SubmitOnePagerRepository.java
@@ -1,14 +1,7 @@
 package team.jeonghokim.daedongyeojido.domain.onepager.domain.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
-import team.jeonghokim.daedongyeojido.domain.club.domain.Club;
 import team.jeonghokim.daedongyeojido.domain.onepager.domain.SubmitOnePager;
 
-import java.util.List;
-
-public interface SubmitOnePagerRepository extends JpaRepository<SubmitOnePager, Long> {
-    @Query("SELECT s FROM SubmitOnePager s JOIN FETCH s.formOnePager WHERE s.club = :club")
-    List<SubmitOnePager> findByClub(@Param("club") Club club);
+public interface SubmitOnePagerRepository extends JpaRepository<SubmitOnePager, Long>, SubmitOnePagerRepositoryCustom {
 }
