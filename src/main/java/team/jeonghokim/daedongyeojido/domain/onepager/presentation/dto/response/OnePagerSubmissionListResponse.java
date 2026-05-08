1ed68490d3ff2aa2dcd4d2bf832857e52bffff07
kn4tti <gaden119000@gmail.com>
2026-05-08 20:14:22 +0900

    refactor(#298) :: 사용하지 않는 팩터리 메서드 제거

--- src/main/java/team/jeonghokim/daedongyeojido/domain/onepager/presentation/dto/response/OnePagerSubmissionListResponse.java
+++ src/main/java/team/jeonghokim/daedongyeojido/domain/onepager/presentation/dto/response/OnePagerSubmissionListResponse.java
@@ -3,7 +3,4 @@ package team.jeonghokim.daedongyeojido.domain.onepager.presentation.dto.response
 import java.util.List;
 
 public record OnePagerSubmissionListResponse(List<OnePagerSubmissionsResponse> submissions) {
-    public static OnePagerSubmissionListResponse from(List<OnePagerSubmissionsResponse> submissions) {
-        return new OnePagerSubmissionListResponse(submissions);
-    }
 }
