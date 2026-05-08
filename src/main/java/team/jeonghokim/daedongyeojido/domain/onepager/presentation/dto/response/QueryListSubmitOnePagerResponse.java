41221d65c5f21a938d19358dbfe1afe1ac9a50dd
oijwef098234 <oijwef098234@gmail.com>
2026-05-08 19:06:58 +0900

    feat(#294) :: 제출된 폼 전체조회 객체 구현

--- src/main/java/team/jeonghokim/daedongyeojido/domain/onepager/presentation/dto/response/QueryListSubmitOnePagerResponse.java
+++ src/main/java/team/jeonghokim/daedongyeojido/domain/onepager/presentation/dto/response/QueryListSubmitOnePagerResponse.java
@@ -1,12 +1,29 @@
 package team.jeonghokim.daedongyeojido.domain.onepager.presentation.dto.response;
 
 import java.time.LocalDateTime;
+import java.util.List;
 
 public record QueryListSubmitOnePagerResponse(
     String title,
     String description,
     LocalDateTime onePagerDuration,
-    String fileUrl
+    String fileUrl,
+    List<SubmitOnePagerResponse> submitOnePagers
 
 ) {
+    public static QueryListSubmitOnePagerResponse from(
+        String title,
+        String description,
+        LocalDateTime onePagerDuration,
+        String fileUrl,
+        List<SubmitOnePagerResponse> submitOnePagers
+    ) {
+        return new QueryListSubmitOnePagerResponse(
+            title,
+            description,
+            onePagerDuration,
+            fileUrl,
+            submitOnePagers
+        );
+    }
 }
